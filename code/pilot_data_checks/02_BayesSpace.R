008d640347c4e41fefef57c2b987e638d7663457
madhavitippani <madhavitippani28@gmail.com>
2021-12-21 14:20:11 -0500

    add array_row/col to colData (if not->Error: subscript contains invalid names) & add offset to run BayesSpace

--- code/pilot_data_checks/02_BayesSpace.R
+++ code/pilot_data_checks/02_BayesSpace.R
@@ -35,7 +35,7 @@ colnames(reducedDim(spePP, "UMAP")) = c("UMAP1", "UMAP2")
 
 # UMAP plots
 dir.create(here::here("processed-data", "pilot_data_checks", "plots"), showWarnings = FALSE)
-pdf(file=here::here("processed-data", "pilot_data_checks", "plots", "UMAP_hpc_spe.pdf"))
+pdf(file=here::here("processed-data", "pilot_data_checks", "plots", "hpc_UMAP_spe.pdf"))
 ggplot(data.frame(reducedDim(spePP, "UMAP")), 
        aes(x = UMAP1, y = UMAP2, color = factor(spePP$sample_id))) +
   geom_point() +
@@ -63,3 +63,55 @@ dev.off()
 speH = RunHarmony(spePP, "sample_id", verbose = F)
 speH = runUMAP(speH, dimred = "HARMONY", name = "UMAP.HARMONY")
 colnames(reducedDim(speH, "UMAP.HARMONY")) = c("UMAP1", "UMAP2")
+
+pdf(file=here::here("processed-data", "pilot_data_checks", "plots", "hpc_UMAP_harmony.pdf"))
+ggplot(data.frame(reducedDim(speH, "UMAP.HARMONY")), 
+       aes(x = UMAP1, y = UMAP2, color = factor(speH$sample_id))) +
+  geom_point() +
+  labs(color = "Sample/Capture Area") +
+  theme_bw()
+
+ggplot(data.frame(reducedDim(speH, "UMAP.HARMONY")), 
+       aes(x = UMAP1, y = UMAP2, color = factor(speH$subject))) +
+  geom_point() +
+  labs(color = "Subject/Brain") +
+  theme_bw()
+
+ggplot(data.frame(reducedDim(speH, "UMAP.HARMONY")), 
+       aes(x = UMAP1, y = UMAP2, color = factor(speH$slice))) +
+  geom_point() +
+  labs(color = "Tissue Slices") +
+  theme_bw()
+
+dev.off()
+
+save(speH, file = here::here("processed-data","pilot_data_checks","spe_harmony.Rdata"))
+
+# OFFSET
+# The spatial locations for each sample need to be offset so that spots of different samples are not neighbors. 
+# summary(spatialData(speH)$array_row)
+# summary(spatialData(speH)$array_col)
+auto_offset_row <- as.numeric(factor(unique(speH$sample_id))) * 100
+names(auto_offset_row) <-unique(speH$sample_id)
+speH$row <- spatialData(speH)$array_row + auto_offset_row[speH$sample_id]
+speH$col <- spatialData(speH)$array_col
+# summary(colData(speH)$row)
+# summary(colData(speH)$col)
+
+pdf(file=here::here("processed-data", "pilot_data_checks", "plots", "hpc_BayesSpace_OffsetCheck.pdf"))
+clusterPlot(speH, "subject", color = NA) + #make sure no overlap between samples
+  labs(fill = "Subject", title = "Offset check")
+dev.off()
+
+Sys.time() 
+speB = spatialCluster(speH, use.dimred = "HARMONY", q = 7, nrep = 10000) #use HARMONY
+Sys.time()
+
+# [1] "2021-12-21 14:00:56 EST"
+
+pdf(file=here::here("processed-data", "pilot_data_checks", "plots", "hpc_BayesSpace_clusterPlot.pdf"))
+clusterPlot(spe, color = NA) + #plot clusters
+  labs(title = "BayesSpace joint clustering")
+dev.off()
+
+save(speB, file=here::here("processed-data","pilot_data_checks", "spe_bayesSpace.Rdata"))
