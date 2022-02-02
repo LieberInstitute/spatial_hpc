aa090befebac44ff21c91c559138a3331dd9d8bc
madhavitippani <madhavitippani28@gmail.com>
2022-02-02 16:45:33 -0500

    Update shinyapp scripts
    Co-authored-by: Leonardo Collado Torres <lcolladotor@gmail.com>
    Co-authored-by: Erik D Nelson <erik.nelson116@gmail.com>

--- code/pilot_data_checks/shinyapp/app.R
+++ code/pilot_data_checks/shinyapp/app.R
@@ -3,8 +3,7 @@ library("markdown") ## Hm... to avoid this error
 # 2021-11-11T05:30:49.941401+00:00 shinyapps[5096402]: Listening on http://127.0.0.1:32863
 # 2021-11-11T05:30:50.218127+00:00 shinyapps[5096402]: Warning: Error in loadNamespace: there is no package called ‘markdown’
 # 2021-11-11T05:30:50.222437+00:00 shinyapps[5096402]:   111: <Anonymous>
-setwd("/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/")
-library("here")
+
 ## spatialLIBD uses golem
 options("golem.app.prod" = TRUE)
 
@@ -19,11 +18,13 @@ options(repos = BiocManager::repositories())
 ## Create a soft link to the data, otherwise rsconnect::deployApp doesn't work
 ## Note that soft link has to be relative to work
 # cd /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/pilot_data_checks/shinyapp/
-# ln -s /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/pilot_data_checks/spe_bayesSpace_50k.Rdata spe.Rdata
+# ln -s ../../../processed-data/pilot_data_checks/spe_bayesSpace_50k.Rdata spe.Rdata
 
 ## Load the data
-load(file=here::here("processed-data","pilot_data_checks","spe_bayesSpace_50k.Rdata", verbose = TRUE))
+load(file="spe.Rdata", verbose = TRUE)
 
+speB$BayesSpace <- speB$spatial.cluster
+speB$BayesSpace_initial <- speB$cluster.init
 vars <- colnames(colData(speB))
 
 ## Deploy the website
@@ -32,18 +33,21 @@ spatialLIBD::run_app(
   sce_layer = NULL,
   modeling_results = NULL,
   sig_genes = NULL,
-  title = "Visium HPC_2022",
+  title = "Visium HPC 2022",
   spe_discrete_vars = c(
-    vars[grep("10x_", vars)],
+    vars[grep("^10x_", vars)],
     "ManualAnnotation",
-    "spatial.cluster",
-    "cluster.init"
+    "BayesSpace",
+    "BayesSpace_initial",
+    "subject"
+ #   "sex"
   ),
   spe_continuous_vars = c(
     "sum_umi",
     "sum_gene",
     "expr_chrM",
-    "expr_chrM_ratio"
+    "expr_chrM_ratio",
+    "age"
   ),
-  default_cluster = "10x_graphclust"
+  default_cluster = "BayesSpace"
 )
--- code/pilot_data_checks/shinyapp/deploy.R
+++ code/pilot_data_checks/shinyapp/deploy.R
@@ -4,12 +4,12 @@ library("here")
 ## Or you can go to your shinyapps.io account and copy this
 ## Here we do this to keep our information hidden.
 
-load(here("code", "pilot_data_checks", "shinyapp", ".deploy_info.Rdata"), verbose = TRUE)
-rsconnect::setAccountInfo(
-  name = deploy_info$name,
-  token = deploy_info$token,
-  secret = deploy_info$secret
-)
+# load(here("code", "pilot_data_checks", "shinyapp", ".deploy_info.Rdata"), verbose = TRUE)
+# rsconnect::setAccountInfo(
+#   name = deploy_info$name,
+#   token = deploy_info$token,
+#   secret = deploy_info$secret
+# )
 
 ## You need this to enable shinyapps to install Bioconductor packages
 options(repos = BiocManager::repositories())
