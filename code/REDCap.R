29bc3f67859d1b962e9764cf219cb8e66527c217
madhavitippani <madhavitippani28@gmail.com>
2022-04-20 18:03:26 -0400

    make samples (slide_array) list for fastqs
    Former-commit-id: 6ff7fa564b98c58688abf2850f9d7e43f6b716b0

--- code/REDCap.R
+++ code/REDCap.R
@@ -22,4 +22,7 @@ REDCap_table = rbind(A1,B1,C1,D1)
 REDCap_HPC = REDCap_table[which(REDCap_table$project == "spatialHPC_LIBD4035"),]
 
 Brain_nums = unique(REDCap_HPC$sample)
-write.table(Brain_nums, file = (here::here("code","BrainList_for_Geo.txt")))
+write.table(Brain_nums, file = (here::here("code","BrainList_for_Geo.txt")),row.names = FALSE, col.names = FALSE)
+
+Samples = unique(paste0(REDCap_HPC$slide,"_",REDCap_HPC$array))
+write.table(Samples,file = (here::here("code","samples.txt"))
