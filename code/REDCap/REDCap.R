7188fe92e526494ce1b95e58e184f4e7c8bf5560
madhavitippani <madhavitippani28@gmail.com>
2022-04-26 10:48:59 -0400

    move REDCap form from code/REDCap/ to raw-data/sample_info/
    Former-commit-id: 6efed2ba06d1accf09d535df2ea808b6da2af35f

--- code/REDCap/REDCap.R
+++ code/REDCap/REDCap.R
@@ -1,8 +1,8 @@
 #cd /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/
 
-suppressPackageStartupMessages(library("here"))
+library("here")
 
-REDCap = read.csv(file.path(here::here("code","Visium_DATA_2022-04-20_1715.csv")),header=TRUE, stringsAsFactors=FALSE)
+REDCap = read.csv(file.path(here::here("code","REDCap","Visium_DATA_2022-04-26_0948.csv")),header=TRUE, stringsAsFactors=FALSE)
 A1 = subset(REDCap, select = c("date","slide","experimenter","species_a1","sample_a1","serial_a1","adjacent_a1",
                                "region_a1","project_a1","sample_number1_a1", "master_sheet1_a1","experimenter1_a1"))
 B1 = subset(REDCap, select = c("date","slide","experimenter","species_b1","sample_b1","serial_b1", "adjacent_b1",
@@ -24,25 +24,25 @@ REDCap_table = REDCap_table[order(REDCap_table$slide),]
 REDCap_HPC = REDCap_table[which(REDCap_table$project == "spatialHPC_LIBD4035"),]
 
 Brain_nums = unique(REDCap_HPC$sample)
-write.table(Brain_nums, file = (here::here("code","BrainList_for_Geo.txt")),row.names = FALSE, col.names = FALSE)
+write.table(Brain_nums, file = (here::here("code","REDCap","BrainList_for_Geo.txt")),row.names = FALSE, col.names = FALSE)
 
 Samples = unique(paste0(REDCap_HPC$slide,"_",REDCap_HPC$array))
-write.table(Samples,file = (here::here("code","samples.txt")),row.names = FALSE, col.names = FALSE)
+write.table(Samples,file = (here::here("code","REDCap","samples.txt")),row.names = FALSE, col.names = FALSE)
 
-save(REDCap_HPC, file = (here::here("code","REDCap_HPC.rda")))
+save(REDCap_HPC, file = (here::here("code","REDCap","REDCap_HPC.rda")))
 
 ##https://jhu-genomics.slack.com/archives/CR9NYA0BF/p1650383126365919 
 ##/dcs04/lieber/lcolladotor/rawDataTDSC_LIBD001/raw-data/2022-04-12_SPag033122/
-dir.create(here::here("raw-data","FASTQ","2022-04-12"))
+dir.create(here::here("raw-data","FASTQ","2022-04-12_SPag033122"))
 
 samples = subset(REDCap_HPC, select = c("date","slide","array","sample_number"))
 samples$date = as.Date(samples$date)
-samples = samples[samples$date > "2021-10-06",]
+samples = samples[samples$date > "2021-10-11",]
 samples$rawFASTQ = paste0("/dcs04/lieber/lcolladotor/rawDataTDSC_LIBD001/raw-data/2022-04-12_SPag033122/",samples$sample_number,"*")
-samples$softlink = paste("raw-data","FASTQ","2022-04-12", paste0(samples$slide,"_",samples$array,"/"), sep = "/")
+samples$softlink = paste("raw-data","FASTQ","2022-04-12_SPag033122", paste0(samples$slide,"_",samples$array,"/"), sep = "/")
 sapply(samples$softlink, dir.create) 
 
 temp = as.factor(paste("ln -s", samples$rawFASTQ, samples$softlink))
 
-write.table(temp,file = (here::here("raw-data","FASTQ","2022-04-12","README.md")),row.names = FALSE, col.names = FALSE, quote = FALSE)
+write.table(temp,file = (here::here("raw-data","FASTQ","2022-04-12_SPag033122","README.md")),row.names = FALSE, col.names = FALSE, quote = FALSE)
 
