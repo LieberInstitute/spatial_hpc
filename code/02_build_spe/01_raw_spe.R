0890bf70c5ddff53cf49332ab80bd78941270e17
madhavitippani <madhavitippani28@gmail.com>
2022-04-26 14:44:35 -0400

    directory for scripts to build SPE
    Former-commit-id: e0db7d1913267ffe8d5ae385164db5536dfd5a91

---
+++ code/02_build_spe/01_raw_spe.R
@@ -0,0 +1,9 @@
+
+#cd /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/
+suppressPackageStartupMessages(library("here"))
+suppressPackageStartupMessages(library("SpatialExperiment"))
+suppressPackageStartupMessages(library("spatialLIBD"))
+suppressPackageStartupMessages(library("rtracklayer"))
+suppressPackageStartupMessages(library("lobstr"))
+suppressPackageStartupMessages(library("sessioninfo"))
+
