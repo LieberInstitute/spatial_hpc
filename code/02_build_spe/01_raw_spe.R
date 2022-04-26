b5e5305f5c66ea0783d0cfdd8ad13942ee179931
madhavitippani <madhavitippani28@gmail.com>
2022-04-26 14:49:52 -0400

    extract sample info from REDCap table
    Former-commit-id: a5d75342b8fb7cf0ae04e38989a0254ff541774f

--- code/02_build_spe/01_raw_spe.R
+++ code/02_build_spe/01_raw_spe.R
@@ -7,3 +7,15 @@ suppressPackageStartupMessages(library("rtracklayer"))
 suppressPackageStartupMessages(library("lobstr"))
 suppressPackageStartupMessages(library("sessioninfo"))
 
+## Define some info for the samples
+load(here::here("code","REDCap","REDCap_HPC.rda"))
+
+sample_info = data.frame(dateImg = as.Date(REDCap_HPC$date))
+sample_info$experimenterImg = as.factor(REDCap_HPC$experimenter_img)
+sample_info$slide = as.factor(REDCap_HPC$slide)
+sample_info$array = as.factor(REDCap_HPC$array)
+sample_info$brnum = as.factor(sapply(strsplit(REDCap_HPC$sample,"-"), `[`, 1))
+sample_info$position = as.factor(REDCap_HPC$adjacent)
+sample_info$seqNum = as.factor(REDCap_HPC$sample_number)
+sample_info$experimenterSeq = as.factor(REDCap_HPC$experimenter_seq)
+sample_info$sample_id = paste(sample_info$slide,sample_info$array,sep ="_")
