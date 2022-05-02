6ff507971345795e033bd2686ffef68f256bcb1d
madhavitippani <madhavitippani28@gmail.com>
2022-05-02 09:07:23 -0400

    script to builf raw spe
    Former-commit-id: 7c324beee6efb0fa21e74229526be337056d9a40

---
+++ code/02_build_spe/02_basic_spe.R
@@ -0,0 +1,12 @@
+
+#cd /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/
+suppressPackageStartupMessages(library("here"))
+suppressPackageStartupMessages(library("SpatialExperiment"))
+suppressPackageStartupMessages(library("spatialLIBD"))
+suppressPackageStartupMessages(library("rtracklayer"))
+suppressPackageStartupMessages(library("lobstr"))
+suppressPackageStartupMessages(library("sessioninfo"))
+suppressPackageStartupMessages(library("ggplot2"))
+suppressPackageStartupMessages(library("gridExtra"))
+suppressPackageStartupMessages(library("ggspavis"))
+
