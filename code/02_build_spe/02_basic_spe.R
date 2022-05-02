87d1677c7016da44438e13a3107c05cfb1fca81a
madhavitippani <madhavitippani28@gmail.com>
2022-05-02 14:25:13 -0400

    add missing capture area position details used for sample orientation
    Former-commit-id: d78f6082765c46c26c5277956c8c87be1d312869

--- code/02_build_spe/02_basic_spe.R
+++ code/02_build_spe/02_basic_spe.R
@@ -14,38 +14,38 @@ load(file=here::here("processed-data","02_build_spe","spe_raw.Rdata"))
 
 # Rotations by sample:
 
-# V10B01-085_A1= 90
-# V10B01-085_B1= 180
-# V10B01-085_C1= 180
-# V10B01-085_D1= 180
-# V10B01-086_A1= 0
-# V10B01-086_B1= 0
-# V10B01-086_C1= 0
-# V10B01-086_D1= 180
-# V11L05-333_A1= 0
-# V11L05-333_B1= 0
-# V11L05-333_C1= 0
-# V11L05-333_D1= 0
-# V11L05-335_A1= 0
-# V11L05-335_B1= 0
-# V11L05-335_C1= 0
-# V11L05-335_D1= 0
-# V11U08-084_A1= 0
-# V11U08-084_B1= 0
-# V11U08-084_C1= 0
-# V11U08-084_D1= 0
-# V11A20-297_A1= 0
-# V11A20-297_B1= 0
-# V11A20-297_C1= 0
-# V11A20-297_D1= 0
-# V11U08-081_A1= 0
-# V11U08-081_B1= 0
-# V11U08-081_C1= 0
-# V11U08-081_D1= 0
-# V11L05-336_A1= 0
-# V11L05-336_B1= 0
-# V11L05-336_C1= 0
-# V11L05-336_D1= 0
+# V10B01-085_A1= 90, TR
+# V10B01-085_B1= 180, TL
+# V10B01-085_C1= 180, BR
+# V10B01-085_D1= 180, BL
+# V10B01-086_A1= 0, TL
+# V10B01-086_B1= 0, BL
+# V10B01-086_C1= 0, TR
+# V10B01-086_D1= 180, BR
+# V11L05-333_A1= 0, TL
+# V11L05-333_B1= 0, TR
+# V11L05-333_C1= 0, BL
+# V11L05-333_D1= 0, BR
+# V11L05-335_A1= 0, BL
+# V11L05-335_B1= 0, TR
+# V11L05-335_C1= 0, TL
+# V11L05-335_D1= 0, BR
+# V11U08-084_A1= 0, TL
+# V11U08-084_B1= 0, TR
+# V11U08-084_C1= 0, BL
+# V11U08-084_D1= 0, BR
+# V11A20-297_A1= 0, BL
+# V11A20-297_B1= 0, BR
+# V11A20-297_C1= 0, TL
+# V11A20-297_D1= 0, TR
+# V11U08-081_A1= 0, TL
+# V11U08-081_B1= 0, TR
+# V11U08-081_C1= 0, BL
+# V11U08-081_D1= 0, BR
+# V11L05-336_A1= 0, BR
+# V11L05-336_B1= 0, BL
+# V11L05-336_C1= 0, TR
+# V11L05-336_D1= 0, TL
 
 sampleID = unique(spe_raw$sample_id)
 angle_list = c(90, 180, 180, 180, 0, 0, 0, 180, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
@@ -70,3 +70,17 @@ for (i in seq_along(angle_list)){
   grid.arrange(p1,p2,ncol=2)
 }
 dev.off()
+
+slide_order = unique(spe$slide)
+sample_order <- unlist(sapply(slide_order, function(i) {
+  (unique(spe$sample_id)[grepl(i, unique(spe$sample_id))])
+}))
+sample_order
+
+spe$position = factor(spe$position, levels = c("TL","TR","BR","BL"))
+spe$sample_id = spe$samlple_id[order(spe$position)]
+
+Newsample_order <- unlist(sapply(slide_order, function(i) {
+  (unique(spe$sample_id)[grepl(i, unique(spe$sample_id))])
+}))
+Newsample_order
