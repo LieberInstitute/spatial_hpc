bdf6de4d03bacbee0eb3bd5b600dc08207a9142c
Leonardo Collado Torres <lcolladotor@gmail.com>
2022-05-20 21:35:34 -0400

    Update the subset.R script. Add more comments explaining the logic behind these steps.
    Former-commit-id: c290b9400d496bb653351b55994f3669f72a4723

--- code/03_shiny_app_basic/subset.R
+++ code/03_shiny_app_basic/subset.R
@@ -1,13 +1,25 @@
 library("spatialLIBD")
+library("lobstr")
 library("here")
 
-## Load the spe object
-
-load(file=here::here("processed-data", "02_build_spe", "spe_basic.Rdata", verbose = TRUE))
-#load("/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/02_build_spe/spe_basic.Rdata")
-lobstr::obj_size(spe) / 1024^3
+## Load the main spe object
+load(here::here("processed-data", "02_build_spe", "spe_basic.Rdata"),
+    verbose = TRUE)
+## Check how big it is in memory
+lobstr::obj_size(spe) / 1024 ^ 3
 # 4.856404 B
-imgData(spe) <- imgData(spe)[!imgData(spe)$image_id %in% c("hires", "detected", "aligned"),]
-lobstr::obj_size(spe) / 1024^3
+## That's too big for shinyapps.io. Aim to have an object near 2GB.
+
+## Subset the spe object outside of shinyapps.io. Otherwise, the peak memory is
+## still affected by loading the object.
+## Also, running lobstr::obj_size() takes a while to run, which we don't need
+## to run every time someone accesses the shiny app.
+imgData(spe) <-
+    imgData(spe)[!imgData(spe)$image_id %in% c("hires", "detected", "aligned"), ]
+lobstr::obj_size(spe) / 1024 ^ 3
 # 2.132348 B
-save(spe, file = here::here("processed-data","02_build_spe","spe_small.Rdata"))
+## Ok, this seems reasonable.
+
+## Save the reduced version of the spe object in the shiny app directory
+## instead of using soft links.
+save(spe, file = here::here("code", "03_shiny_app_basic", "spe.Rdata"))
