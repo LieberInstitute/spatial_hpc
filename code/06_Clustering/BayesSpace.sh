fef1ffca265eeed3c9b7ae062200d15b5858560d
madhavitippani <madhavitippani28@gmail.com>
2022-06-28 12:00:14 -0400

    BayesSpace scripts for k=5-20
    Former-commit-id: 07300533de6b5d717ad540557f309e1fd6f455f8

---
+++ code/06_Clustering/BayesSpace.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#$ -cwd
+#$ -l bluejay,mem_free=100G,h_vmem=100G,h_fsize=100G
+#$ -N BayesSpace_k5-20
+#$ -o logs/BayesSpace_k5-20.$TASK_ID.txt
+#$ -e logs/BayesSpace_k5-20.$TASK_ID.txt
+#$ -m e
+#$ -t 5-6
+#$ -tc 20
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
+## Load the R module (absent since the JHPCE upgrade to CentOS v7)
+module load conda_R/devel
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript BayesSpace.R
+
+echo "**** Job ends ****"
+date
+
+## This script was made using sgejobs version 0.99.1
+## available from http://research.libd.org/sgejobs/
