33896dbccd5ebe48fee1e03e38120c9b16484b8c
madhavitippani <madhavitippani28@gmail.com>
2022-06-29 15:09:36 -0400

    make anndata object for spaGCN
    Former-commit-id: 5e85bdd801deb71e9d3b87e704d1d026bddbb937

---
+++ code/06_Clustering/spe_to_anndata.R
@@ -0,0 +1,41 @@
+setwd("/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/")
+
+library('basilisk')
+library('zellkonverter')
+library('spatialLIBD')
+library('here')
+library('sessioninfo')
+
+load(file = here::here("processed-data", "05_Batch_correction", "spe_harmony.Rdata"),verbose = TRUE)
+anndata_out = here("processed-data", "06_Clustering", "spe_harmony_anndata.h5ad")
+
+#  Append 'spatialCoords' and 'spatialData' slots to 'colData', since in
+#  conversion we're treating the spatialExperiment object as if it is a
+#  singleCellExperiment, which doesn't have those additional slots.
+colData(spe) = cbind(colData(spe), spatialData(spe), spatialCoords(spe))
+
+write_anndata = function(sce, out_path) {
+  invisible(
+    basiliskRun(
+      fun = function(sce, filename) {
+        library('zellkonverter')
+        library('reticulate')
+        
+        # Convert SCE to AnnData:
+        adata <- SCE2AnnData(sce)
+        
+        #  Write AnnData object to disk
+        adata$write(filename = filename)
+        
+        return()
+      },
+      env = zellkonverterAnnDataEnv,
+      sce = sce,
+      filename = out_path
+    )
+  )
+}
+
+write_anndata(spe, anndata_out)
+
+session_info()
