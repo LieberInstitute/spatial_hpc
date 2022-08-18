8871d06e6c2a05950515da122e6ec07164d7cf90
madhavitippani <madhavitippani28@gmail.com>
2022-08-18 15:03:28 -0400

    refining based on euclidean distance
    Former-commit-id: 791f368f1bf9b4530f6ee3bebb65ff21b63594a3

--- code/06_Clustering/mbkmeans_smoothening.R
+++ code/06_Clustering/mbkmeans_smoothening.R
@@ -16,10 +16,20 @@ load(file = here::here("processed-data", "06_Clustering","spe_modify.Rdata"), ve
 
 ## load clusters
 load(file = here::here("processed-data", "06_Clustering","mbkmeans.Rdata"), verbose = TRUE)
-spe$kmeans <- km_res[[17]]$Clusters
+spe$kmeans <- km_res[[13]]$Clusters
 
 colData(spe)$imagerow = spatialCoords(spe)[,2]
 colData(spe)$imagecol = spatialCoords(spe)[,1]
 
 spec = spe[, which(spe$brnum == "Br3942")]
 spec = spatialEnhance(spec, q = 17, platform = "Visium", use.dimred = "HARMONY", nrep = 100, init = "kmeans", burn.in = 10)
+
+speb = spe[, which(spe$sample_id == "V11U08-081_C1")]
+x = spatialCoords(speb)[,1]
+y = spatialCoords(speb)[,2]
+mat = as.data.frame(cbind(x,y))
+adj = as.matrix(dist(mat, method= "euclidean", diag = TRUE, upper = TRUE))
+refined = refine(sample_id = "V11U08-081_C1", pred = speb$kmeans, dis, shape = "hexagon")
+speb$kmeans_refine = refined
+
+vis_clus(spe = speb, sampleid = "V11U08-081_C1", clustervar = "kmeans_refine", colors = cols, point_size = 2)
