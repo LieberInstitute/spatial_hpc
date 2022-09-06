e1273458ff6562ba7f0b7615be32ed8c4e1b635f
madhavitippani <madhavitippani28@gmail.com>
2022-09-06 13:53:15 -0400

    create pseudobulk spe object by brain and manually annotated regions
    Former-commit-id: 45461f9cce1e84d2bbf1a48026c9c60ef6760a4e

---
+++ code/08_pseudobulk/manual_annotations/create_pseudobulk_spe_brain.R
@@ -0,0 +1,154 @@
+setwd("/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/")
+suppressPackageStartupMessages({
+  library(SpatialExperiment)
+  library(spatialLIBD)
+  library(here)
+  library(edgeR)
+  library(scuttle)
+  library(scater)
+  library(scran)
+  library(dplyr)
+  library(PCAtools)
+  library(sessioninfo)
+  library(gridExtra)
+  library(SingleCellExperiment)
+})
+
+
+# Load SPE
+load(file = here::here("processed-data", "06_Clustering", "spe_modify.Rdata"))
+
+## Pseudo-bulk for mbkmeans k = 17 results
+sce <- as(speb, "SingleCellExperiment")
+spe_pseudo <- aggregateAcrossCells(
+  sce,
+  id=DataFrame(
+    manual_annotations = colData(sce)$kmeans,
+    brnum = colData(sce)$brnum)
+)
+
+spe_pseudo$manual_annotations <- factor(spe_pseudo$manual_annotations)
+spe_pseudo <- spe_pseudo[, spe_pseudo$ncells >= 10]
+
+#find a good expression cutoff using edgeR::filterByExpr
+rowData(spe_pseudo)$high_expr_group_br <- filterByExpr(spe_pseudo, group = spe_pseudo$brnum.1)
+rowData(spe_pseudo)$high_expr_group_cluster <- filterByExpr(spe_pseudo, group = spe_pseudo$manual_annotations)
+summary(rowData(spe_pseudo)$high_expr_group_cluster)
+summary(rowData(spe_pseudo)$high_expr_group_br)
+with(rowData(spe_pseudo), table(high_expr_group_br, high_expr_group_cluster))
+
+## Now filter
+dim(spe_pseudo)
+spe_pseudo <- spe_pseudo[rowData(spe_pseudo)$high_expr_group_cluster, ]
+dim(spe_pseudo)
+
+# Store the log normalized counts on the spe object
+x <- edgeR::cpm(edgeR::calcNormFactors(spe_pseudo), log = TRUE, prior.count = 1)
+
+# Verify that the gene order hasn't changed
+stopifnot(identical(rownames(x), rownames(spe_pseudo)))
+
+# Fix the column names. DGEList will have samples name as Sample1 Sample2 etc
+dimnames(x) <- dimnames(spe_pseudo)
+
+# Store the log normalized counts on the SingleCellExperiment object
+logcounts(spe_pseudo) <- x
+dim(spe_pseudo)
+
+rm(x)
+
+#run PCA
+pca <- prcomp(t(assays(spe_pseudo)$logcounts))
+
+message(Sys.time(), " % of variance explained for the top 20 PCs:")
+metadata(spe_pseudo)
+metadata(spe_pseudo) <- list("PCA_var_explained" = jaffelab::getPcaVars(pca)[seq_len(20)])
+metadata(spe_pseudo)
+# $PCA_var_explained
+# [1] 23.000  8.890  6.930  4.520  3.940  3.550  3.280  2.710  2.230  2.000
+# [11]  1.740  1.650  1.480  1.330  1.240  1.230  1.140  1.100  1.020  0.983
+
+pca_pseudo <- pca$x[, seq_len(50)]
+colnames(pca_pseudo) <- paste0("PC", sprintf("%02d", seq_len(ncol(pca_pseudo))))
+reducedDims(spe_pseudo) <- list(PCA = pca_pseudo)
+
+jaffelab::getPcaVars(pca)[seq_len(50)]
+# [1] 23.000  8.890  6.930  4.520  3.940  3.550  3.280  2.710  2.230  2.000
+# [11]  1.740  1.650  1.480  1.330  1.240  1.230  1.140  1.100  1.020  0.983
+# [21]  0.961  0.898  0.873  0.849  0.812  0.768  0.749  0.740  0.686  0.641
+# [31]  0.620  0.573  0.536  0.468  0.445  0.433  0.406  0.400  0.378  0.353
+# [41]  0.346  0.330  0.327  0.321  0.311  0.310  0.304  0.298  0.289  0.282
+
+# Plot PCA
+pdf(file = here::here("plots","08_pseudobulk", "manual_annotations", "pseudobulk_brain_PCA.pdf"), width = 14, height = 14)
+plotPCA(spe_pseudo, colour_by = "brnum", ncomponents = 12, point_size = 3, label_format = c("%s %02i", " (%i%%)"),
+        percentVar = metadata(spe_pseudo)$PCA_var_explained)
+plotPCA(spe_pseudo, colour_by = "manual_annotations", ncomponents = 12, point_size = 1, label_format = c("%s %02i", " (%i%%)"),
+        percentVar = metadata(spe_pseudo)$PCA_var_explained)
+plotPCA(spe_pseudo, colour_by = "sample_id", ncomponents = 12, point_size = 1, label_format = c("%s %02i", " (%i%%)"),
+        percentVar = metadata(spe_pseudo)$PCA_var_explained)
+plotPCA(spe_pseudo, colour_by = "age", ncomponents = 12, point_size = 1, label_format = c("%s %02i", " (%i%%)"),
+        percentVar = metadata(spe_pseudo)$PCA_var_explained)
+plotPCA(spe_pseudo, colour_by = "sex", ncomponents = 12, point_size = 1, label_format = c("%s %02i", " (%i%%)"),
+        percentVar = metadata(spe_pseudo)$PCA_var_explained)
+dev.off()
+
+pdf(file = here::here("plots","08_pseudobulk", "manual_annotations", "pseudobulk_brain_PCA_2.pdf"), width = 14, height = 14)
+plotPCA(spe_pseudo, colour_by = "brnum", ncomponents = 2, point_size = 8, label_format = c("%s %02i", " (%i%%)"),
+        percentVar = metadata(spe_pseudo)$PCA_var_explained)
+plotPCA(spe_pseudo, colour_by = "manual_annotationss", ncomponents = 2, point_size = 8, label_format = c("%s %02i", " (%i%%)"),
+        percentVar = metadata(spe_pseudo)$PCA_var_explained)
+plotPCA(spe_pseudo, colour_by = "age", ncomponents = 2, point_size = 8, label_format = c("%s %02i", " (%i%%)"),
+        percentVar = metadata(spe_pseudo)$PCA_var_explained)
+plotPCA(spe_pseudo, colour_by = "sex", ncomponents = 2, point_size = 8, label_format = c("%s %02i", " (%i%%)"),
+        percentVar = metadata(spe_pseudo)$PCA_var_explained)
+plotPCA(spe_pseudo, colour_by = "sample_id", ncomponents = 2, point_size = 8, label_format = c("%s %02i", " (%i%%)"),
+        percentVar = metadata(spe_pseudo)$PCA_var_explained)
+dev.off()
+
+pdf(file = here::here("plots","08_pseudobulk", "manual_annotations", "pseudobulk_brain_PCA_4.pdf"), width = 14, height = 14)
+plotPCA(spe_pseudo, colour_by = "brnum", ncomponents = 4, point_size = 4, label_format = c("%s %02i", " (%i%%)"),
+        percentVar = metadata(spe_pseudo)$PCA_var_explained)
+plotPCA(spe_pseudo, colour_by = "manual_annotations", ncomponents = 4, point_size = 4, label_format = c("%s %02i", " (%i%%)"),
+        percentVar = metadata(spe_pseudo)$PCA_var_explained)
+plotPCA(spe_pseudo, colour_by = "age", ncomponents = 4, point_size = 4, label_format = c("%s %02i", " (%i%%)"),
+        percentVar = metadata(spe_pseudo)$PCA_var_explained)
+plotPCA(spe_pseudo, colour_by = "sex", ncomponents = 4, point_size = 4, label_format = c("%s %02i", " (%i%%)"),
+        percentVar = metadata(spe_pseudo)$PCA_var_explained)
+plotPCA(spe_pseudo, colour_by = "sample_id", ncomponents = 4, point_size = 4, label_format = c("%s %02i", " (%i%%)"),
+        percentVar = metadata(spe_pseudo)$PCA_var_explained)
+
+dev.off()
+
+## Compute some reduced dims
+set.seed(20220423)
+spe_pseudo <- scater::runMDS(spe_pseudo, ncomponents = 20)
+spe_pseudo <- scater::runPCA(spe_pseudo, name = "runPCA")
+
+####plot explanatory variables ####
+
+#uses linear regression model
+vars <- getVarianceExplained(spe_pseudo, variables=c("manual_annotations","brnum","age","sex"))
+head(vars)
+
+# mbkmeans     brnum         age       sex
+# ENSG00000241860 29.016831 22.786306 0.102747369 3.7426821
+# ENSG00000237491 16.659154 14.788938 0.222504868 2.1107453
+# ENSG00000228794 42.612793  4.352146 0.009069138 1.1719999
+# ENSG00000230368  9.952576 37.512726 5.291249484 0.9650065
+# ENSG00000223764 43.530548  4.313264 0.158042235 1.8903882
+# ENSG00000187634 48.375580  8.983505 1.693109811 4.4296255
+
+pdf(file = here::here("plots","08_pseudobulk", "manual_annotations", "plot_explanatory_vars_brain.pdf"))
+plotExplanatoryVariables(vars)
+dev.off()
+
+# save file
+save(spe_pseudo, file = here::here("processed-data", "08_pseudobulk", "manual_annotations", "spe_pseudo_brain.Rdata"))
+
+## Reproducibility information
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
