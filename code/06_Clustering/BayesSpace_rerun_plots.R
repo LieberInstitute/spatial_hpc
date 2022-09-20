369cc4544057764cded70f8beca55e756250b1e4
madhavitippani <madhavitippani28@gmail.com>
2022-09-20 14:13:45 -0400

    mclust/initia.cluster and BayesSpace/spatial.cluster plots from BayesSpace rerun k15, long array
    Former-commit-id: e8e055e3d34ddf65489a4e3be07ca2851f63c124

---
+++ code/06_Clustering/BayesSpace_rerun_plots.R
@@ -0,0 +1,148 @@
+setwd("/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/")
+suppressPackageStartupMessages({
+  library(here)
+  library(sessioninfo)
+  library(SpatialExperiment)
+  library(spatialLIBD)
+  library(BayesSpace)
+  library(ggplot2)
+  library(Polychrome)
+  library(tidySingleCellExperiment)
+})
+suppressPackageStartupMessages(library("ggspavis"))
+suppressPackageStartupMessages(library("gridExtra"))
+
+load(file = here::here("processed-data", "06_Clustering", "BayesSpace_rerun_k15.Rdata"))
+
+k <- 15
+cols <- Polychrome::palette36.colors(k)
+names(cols) <- sort(unique(spe$spatial.cluster))
+clustV = "cluster.init"
+
+brains = c("Br6423","Br6432","Br2743","Br8325","Br3942","Br6471","Br8667","Br8492","Br6522")
+
+pdf(here("plots", "06_Clustering", "BayesSpace", "BayesSpace_rerun_K15_mCluster.pdf"), width = 21, height = 20)
+# QC plot of tissue spots discarded
+
+ii <- 1
+speb <- spe[, which(spe$brnum == brains[ii])]
+samples <- unique(speb$sample_id)
+samples
+p1 <- vis_clus(spe = speb, sampleid = samples[1], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p2 <- vis_clus(spe = speb, sampleid = samples[2], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p3 <- vis_clus(spe = speb, sampleid = samples[3], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p4 <- vis_clus(spe = speb, sampleid = samples[4], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+
+grid.arrange(p1, p2, p3, p4, nrow = 2)
+
+##
+ii <- 2
+speb <- spe[, which(spe$brnum == brains[ii])]
+samples <- unique(speb$sample_id)
+samples
+
+p1 <- vis_clus(spe = speb, sampleid = samples[1], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p2 <- vis_clus(spe = speb, sampleid = samples[2], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p3 <- vis_clus(spe = speb, sampleid = samples[1], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p4 <- vis_clus(spe = speb, sampleid = samples[2], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+
+grid.arrange(p1, p3, p2, p4, nrow = 2)
+
+##
+ii <- 3
+speb <- spe[, which(spe$brnum == brains[ii])]
+samples <- unique(speb$sample_id)
+samples
+
+p1 <- vis_clus(spe = speb, sampleid = samples[3], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p2 <- vis_clus(spe = speb, sampleid = samples[1], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p3 <- vis_clus(spe = speb, sampleid = samples[3], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p4 <- vis_clus(spe = speb, sampleid = samples[1], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+
+grid.arrange(p1, p2, p3, p4, nrow = 2)
+
+p1 <- vis_clus(spe = speb, sampleid = samples[2], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p2 <- vis_clus(spe = speb, sampleid = samples[4], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p3 <- vis_clus(spe = speb, sampleid = samples[2], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p4 <- vis_clus(spe = speb, sampleid = samples[4], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+
+grid.arrange(p1, p3, p2, p4, nrow = 2)
+
+##
+ii <- 4
+speb <- spe[, which(spe$brnum == brains[ii])]
+samples <- unique(speb$sample_id)
+samples
+
+p1 <- vis_clus(spe = speb, sampleid = samples[1], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p2 <- vis_clus(spe = speb, sampleid = samples[2], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p3 <- vis_clus(spe = speb, sampleid = samples[5], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p4 <- vis_clus(spe = speb, sampleid = samples[3], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p5 <- vis_clus(spe = speb, sampleid = samples[4], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+grid.arrange(p1, p2, p3, p4, p5, nrow = 2)
+
+##
+ii <- 5
+speb <- spe[, which(spe$brnum == brains[ii])]
+samples <- unique(speb$sample_id)
+samples
+
+p1 <- vis_clus(spe = speb, sampleid = samples[1], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p2 <- vis_clus(spe = speb, sampleid = samples[2], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p3 <- vis_clus(spe = speb, sampleid = samples[3], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p4 <- vis_clus(spe = speb, sampleid = samples[4], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+
+grid.arrange(p1, p2, p3, p4, nrow = 2)
+
+##
+ii <- 6
+speb <- spe[, which(spe$brnum == brains[ii])]
+samples <- unique(speb$sample_id)
+samples
+
+p1 <- vis_clus(spe = speb, sampleid = samples[1], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p2 <- vis_clus(spe = speb, sampleid = samples[2], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p3 <- vis_clus(spe = speb, sampleid = samples[3], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+
+grid.arrange(p1, p2, p3, nrow = 2)
+
+##
+ii <- 7
+speb <- spe[, which(spe$brnum == brains[ii])]
+samples <- unique(speb$sample_id)
+samples
+
+p1 <- vis_clus(spe = speb, sampleid = samples[2], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p2 <- vis_clus(spe = speb, sampleid = samples[1], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p3 <- vis_clus(spe = speb, sampleid = samples[4], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p4 <- vis_clus(spe = speb, sampleid = samples[3], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+
+grid.arrange(p1, p2, p3, p4, nrow = 2)
+
+##
+ii <- 8
+speb <- spe[, which(spe$brnum == brains[ii])]
+samples <- unique(speb$sample_id)
+samples
+
+p1 <- vis_clus(spe = speb, sampleid = samples[1], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p2 <- vis_clus(spe = speb, sampleid = samples[2], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p3 <- vis_clus(spe = speb, sampleid = samples[1], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p4 <- vis_clus(spe = speb, sampleid = samples[2], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+
+grid.arrange(p1, p2, p3, p4, nrow = 2)
+
+##
+ii <- 9
+speb <- spe[, which(spe$brnum == brains[ii])]
+samples <- unique(speb$sample_id)
+samples
+
+p1 <- vis_clus(spe = speb, sampleid = samples[1], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p2 <- vis_clus(spe = speb, sampleid = samples[2], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p3 <- vis_clus(spe = speb, sampleid = samples[3], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+p4 <- vis_clus(spe = speb, sampleid = samples[4], clustervar = clustV, colors = cols, point_size = 2, ... = paste0("_", brains[ii]))
+
+grid.arrange(p1, p2, p3, p4, nrow = 2)
+
+dev.off()
