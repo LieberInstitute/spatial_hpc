a011432af07d57a46a057d9a1e3c137b0ae2b80a
madhavitippani <madhavitippani28@gmail.com>
2022-09-28 09:46:52 -0400

    reduce memory used for watershed
    Former-commit-id: dc216a3db5b79a3b55560850535411a6f8be3ea9

--- code/VistoSeg/code/WS.sh
+++ code/VistoSeg/code/WS.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -cwd
-#$ -l mem_free=20G,h_vmem=20G,h_fsize=100G
+#$ -l mem_free=10G,h_vmem=10G,h_fsize=100G
 #$ -pe local 4
 #$ -o /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/VistoSeg/code/logs/WS.$TASK_ID.txt
 #$ -e /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/VistoSeg/code/logs/WS.$TASK_ID.txt
