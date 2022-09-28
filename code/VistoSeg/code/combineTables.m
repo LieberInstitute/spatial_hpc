5915445106a455c02ca80fdbb1698e56d1634ec8
madhavitippani <madhavitippani28@gmail.com>
2022-09-28 14:43:54 -0400

    combine metrics after watershed
    Former-commit-id: 28cea9cbc4092c8c28c248492feb48caa6cea2f2

--- code/VistoSeg/code/combineTables.m
+++ code/VistoSeg/code/combineTables.m
@@ -1,9 +1,9 @@
 disp('loading data')
 tic
-%fname =
-%'/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/Images/VistoSeg/Capture_areas/*.csv';
+fname = '/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/Images/VistoSeg/Capture_areas/*.csv';
 
 myfiles = dir(fname);
+length(myfiles)
 
 T = cell2table(cell(0,16), 'VariableNames', {'Area', 'Centroid_1', 'Centroid_2', 'BoundingBox_1', 'BoundingBox_2', 'BoundingBox_3', 'BoundingBox_4', 'MajorAxisLength', 'MinorAxisLength', 'Eccentricity', 'Circularity', 'Perimeter', 'WeightedCentroid_1', 'WeightedCentroid_2', 'MeanIntensity', 'name'});
 
@@ -14,5 +14,5 @@ T1.name = repmat(myfiles(i).name,height(T1),1);
 T = [T;T1];
 end
 
-writetable(T,fullfile(myfiles(1).folder,'combined_refineVNS_metric.csv'))
+writetable(T,fullfile(myfiles(1).folder,'combined_refineVNS_metric_WS.csv'))
 
