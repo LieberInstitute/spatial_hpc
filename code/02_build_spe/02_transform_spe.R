181ad57173af711e1fd0f10effa247d923add1d9
madhavitippani <madhavitippani28@gmail.com>
2022-10-05 15:58:01 -0400

    perform rearrangement of samples 1st and the rotations according to the reaarangement
    Former-commit-id: b0922c81013fbad5ed35ece2a423876063e5a84e

--- code/02_build_spe/02_transform_spe.R
+++ code/02_build_spe/02_transform_spe.R
@@ -17,6 +17,7 @@ dim(spe_raw)
 # [1]  36601 159744
 
 sampleID <- unique(spe_raw$sample_id)
+sampleID
 # [1] "V10B01-086_C1" "V10B01-086_D1" "V11U08-081_C1" "V11U08-081_D1"
 # [5] "V11L05-333_A1" "V11L05-333_B1" "V11L05-333_C1" "V11L05-333_D1"
 # [9] "V10B01-085_A1" "V10B01-085_B1" "V10B01-085_C1" "V10B01-085_D1"
@@ -26,127 +27,61 @@ sampleID <- unique(spe_raw$sample_id)
 # [25] "V11A20-297_D1" "V11L05-335_D1" "V11U08-081_A1" "V11U08-081_B1"
 # [29] "V11L05-336_A1" "V11L05-336_B1" "V11L05-336_C1" "V11L05-336_D1"
 
-# Rotations by sample:
-angle_list <- c(0, 180, 0, 0,
-                0, 0, 0, 0,
-                90, 180, 180, 180, 
-                0, 0, 0, 0, 
-                0, 0, 0, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0)
-source(file = here::here("code", "02_build_spe", "transform_spe.R"))
+colnames(spe_raw) <- spe_raw$key
+rownames(spatialCoords(spe_raw)) <- spe_raw$key
 
-for (i in seq_along(angle_list)) {
-    id <- sampleID[i]
-    x <- trans_geom(spe_raw, sample_id = id, degrees = angle_list[i])
+## arrange by brain and array (A1, B1, C1, D1)
+spe_raw <- arrange(spe_raw, brnum, array)
 
-    if (i == 1) {
-        spe <- x
-    } else {
-        spe <- cbind(spe, x)
-    }
+samples <- unique(spe_raw$sample_id)
+samples
+# [1] "V10B01-086_C1" "V11U08-081_C1" "V10B01-086_D1" "V11U08-081_D1"
+# [5] "V11L05-333_A1" "V11L05-333_B1" "V11L05-333_C1" "V11L05-333_D1"
+# [9] "V10B01-085_A1" "V10B01-085_B1" "V10B01-085_C1" "V10B01-085_D1"
+# [13] "V10B01-086_A1" "V10B01-086_B1" "V11L05-335_A1" "V11L05-335_B1"
+# [17] "V11L05-335_C1" "V11U08-084_A1" "V11U08-084_B1" "V11U08-084_C1"
+# [21] "V11U08-084_D1" "V11A20-297_A1" "V11A20-297_B1" "V11A20-297_C1"
+# [25] "V11A20-297_D1" "V11L05-335_D1" "V11U08-081_A1" "V11U08-081_B1"
+# [29] "V11L05-336_A1" "V11L05-336_B1" "V11L05-336_C1" "V11L05-336_D1"
 
-    spe <- rotateImg(spe, sample_id = id, image_id = TRUE, angle_list[i])
-   }
+brains = unique(spe_raw$brnum)
+pdf(here("plots", "02_build_spe", "prearrangedSamples.pdf"), width = 8, height = 10)
 
-
-pdf(file = here::here("plots", "02_build_spe", "ReferenceMapping1.pdf"), h = 10, w = 20)
-for (i in seq_along(sampleID)) {
-    id <- sampleID[i]
-    x_raw = spe_raw[, (colData(spe_raw)$in_tissue & colData(spe_raw)$sample_id == id)]
-    x_trans = spe[, (colData(spe)$in_tissue & colData(spe)$sample_id == id)]
-    p1 <- plotVisium(x_raw, spots = TRUE, y_reverse = TRUE)
-    p2 <- plotVisium(x_trans, spots = TRUE, y_reverse = TRUE)
-    grid.arrange(p1, p2, ncol = 2)
+for (i in seq_along(brains)){
+    speb <- spe_raw[, which(spe_raw$brnum == brains[i])]
+    samples <- unique(speb$sample_id)
+    samples
     
-    df_raw <- cbind.data.frame(colData(x_raw), spatialCoords(x_raw))
-    df_trans <- cbind.data.frame(colData(x_trans), spatialCoords(x_trans))
-    p3 = ggplot(df_raw, aes(x = array_row, y = array_col, color = sample_id)) + 
-         geom_point(size = 1) + coord_fixed() + theme_bw()
-    p4 = ggplot(df_trans, aes(x = array_row, y = array_col, color = sample_id)) + 
-        geom_point(size = 1) + coord_fixed() + theme_bw() 
-    
-    grid.arrange(p3, p4, ncol = 2)
+    if (length(samples) == 1){
+        p1 <- plotVisium(speb[, which(speb$sample_id == samples[1])], spots = FALSE)
+    } else if (length(samples) == 2){
+        p1 <- plotVisium(speb[, which(speb$sample_id == samples[1])], spots = FALSE)
+        p2 <- plotVisium(speb[, which(speb$sample_id == samples[2])], spots = FALSE)
+        grid.arrange(p1, p2, nrow = 2)
+    } else if (length(samples) == 3){
+        p1 <- plotVisium(speb[, which(speb$sample_id == samples[1])], spots = FALSE)
+        p2 <- plotVisium(speb[, which(speb$sample_id == samples[2])], spots = FALSE)
+        p3 <- plotVisium(speb[, which(speb$sample_id == samples[3])], spots = FALSE)
+        grid.arrange(p1, p2, p3, nrow = 2)
+    } else if (length(samples) == 4){
+        p1 <- plotVisium(speb[, which(speb$sample_id == samples[1])], spots = FALSE)
+        p2 <- plotVisium(speb[, which(speb$sample_id == samples[2])], spots = FALSE)
+        p3 <- plotVisium(speb[, which(speb$sample_id == samples[3])], spots = FALSE)
+        p4 <- plotVisium(speb[, which(speb$sample_id == samples[4])], spots = FALSE)
+        grid.arrange(p1, p2, p3, p4, nrow = 2)
+    } else if (length(samples) == 5){
+        p1 <- plotVisium(speb[, which(speb$sample_id == samples[1])], spots = FALSE)
+        p2 <- plotVisium(speb[, which(speb$sample_id == samples[2])], spots = FALSE)
+        p3 <- plotVisium(speb[, which(speb$sample_id == samples[3])], spots = FALSE)
+        p4 <- plotVisium(speb[, which(speb$sample_id == samples[4])], spots = FALSE)
+        p5 <- plotVisium(speb[, which(speb$sample_id == samples[5])], spots = FALSE)
+        grid.arrange(p1, p2, p3, p4, p5, nrow = 2)}
 }
+
 dev.off()
 
-table(spe$sample_id,spe$position)
+table(spe_raw$sample_id,spe_raw$brnum)
 
-#                      BL   BR    N   TL   TR
-# V10B01-085_A1 4992    0    0    0    0    0
-# V10B01-085_B1 4992    0    0    0    0    0
-# V10B01-085_C1 4992    0    0    0    0    0
-# V10B01-085_D1 4992    0    0    0    0    0
-# V10B01-086_A1    0    0    0    0 4992    0
-# V10B01-086_B1    0 4992    0    0    0    0
-# V10B01-086_C1    0    0    0    0    0 4992
-# V10B01-086_D1    0    0 4992    0    0    0
-# V11A20-297_A1    0 4992    0    0    0    0
-# V11A20-297_B1    0    0 4992    0    0    0
-# V11A20-297_C1    0    0    0    0 4992    0
-# V11A20-297_D1    0    0    0    0    0 4992
-# V11L05-333_A1    0    0    0    0 4992    0
-# V11L05-333_B1    0    0    0    0    0 4992
-# V11L05-333_C1    0 4992    0    0    0    0
-# V11L05-333_D1    0    0 4992    0    0    0
-# V11L05-335_A1    0    0 4992    0    0    0
-# V11L05-335_B1    0    0    0    0    0 4992
-# V11L05-335_C1    0    0    0    0 4992    0
-# V11L05-335_D1    0    0    0 4992    0    0
-# V11L05-336_A1    0    0 4992    0    0    0
-# V11L05-336_B1    0 4992    0    0    0    0
-# V11L05-336_C1    0    0    0    0    0 4992
-# V11L05-336_D1    0    0    0    0 4992    0
-# V11U08-081_A1    0    0    0    0 4992    0
-# V11U08-081_B1    0    0    0    0    0 4992
-# V11U08-081_C1    0    0    0    0 4992    0
-# V11U08-081_D1    0    0    0    0    0 4992
-# V11U08-084_A1    0    0    0    0 4992    0
-# V11U08-084_B1    0    0    0    0    0 4992
-# V11U08-084_C1    0 4992    0    0    0    0
-# V11U08-084_D1    0    0 4992    0    0    0
-
-unique(spe$sample_id[which(spe$position == "N")])
-spe$position[which(spe$sample_id == "V11L05-335_D1")] <- "BR"
-
-unique(spe$sample_id[which(spe$position == "")])
-spe$position[which(spe$sample_id == "V10B01-085_A1")] <- "TR"
-spe$position[which(spe$sample_id == "V10B01-085_B1")] <- "TL"
-spe$position[which(spe$sample_id == "V10B01-085_C1")] <- "BR"
-spe$position[which(spe$sample_id == "V10B01-085_D1")] <- "BL"
-
-colnames(spe) <- spe$key
-rownames(spatialCoords(spe)) <- spe$key
-
-## arrange by slide_array (A1, B1, C1, D1)
-spe <- arrange(spe, slide, array)
-unique(spe$sample_id)
-# [1] "V10B01-085_A1" "V10B01-085_B1" "V10B01-085_C1" "V10B01-085_D1"
-# [5] "V10B01-086_A1" "V10B01-086_B1" "V10B01-086_C1" "V10B01-086_D1"
-# [9] "V11A20-297_A1" "V11A20-297_B1" "V11A20-297_C1" "V11A20-297_D1"
-# [13] "V11L05-333_A1" "V11L05-333_B1" "V11L05-333_C1" "V11L05-333_D1"
-# [17] "V11L05-335_A1" "V11L05-335_B1" "V11L05-335_C1" "V11L05-335_D1"
-# [21] "V11L05-336_A1" "V11L05-336_B1" "V11L05-336_C1" "V11L05-336_D1"
-# [25] "V11U08-081_A1" "V11U08-081_B1" "V11U08-081_C1" "V11U08-081_D1"
-# [29] "V11U08-084_A1" "V11U08-084_B1" "V11U08-084_C1" "V11U08-084_D1"
-
-spe$position <- factor(spe$position, levels = c("TL", "TR", "BL", "BR"))
-
-## now arrange() to get the correct order
-spe <- arrange(spe, brnum, position)
-
-unique(spe$sample_id)
-# [1] "V11U08-081_C1" "V10B01-086_C1" "V11U08-081_D1" "V10B01-086_D1"
-# [5] "V11L05-333_A1" "V11L05-333_B1" "V11L05-333_C1" "V11L05-333_D1"
-# [9] "V10B01-085_B1" "V10B01-085_A1" "V10B01-085_D1" "V10B01-085_C1"
-# [13] "V10B01-086_A1" "V10B01-086_B1" "V11L05-335_C1" "V11L05-335_B1"
-# [17] "V11L05-335_A1" "V11U08-084_A1" "V11U08-084_B1" "V11U08-084_C1"
-# [21] "V11U08-084_D1" "V11A20-297_C1" "V11A20-297_D1" "V11A20-297_A1"
-# [25] "V11A20-297_B1" "V11L05-335_D1" "V11U08-081_A1" "V11U08-081_B1"
-# [29] "V11L05-336_D1" "V11L05-336_C1" "V11L05-336_B1" "V11L05-336_A1"
-
-table(spe$sample_id, spe$brnum)
 #                   Br2743 Br3942 Br6423 Br6432 Br6471 Br6522 Br8325 Br8492 Br8667
 # V10B01-085_A1      0      0   4992      0      0      0      0      0      0
 # V10B01-085_B1      0      0   4992      0      0      0      0      0      0
@@ -181,6 +116,136 @@ table(spe$sample_id, spe$brnum)
 # V11U08-084_C1      0      0      0      0      0   4992      0      0      0
 # V11U08-084_D1      0      0      0      0      0   4992      0      0      0
 
+samples = unique(spe_raw$sample_id)
+# Br2743 [1] "V10B01-086_C1" "V11U08-081_C1" "V10B01-086_D1" "V11U08-081_D1"
+# Br3942 [5] "V11L05-333_A1" "V11L05-333_B1" "V11L05-333_C1" "V11L05-333_D1"
+# Br6423 [9] "V10B01-085_A1" "V10B01-085_B1" "V10B01-085_C1" "V10B01-085_D1"
+# Br6432 [13] "V10B01-086_A1" "V10B01-086_B1" 
+# Br6471 [15] "V11L05-335_A1" "V11L05-335_B1" "V11L05-335_C1" 
+# Br6522 [18] "V11U08-084_A1" "V11U08-084_B1" "V11U08-084_C1" "V11U08-084_D1" 
+# Br8325 [22] "V11A20-297_A1" "V11A20-297_B1" "V11A20-297_C1" "V11A20-297_D1"  "V11L05-335_D1" 
+# Br8492 [27] "V11U08-081_A1" "V11U08-081_B1"
+# Br8667 [29] "V11L05-336_A1" "V11L05-336_B1" "V11L05-336_C1" "V11L05-336_D1"
+
+position_list <- c("TR", "BL", "TL", "BR",
+                   "TL", "TR", "BL", "BR",
+                   "TR", "TL", "BR", "BL",
+                   "TL", "BL",
+                   "BL", "TR", "TL",
+                   "TL", "TR", "BL", "BR",
+                   "BL", "BR", "TL", "TR", "TR",
+                   "TL", "TR",
+                   "TL", "TR", "BL", "BR")
+
+for (i in seq_along(samples)){
+    spe_raw$position[colData(spe_raw)$sample_id == samples[i]] = position_list[i]
+}
+
+spe_raw$position <- factor(spe_raw$position, levels = c("TL", "TR", "BL", "BR"))
+
+## now arrange() to get the correct order
+spe_raw <- arrange(spe_raw, brnum, position)
+
+brains = unique(spe_raw$brnum)
+pdf(here("plots", "02_build_spe", "rearrangedSamples.pdf"), width = 8, height = 10)
+
+for (i in seq_along(brains)){
+speb <- spe_raw[, which(spe_raw$brnum == brains[i])]
+samples <- unique(speb$sample_id)
+samples
+
+if (length(samples) == 1){
+    p1 <- plotVisium(speb[, which(speb$sample_id == samples[1])], spots = FALSE)
+} else if (length(samples) == 2){
+    p1 <- plotVisium(speb[, which(speb$sample_id == samples[1])], spots = FALSE)
+    p2 <- plotVisium(speb[, which(speb$sample_id == samples[2])], spots = FALSE)
+    grid.arrange(p1, p2, nrow = 2)
+} else if (length(samples) == 3){
+    p1 <- plotVisium(speb[, which(speb$sample_id == samples[1])], spots = FALSE)
+    p2 <- plotVisium(speb[, which(speb$sample_id == samples[2])], spots = FALSE)
+    p3 <- plotVisium(speb[, which(speb$sample_id == samples[3])], spots = FALSE)
+    grid.arrange(p1, p2, p3, nrow = 2)
+} else if (length(samples) == 4){
+    p1 <- plotVisium(speb[, which(speb$sample_id == samples[1])], spots = FALSE)
+    p2 <- plotVisium(speb[, which(speb$sample_id == samples[2])], spots = FALSE)
+    p3 <- plotVisium(speb[, which(speb$sample_id == samples[3])], spots = FALSE)
+    p4 <- plotVisium(speb[, which(speb$sample_id == samples[4])], spots = FALSE)
+    grid.arrange(p1, p2, p3, p4, nrow = 2)
+} else if (length(samples) == 5){
+    p1 <- plotVisium(speb[, which(speb$sample_id == samples[1])], spots = FALSE)
+    p2 <- plotVisium(speb[, which(speb$sample_id == samples[2])], spots = FALSE)
+    p3 <- plotVisium(speb[, which(speb$sample_id == samples[3])], spots = FALSE)
+    p4 <- plotVisium(speb[, which(speb$sample_id == samples[4])], spots = FALSE)
+    p5 <- plotVisium(speb[, which(speb$sample_id == samples[5])], spots = FALSE)
+    grid.arrange(p1, p2, p3, p4, p5, nrow = 2)}
+}
+
+dev.off()
+
+samples = unique(spe_raw$sample_id)
+# Rotations by sample:
+angle_list <- c(270, 90, 0, 0,
+                0, 0, 0, 0,
+                180, 90, 180, 180, 
+                0, 0,
+                0, 0, 0, 
+                0, 0, 0, 0,
+                0, 0, 0, 0, 0,
+                90, 90, 
+                0, 0, 0, 0)
+source(file = here::here("code", "02_build_spe", "transform_spe.R"))
+
+for (i in seq_along(angle_list)) {
+    id <- sampleID[i]
+    x <- trans_geom(spe_raw, sample_id = id, degrees = angle_list[i])
+
+    if (i == 1) {
+        spe <- x
+    } else {
+        spe <- cbind(spe, x)
+    }
+
+    spe <- rotateImg(spe, sample_id = id, image_id = TRUE, angle_list[i])
+   }
+
+
+pdf(file = here::here("plots", "02_build_spe", "referenceMapping.pdf"), h = 10, w = 20)
+for (i in seq_along(sampleID)) {
+    id <- sampleID[i]
+    x_raw = spe_raw[, (colData(spe_raw)$in_tissue & colData(spe_raw)$sample_id == id)]
+    x_trans = spe[, (colData(spe)$in_tissue & colData(spe)$sample_id == id)]
+    p1 <- plotVisium(x_raw, spots = TRUE, y_reverse = TRUE)
+    p2 <- plotVisium(x_trans, spots = TRUE, y_reverse = TRUE)
+    grid.arrange(p1, p2, ncol = 2)
+    
+    df_raw <- cbind.data.frame(colData(x_raw), spatialCoords(x_raw))
+    df_trans <- cbind.data.frame(colData(x_trans), spatialCoords(x_trans))
+    p3 = ggplot(df_raw, aes(x = array_row, y = array_col, color = sample_id)) + 
+         geom_point(size = 1) + coord_fixed() + theme_bw()
+    p4 = ggplot(df_trans, aes(x = array_row, y = array_col, color = sample_id)) + 
+        geom_point(size = 1) + coord_fixed() + theme_bw() 
+    
+    grid.arrange(p3, p4, ncol = 2)
+}
+dev.off()
+
+
+
+unique(spe$sample_id)
+# [1] "V11U08-081_C1" "V10B01-086_C1" "V11U08-081_D1" "V10B01-086_D1"
+# [5] "V11L05-333_A1" "V11L05-333_B1" "V11L05-333_C1" "V11L05-333_D1"
+# [9] "V10B01-085_B1" "V10B01-085_A1" "V10B01-085_D1" "V10B01-085_C1"
+# [13] "V10B01-086_A1" "V10B01-086_B1" "V11L05-335_C1" "V11L05-335_B1"
+# [17] "V11L05-335_A1" "V11U08-084_A1" "V11U08-084_B1" "V11U08-084_C1"
+# [21] "V11U08-084_D1" "V11A20-297_C1" "V11A20-297_D1" "V11A20-297_A1"
+# [25] "V11A20-297_B1" "V11L05-335_D1" "V11U08-081_A1" "V11U08-081_B1"
+# [29] "V11L05-336_D1" "V11L05-336_C1" "V11L05-336_B1" "V11L05-336_A1"
+
+
+
+
+#########
+
 ## Size in Gb
 lobstr::obj_size(spe)
 # 5.53 GB
@@ -189,7 +254,6 @@ dim(spe)
 save(spe, file = here::here("processed-data", "02_build_spe", "spe_transform.Rdata"))
 
 ## Reproducibility information
-# print("Reproducibility information:")
 # Sys.time
 # "2022-10-05 12:48:52 EDT"
 # proc.time()
