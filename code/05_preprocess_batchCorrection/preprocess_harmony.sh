54098fede92a9ab14a1f9b90cb3edff979525577
madhavitippani <madhavitippani28@gmail.com>
2022-10-11 08:32:21 -0400

    qsub OSCApreprocess_harmony_brain
    Former-commit-id: b40b90a148f768ca12bc85ea0bf773bcafc8484d

--- code/05_preprocess_batchCorrection/preprocess_harmony.sh
+++ code/05_preprocess_batchCorrection/preprocess_harmony.sh
@@ -2,9 +2,9 @@
 #$ -cwd
 #$ -l bluejay,mem_free=10G,h_vmem=10G,h_fsize=100G
 #$ -pe local 8
-#$ -N HPC_harmony
-#$ -o /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/05_Batch_correction/logs/harmony.txt
-#$ -e /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/05_Batch_correction/logs/harmony.txt
+#$ -N OSCApreprocess_harmony_brain
+#$ -o /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/05_preprocess_batchCorrection/logs/OSCApreprocess_harmony_brain.txt
+#$ -e /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/05_preprocess_batchCorrection/logs/OSCApreprocess_harmony_brain.txt
 #$ -m e
 
 echo "**** Job starts ****"
@@ -21,4 +21,4 @@ echo "Task id: ${SGE_TASK_ID}"
 module list
 
 module load conda_R/devel
-Rscript harmony.R
+Rscript OSCApreprocess_harmony_brain.R
