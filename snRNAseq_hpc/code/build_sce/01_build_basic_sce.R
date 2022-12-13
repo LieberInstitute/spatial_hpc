f7c81aea0de531d5929ce4a8b72f69c1f949b48e
Erik Nelson <erik.nelson116@gmail.com>
2022-12-13 17:37:10 -0500

    adding draft scripts to build sce and perform emptydrops
    Former-commit-id: 18d211eaa38d2e5c2ca6109ffbd3619c9437dbe3

---
+++ snRNAseq_hpc/code/build_sce/01_build_basic_sce.R
@@ -0,0 +1,106 @@
+# qrsh -l bluejay,mem_free=80G,h_vmem=80G
+# cd /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/
+
+library("SingleCellExperiment")
+library("DropletUtils")
+library("here")
+library("rtracklayer")
+library("lobstr")
+library("sessioninfo")
+library("dplyr")
+
+## Define some info for the samples
+tmp <- read.delim(here("snRNA_seq","raw-data","sample_info",
+                  "snRNAseq_U01_HPC_AllRounds_Master_Spreadsheet_12012022.csv"),
+                  header = T,sep=',')
+
+##Remove excluded samples
+tmp<-tmp[c(1:2,10:27),]
+
+##set up sample data table
+sample_info <- data.frame(
+  sample_ID = tmp[,1],
+  sort = tmp[,2],
+  BrNum = tmp[,3],
+  round = tmp[,4]
+)
+
+stopifnot(all(!duplicated(sample_info$Sample)))
+
+sample_info$sample_path <- file.path(
+  here::here("snRNAseq_hpc","processed-data", "cellranger"),
+  sample_info$sample_ID,
+  "outs",
+  "raw_feature_bc_matrix"
+)
+stopifnot(all(file.exists(sample_info$sample_path)))
+
+## Get the rest of the donor info from the spatialDLPFC SPE object
+## Load SPE data
+load(here("processed-data","02_build_spe","spe_basic.Rdata"))
+m <- match(sample_info$BrNum, spe$subject)
+sample_info$age <- spe$age[m]
+sample_info$sex <- spe$sex[m]
+rm(m, spe)
+
+## Build basic SCE
+message("Read 10x data and create sce - ", Sys.time())
+sce <- read10xCounts(
+  sample_info$sample_path,
+  sample_info$sample_ID,
+  type = "sparse",
+  col.names = TRUE
+)
+message("RDone - ", Sys.time())
+
+## Use key similar to spe objects
+sce$key <- paste0(sce$Barcode, "_", sce$sample_ID)
+
+## Add the study design info
+new_col <- merge(colData(sce), sample_info[, -which(colnames(sample_info) == "sample_path")])
+## Fix order
+new_col <- new_col[match(sce$key, new_col$key), ]
+stopifnot(identical(sce$key, new_col$key))
+rownames(new_col) <- colnames(sce)
+colData(sce) <- new_col
+
+## Use code from https://github.com/LieberInstitute/Visium_IF_AD/commit/08df3f7e4a3178563d6b4b1861b664b21466b395#diff-10cb35de98e2a3e5f4235cd88f6dabce5469eead2b2db1fd7121126849fcf585L100
+## Read in the gene information from the annotation GTF file
+gtf <-
+  rtracklayer::import(
+    "/dcs04/lieber/lcolladotor/annotationFiles_LIBD001/10x/refdata-gex-GRCh38-2020-A/genes/genes.gtf"
+  )
+gtf <- gtf[gtf$type == "gene"]
+names(gtf) <- gtf$gene_id
+
+## Match the genes
+match_genes <- match(rownames(sce), gtf$gene_id)
+stopifnot(all(!is.na(match_genes)))
+
+## Keep only some columns from the gtf
+mcols(gtf) <- mcols(gtf)[, c("source", "type", "gene_id", "gene_version", "gene_name", "gene_type")]
+
+## Add the gene info to our sce object
+rowRanges(sce) <- gtf[match_genes]
+
+## Inspect object
+sce
+
+## Note that no empty droplets have been filtered out yet!
+
+## Save for later
+if (!dir.exists(here("processed-data", "sce"))) dir.create(here("processed-data", "sce"))
+save(sce, file = here("processed-data", "sce", "sce_raw.Rdata"))
+
+## Size in Gb
+lobstr::obj_size(sce)
+
+
+# sgejobs::job_single('01_build_basic_sce', create_shell = TRUE, queue= 'bluejay', memory = '50G', command = "Rscript 01_build_basic_sce.R")
+
+## Reproducibility information
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
---
+++ snRNAseq_hpc/code/build_sce/02_barcode_ranks_empty_drops_qc.R
@@ -0,0 +1,116 @@
+## Based on
+## https://github.com/LieberInstitute/DLPFC_snRNAseq/blob/main/code/03_build_sce/02_get_droplet_scores.R
+
+library("SingleCellExperiment")
+library("DropletUtils")
+library("scuttle")
+library("tidyverse")
+library("here")
+library("sessioninfo")
+
+## get sample i
+args <- commandArgs(trailingOnly = TRUE)
+sample_i <- as.integer(args[[1]])
+
+#### Load & Subset raw data ####
+load(here("snRNA_seq","processed-data", "sce", "sce_raw.Rdata"), verbose = TRUE)
+
+samples <- unique(sce$Sample)
+sample_run <- samples[[sample_i]]
+message("Running Sample: ", sample_run, " (", sample_i, "/", length(samples), ")")
+
+sce <- sce[, sce$Sample == sample_run]
+message("ncol:", ncol(sce))
+
+#### Run barcodeRanks to find knee ####
+
+bcRanks <- barcodeRanks(sce, fit.bounds = c(10, 1e3))
+
+knee_higher <- metadata(bcRanks)$knee - 100
+message(
+  "'First knee point' = ", metadata(bcRanks)$knee, "\n",
+  "knee_higher =", knee_higher
+)
+
+message(
+  "'Second knee point' = ", metadata(bcRanks)$knee, "\n",
+  "knee =", metadata(bcRanks)$knee
+)
+
+
+knee_lower <- metadata(bcRanks)$knee + 100
+message(
+  "'Third knee point' = ", metadata(bcRanks)$knee, "\n",
+  "knee_lower =", knee_lower
+)
+
+
+
+#### Run emptyDrops w/ knee + 100 ####
+set.seed(100)
+message("Starting emptyDrops")
+Sys.time()
+e.out <- DropletUtils::emptyDrops(
+  sce,
+  niters = 25000,
+  lower = knee_lower
+  # ,
+  # BPPARAM = BiocParallel::MulticoreParam(4)
+)
+message("Done - saving data")
+Sys.time()
+
+save(e.out, file = here("processed-data", "03_build_sce", "droplet_scores", paste0("droplet_scores_", sample_run, ".Rdata")))
+
+#### QC Plots ####
+message("QC check")
+FDR_cutoff <- 0.001
+addmargins(table(Signif = e.out$FDR <= FDR_cutoff, Limited = e.out$Limited, useNA = "ifany"))
+
+n_cell_anno <- paste("Non-empty:", sum(e.out$FDR < FDR_cutoff, na.rm = TRUE))
+message(n_cell_anno)
+
+my_theme <- theme_bw() +
+  theme(text = element_text(size = 15))
+
+droplet_elbow_plot <- as.data.frame(bcRanks) %>%
+  add_column(FDR = e.out$FDR) %>%
+  ggplot(aes(x = rank, y = total, color = FDR < FDR_cutoff)) +
+  geom_point(alpha = 0.5, size = 1) +
+  geom_hline(yintercept = metadata(bcRanks)$knee, linetype = "dotted", color = "gray") +
+  annotate("text", x = 10, y = metadata(bcRanks)$knee, label = "Second Knee", vjust = -1, color = "gray") +
+  geom_hline(yintercept = knee_lower, linetype = "dashed") +
+  annotate("text", x = 10, y = knee_lower, label = "Knee est 'lower'", vjust = -0.5) +
+  scale_x_continuous(trans = "log10") +
+  scale_y_continuous(trans = "log10") +
+  labs(
+    x = "Barcode Rank",
+    y = "Total UMIs",
+    title = paste("Sample", sample_run),
+    subtitle = n_cell_anno,
+    color = paste("FDR <", FDR_cutoff)
+  ) +
+  my_theme +
+  theme(legend.position = "bottom")
+
+# droplet_scatter_plot <- as.data.frame(e) %>%
+#   ggplot(aes(x = Total, y = -LogProb, color = FDR < FDR_cutoff)) +
+#   geom_point(alpha = 0.5, size = 1) +
+#   labs(x = "Total UMIs", y = "-Log Probability",
+#        color = paste("FDR <", FDR_cutoff)) +
+#   my_theme+
+#   theme(legend.position = "bottom")
+# # print(droplet_elbow_plot/droplet_scatter_plot)
+# ggsave(droplet_elbow_plot/droplet_scatter_plot, filename = here("plots","03_build_sce", "droplet_qc_png",paste0("droplet_qc_",sample,".png")))
+
+ggsave(droplet_elbow_plot, filename = here("plots", "03_build_sce", "droplet_qc_png", paste0("droplet_qc_", sample_run, ".png")))
+
+
+# sgejobs::job_single('get_droplet_scores', create_shell = TRUE, queue= 'bluejay', memory = '50G', command = "Rscript get_droplet_scores.R")
+
+## Reproducibility information
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
