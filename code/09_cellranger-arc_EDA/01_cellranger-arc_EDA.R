5a99e5896961a6ec007ee7ca3bfc8dd3212530ff
Cynthia Soto <csoto@compute-090.cm.cluster>
2023-03-13 11:58:58 -0400

    Add Findmarkers() and coverageplot() for differentially expressed genes and add dinmarkers() for differential accesability peaks -- Cynthia Soto / Hedia Tnani
    Former-commit-id: 091c09b33f8b7d42c4766e7485a82866013f7fcb

--- code/09_cellranger-arc_EDA/01_cellranger-arc_EDA.R
+++ code/09_cellranger-arc_EDA/01_cellranger-arc_EDA.R
@@ -1,8 +1,8 @@
 ########################################################################
 ## 1st EDA with scCellRanger-ARC
-## Author. CSC / LIEBER
+## Authors. CSC + HT / LIEBER
 ## Date. March 3rd, 2023
-## LM. March 7th, 2023
+## LM. March 13th, 2023
 ## Reference Link. 
 ## Use this first: https://stuartlab.org/signac/articles/pbmc_multiomic.html 
 ## https://stuartlab.org/signac/articles/pbmc_vignette.html
@@ -32,6 +32,9 @@ hippo <- CreateSeuratObject(
   counts = rna_counts,
   assay = "RNA"
 )
+
+# check metadata
+hippo@meta.data
 
 Idents(hippo) <- "hippo-42_1"
 hippo[["percent.mt"]] <- PercentageFeatureSet(hippo, pattern = "^MT-")
@@ -82,12 +85,11 @@ genome(annotations) <- "hg38"
 # annotation_chunk_all$gene_biotype[!is.na(m)] <- annotations$gene_id[m[!is.na(m)]]
 
 # faq recommendation
-# 
-gtf <- rtracklayer::import('/dcs04/lieber/lcolladotor/annotationFiles_LIBD001/10x/refdata-cellranger-arc-GRCh38-2020-A-2.0.0/genes/genes.gtf.gz')
-gene.coords <- gtf[gtf$type == 'gene']
-seqlevelsStyle(gene.coords) <- 'UCSC'
-gene.coords <- keepStandardChromosomes(gene.coords, pruning.mode = 'coarse')
-
+# gtf <- rtracklayer::import('/dcs04/lieber/lcolladotor/annotationFiles_LIBD001/10x/refdata-cellranger-arc-GRCh38-2020-A-2.0.0/genes/genes.gtf.gz')
+# gene.coords <- gtf[gtf$type == 'gene']
+# seqlevelsStyle(gene.coords) <- 'UCSC'
+# gene.coords <- keepStandardChromosomes(gene.coords, pruning.mode = 'coarse')
+# annotations <- gtf
 
 # create ATAC assay and add it to the object
 chrom_assay <- CreateChromatinAssay(
@@ -145,7 +147,7 @@ hippo <- SCTransform(hippo, verbose = FALSE) %>% RunPCA() %>% RunUMAP(dims = 1:3
 # ATAC analysis
 # We exclude the first dimension as this is typically correlated with sequencing depth
 DefaultAssay(hippo) <- "ATAC"
-hippo <- FindTopFeatures(hippo, min.cutoff = 'q0')
+hippo <- FindTopFeatures(hippo, min.cutoff = "q0")
 hippo <- RunTFIDF(hippo)
 hippo <- RunSVD(hippo)
 hippo <- RunUMAP(hippo, reduction = 'lsi', dims = 2:30, reduction.name = "umap.atac", reduction.key = "atacUMAP_")
@@ -154,7 +156,7 @@ hippo <- RunUMAP(hippo, reduction = 'lsi', dims = 2:30, reduction.name = "umap.a
 # WNN graph, representing a weighted combination of RNA and ATAC-seq modalities
 hippo <- FindMultiModalNeighbors(hippo, reduction.list = list("pca", "lsi"), dims.list = list(1:30, 2:30))
 hippo <- RunUMAP(hippo, nn.name = "weighted.nn", reduction.name = "wnn.umap", reduction.key = "wnnUMAP_")
-hippo <- FindClusters(hippo, graph.name = "wsnn", algorithm = 3, resolution = 0.07,verbose = TRUE)
+hippo <- FindClusters(hippo, graph.name = "wsnn", algorithm = 3, resolution = 0.5,verbose = TRUE)
 
 # visualization
 p1 <- DimPlot(hippo, reduction = "umap.rna", group.by = "seurat_clusters", label = TRUE, label.size = 2.5, repel = TRUE) + ggtitle("RNA")
@@ -163,46 +165,111 @@ p3 <- DimPlot(hippo, reduction = "wnn.umap", group.by = "seurat_clusters", label
 p1 + p2 + p3 & NoLegend() & theme(plot.title = element_text(hjust = 0.5))
 
 # findmarkers
-# Map over the cluster levels and run FindMarkers for each cluster
-# cluster.markers <- map(levels(hippo$seurat_clusters[[1]]), ~ FindMarkers(hippo, ident.1 = .x, min.pct = 0.25))
+#Map over the cluster levels and run FindMarkers for each cluster
+DefaultAssay(hippo) <- "RNA"
+cluster.markers <- map(levels(hippo$seurat_clusters[[1]]), ~ FindMarkers(hippo, ident.1 = .x, min.pct = 0.25))
 
-# Add cluster number to each marker using map and mutate
-# cluster.markers <- map2(cluster.markers, seq_along(cluster.markers), ~ mutate(.x, cluster = .y - 1, gene = rownames(.x)))
+#Add cluster number to each marker using map and mutate
+cluster.markers <- map2(cluster.markers, seq_along(cluster.markers), ~ mutate(.x, cluster = .y - 1, gene = rownames(.x)))
+
+# find markers for every cluster compared to all remaining cells, report only the positive
+# ones
+DefaultAssay(hippo) <- "RNA"
+
+cluster.markers.all <- FindAllMarkers(hippo, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 1)
+cluster.markers.all %>%
+  group_by(cluster) %>%
+  slice_max(n = 5, order_by = avg_log2FC)
+
+# top 5 
+cluster.markers.all %>%
+  group_by(cluster) %>%
+  top_n(n = 5, wt = avg_log2FC) -> top5
 
 #Linking peaks to genes
+# DefaultAssay(hippo) <- "ATAC"
+# 
+# # first compute the GC content for each peak
+# hippo <- RegionStats(hippo, genome = BSgenome.Hsapiens.UCSC.hg38,assay = "ATAC")
+
+# link peaks to genes taking an example two genes
+# hippo <- LinkPeaks(
+#   object = hippo,
+#   peak.assay = "ATAC",
+#   expression.assay = "SCT",
+#   genes.use = c("PRAMEF2", "PLCH2")
+# )
+# 
+# p4 <- CoveragePlot(
+#   object = hippo,
+#   region = "PEX10",
+#   features = "PEX10",
+#   expression.assay = "SCT",
+#   extend.upstream = 500,
+#   extend.downstream = 500
+# )
+# 
+# p5 <- CoveragePlot(
+#   object = hippo,
+#   region = "PLCH2",
+#   features = "PLCH2",
+#   expression.assay = "SCT",
+#   extend.upstream = 500,
+#   extend.downstream = 500
+# )
+# 
+# patchwork::wrap_plots(p4, p5, ncol = 1)
+
+# link peaks to differentially expressed features (cluster biomarkers)
+
+pdf(file=here("plots", "09_cellranger-arc_EDA", "CoveragePlot_Top5markers_percluster.pdf"))
+
+walk(seq_along(top5$gene), ~ {
+  message(paste0("Processing gene ",.x, " ", top5$gene[.x]))
+  
+  tryCatch({
+    DefaultAssay(hippo) <- "ATAC"
+    hippo <- RegionStats(hippo, genome = BSgenome.Hsapiens.UCSC.hg38, assay = "ATAC")
+    hippo <- LinkPeaks(object = hippo, peak.assay = "ATAC", expression.assay = "SCT", genes.use = top5$gene[.x])
+    message(paste0("Generating coverage plot for gene ", top5$gene[.x]))
+    
+p <- CoveragePlot(
+      object = hippo,
+      region = top5$gene[.x],
+      features = top5$gene[.x],
+      expression.assay = "SCT",
+      extend.upstream = 500,
+      extend.downstream = 500
+    )
+
+print(p)
+    
+    message(paste0("Gene ", .x , " ", top5$gene[.x], " completed successfully"))
+    message(paste0(length(top5$gene)-.x, " ", "genes still need to be processed"))
+  }, error = function(e) {
+    message(paste0("Error occurred while processing gene ", top5$gene[.x], ": ", e$message))
+  })
+  
+})
+
+dev.off()
+
+
+# Find differentially accessible peaks between clusters
+# change back to working with peaks instead of gene activities
 DefaultAssay(hippo) <- "ATAC"
+clusters <- unique(levels(hippo$seurat_clusters[[1]]))
+pairwise <- combn(clusters, 2)
 
-# first compute the GC content for each peak
-hippo <- RegionStats(hippo, genome = BSgenome.Hsapiens.UCSC.hg38,assay = "ATAC")
+# all pairwise comparisons
+da_peaks <- map(1:ncol(pairwise), ~{
+  markers <- FindMarkers(hippo, ident.1 = pairwise[1, .x], ident.2 = pairwise[2, .x],test.use = 'LR')
+  comparisons <- pairwise[, .x]
+  markers$comparison <- paste(comparisons[1], comparisons[2], sep = '_')
+  markers
+})
 
-# link peaks to genes
-hippo <- LinkPeaks(
-  object = hippo,
-  peak.assay = "ATAC",
-  expression.assay = "SCT",
-  genes.use = c("PRAMEF2", "PLCH2")
-)
+da_peaks.df <- do.call(rbind, da_peaks)
 
-p4 <- CoveragePlot(
-  object = hippo,
-  region = "PEX10",
-  features = "PEX10",
-  expression.assay = "SCT",
-  extend.upstream = 500,
-  extend.downstream = 500
-)
-
-p5 <- CoveragePlot(
-  object = hippo,
-  region = "PLCH2",
-  features = "PLCH2",
-  expression.assay = "SCT",
-  extend.upstream = 500,
-  extend.downstream = 500
-)
-
-patchwork::wrap_plots(p4, p5, ncol = 1)
-
-
-
+da_peaks.df
 
