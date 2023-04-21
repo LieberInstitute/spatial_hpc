4c2ca2e3fead24f9bb20de991254c6839e9cfd26
Erik Nelson <erik.nelson116@gmail.com>
2023-04-21 13:36:15 -0400

    updating code to run cellranger on new sce samples
    Former-commit-id: f3b52eb399caea51e25e02365557e7719b9d2fad

---
+++ snRNAseq_hpc/code/01_align/rounds4-5.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#$ -cwd
+#$ -l mem_free=10G,h_vmem=10G,h_fsize=100G,bluejay
+#$ -pe local 4
+#$ -N rounds4-5
+#$ -o logs/rounds4-5.$TASK_ID.txt
+#$ -e logs/rounds4-5.$TASK_ID.txt
+#$ -m e
+#$ -t 1-6
+#$ -tc 3
+echo "**** Job starts ****"
+date
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "****"
+echo "Task id: ${SGE_TASK_ID}"
+echo "****"
+##load cellranger 7
+module load cellranger/7.0.0
+## List current modules for reproducibility
+module list
+## Locate file
+SAMPLE=$(awk "NR==${SGE_TASK_ID}" ${JOB_NAME}.txt)
+echo "Processing sample ${SAMPLE}"
+date
+cellranger count --id=${SAMPLE} \
+     --transcriptome=/dcs04/lieber/lcolladotor/annotationFiles_LIBD001/10x/refdata-gex-GRCh38-2020-A \
+		 --fastqs=/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/snRNAseq_hpc/raw-data/FASTQ/${SAMPLE} \
+		 --sample=${SAMPLE} \
+		 --jobmode=local \
+		 --localcores=4 \
+		 --localmem=40 \
