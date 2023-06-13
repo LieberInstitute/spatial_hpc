a0fbd012dcd5886658bfeef0126e8a9ceec50a16
Erik Nelson <erik.nelson116@gmail.com>
2023-06-13 15:06:41 -0400

    updating snRNAseq dimred/clustering code
    Former-commit-id: 4c777eed5c9f17364b990b424ce32ec44c0b4cbf

--- snRNAseq_hpc/code/build_sce/cluster.R
+++ snRNAseq_hpc/code/build_sce/cluster.R
@@ -5,26 +5,37 @@ library("scran")
 library("here")
 library("sessioninfo")
 
-load(here("snRNAseq_hpc","processed-data", "sce", "sce_harmony.rda"), verbose = TRUE)
+load(here("snRNAseq_hpc","processed-data", "sce", "sce_MNN.rda"), verbose = TRUE)
 
 message("running buildSNNGraph - ", Sys.time())
-snn.gr <- buildSNNGraph(sce, k = 25, use.dimred = "HARMONY")
-snn.gr2 <- buildSNNGraph(sce, k = 50, use.dimred = "HARMONY")
+snn.gr <- buildSNNGraph(sce, k = 10, use.dimred = "MNN",type='jaccard')
 
-message("running walktrap - ", Sys.time())
-clust <- igraph::cluster_walktrap(snn.gr)$membership
-clust50 <- igraph::cluster_walktrap(snn.gr2)$membership
 
-table(clust)
-table(clust50)
+message("running louvain - ", Sys.time())
+set.seed(100)
+clust_5 <- igraph::cluster_louvain(snn.gr,resolution=1.5)$membership
+table(clust_5)
+sce$k_10_louvain<-factor(clust_5)
+
+sce$discard<-ifelse(sce$k_10_louvain %in% c(6,7,48),T,F)
+sce<-sce[,sce$discard==F]
+
+set.seed(000)
+clust_5 <- igraph::cluster_louvain(snn.gr,resolution=2)$membership
+table(clust_5)
+sce$k_5_louvain_4<-factor(clust_5)
+
+
+table(clust_50)
+table(clust_5)
 
 ##add to sce
-sce$k_25_label<-clust
-sce$k_50_label<-clust50
+sce$k_5_louvain_4<-factor(clust_5)
+sce$k_25_pca_walk<-clust_25
 
 ##make some prelim plots
-pdf(here("snRNAseq_hpc","plots","UMAP_k25.pdf"))
-plotUMAP(sce,colour_by='k_25_label',text_by='k_25_label')
+pdf(here("snRNAseq_hpc","plots","UMAP_k20.pdf"))
+plotUMAP(sce,colour_by='k_20_label',text_by='k_20_label')
 dev.off()
 
 pdf(here("snRNAseq_hpc","plots","UMAP_k50.pdf"))
@@ -49,9 +60,162 @@ plotExpression(sce,features=c('SYT1','SLC17A7','SLC17A6',
                               'GFAP','TTR','CSF1R',
                               'PDGFRA','FLT1','TNNT2'),
                x="k_50_label", colour_by="k_50_label", point_alpha=0.5, point_size=.7,add_legend=F)+
-  stat_summary(fun = median, fun.min = median, fun.max = median, geom = "crossbar", 
+  stat_summary(fun = median, fun.min = median, fun.max = median, geom = "crossbar",
                width = 0.3)
 dev.off()
+
+x<-RcppML::nmf(
+    assay(sce,'logcounts'),
+    75,
+    tol = 1e-04,
+    maxit = 100,
+    verbose = TRUE,
+    L1 = c(0, 0),
+    mask_zeros = FALSE,
+    diag = TRUE,
+    nonneg = TRUE,
+    seed=122
+)
+
+####First remove low quality neuron clusters from first run and rerun all feature selection/dimred/batch correction steps
+message("running louvain - ", Sys.time())
+set.seed(100)
+clust_5 <- igraph::cluster_louvain(snn.gr,resolution=1.2)$membership
+table(clust_5)
+sce$cluster_harmony<-factor(clust_5)
+
+
+
+tab<-data.frame('cluster'=1:51,'annotation'=rep(NA,51),'annoType'=rep(NA,51))
+########Neurons
+##Granule cells
+tab$annotation[tab$cluster %in% c(10,28,39,40)]<-'EXC'
+tab$annoType[tab$cluster %in% c(10,28,39,40)]<-paste0('GC.',c(1:4))
+##Mossy cells
+tab$annotation[tab$cluster %in% c(18)]<-'EXC'
+tab$annoType[tab$cluster %in% c(18)]<-'MC'
+##CA3
+tab$annotation[tab$cluster %in% c(9)]<-'EXC'
+tab$annoType[tab$cluster %in% c(9)]<-'CA3'
+##CA3
+tab$annotation[tab$cluster %in% c(33)]<-'EXC'
+tab$annoType[tab$cluster %in% c(33)]<-'CA3.deep'
+##CA2
+tab$annotation[tab$cluster %in% c(36)]<-'EXC'
+tab$annoType[tab$cluster %in% c(36)]<-'CA2'
+##CA1
+tab$annotation[tab$cluster %in% c(11)]<-'EXC'
+tab$annoType[tab$cluster %in% c(11)]<-'CA1'
+
+##Sub_sup
+tab$annotation[tab$cluster %in% c(23)]<-'EXC'
+tab$annoType[tab$cluster %in% c(23)]<-'Sub.Sup'
+##Sub_deep
+tab$annotation[tab$cluster %in% c(19)]<-'EXC'
+tab$annoType[tab$cluster %in% c(19)]<-'Sub.Deep'
+
+##RHP
+tab$annotation[tab$cluster %in% c(25,29,38,43,45)]<-'EXC'
+tab$annoType[tab$cluster %in% c(25,29,38,43,45)]<-paste0('RHP.L2.',c(1:5))
+
+tab$annotation[tab$cluster %in% c(5)]<-'EXC'
+tab$annoType[tab$cluster %in% c(5)]<-'RHP.L3'
+
+tab$annotation[tab$cluster %in% c(25,29,38,43,45)]<-'EXC'
+tab$annoType[tab$cluster %in% c(25,29,38,43,45)]<-paste0('RHP.L2.',c(1:5))
+
+tab$annotation[tab$cluster %in% c(21)]<-'EXC'
+tab$annoType[tab$cluster %in% c(21)]<-'RHP.L5'
+
+tab$annotation[tab$cluster %in% c(12,14)]<-'EXC'
+tab$annoType[tab$cluster %in% c(12,14)]<-paste0('RHP.L6.',c(1:2))
+
+tab$annotation[tab$cluster %in% c(20)]<-'EXC'
+tab$annoType[tab$cluster %in% c(20)]<-paste0('RHP.L6b')
+
+tab$annotation[tab$cluster %in% c(4)]<-'EXC'
+tab$annoType[tab$cluster %in% c(4)]<-paste0('Thal')
+
+
+
+##Amygdala
+tab$annotation[tab$cluster %in% c(41,42,48)]<-'EXC'
+tab$annoType[tab$cluster %in% c(41,42,48)]<-paste0('Amy.',c(1:3))
+##HATA
+tab$annotation[tab$cluster %in% c(32)]<-'EXC'
+tab$annoType[tab$cluster %in% c(32)]<-'HATA'
+
+##Cajal Retzius
+tab$annotation[tab$cluster %in% c(46)]<-'EXC'
+tab$annoType[tab$cluster %in% c(46)]<-'Cajal'
+
+##GABAergics
+tab$annotation[tab$cluster %in% c(47)]<-'INH'
+tab$annoType[tab$cluster %in% c(47)]<-'MEIS2'
+
+tab$annotation[tab$cluster %in% c(13)]<-'INH'
+tab$annoType[tab$cluster %in% c(13)]<-'PVALB'
+
+tab$annotation[tab$cluster %in% c(8)]<-'INH'
+tab$annoType[tab$cluster %in% c(8)]<-'SST'
+
+tab$annotation[tab$cluster %in% c(49)]<-'INH'
+tab$annoType[tab$cluster %in% c(49)]<-'NKX2-1'
+
+tab$annotation[tab$cluster %in% c(3)]<-'INH'
+tab$annoType[tab$cluster %in% c(3)]<-'LAMP5 LHX6'
+
+tab$annotation[tab$cluster %in% c(15)]<-'INH'
+tab$annoType[tab$cluster %in% c(15)]<-'LAMP5 KIT'
+
+tab$annotation[tab$cluster %in% c(34)]<-'INH'
+tab$annoType[tab$cluster %in% c(34)]<-'HTR3A'
+
+tab$annotation[tab$cluster %in% c(26)]<-'INH'
+tab$annoType[tab$cluster %in% c(26)]<-'VIP'
+
+tab$annotation[tab$cluster %in% c(44)]<-'INH'
+tab$annoType[tab$cluster %in% c(44)]<-'CORT'
+
+######NNC
+tab$annotation[tab$cluster %in% c(51,30)]<-'NNC'
+tab$annoType[tab$cluster %in% c(51,30)]<-paste0('CP.',c(1:2))
+
+tab$annotation[tab$cluster %in% c(1,7)]<-'NNC'
+tab$annoType[tab$cluster %in% c(1,7)]<-paste0('Oligo.',c(1:2))
+
+tab$annotation[tab$cluster %in% c(31)]<-'NNC'
+tab$annoType[tab$cluster %in% c(31)]<-'Oligo.NB'
+
+tab$annotation[tab$cluster %in% c(50)]<-'NNC'
+tab$annoType[tab$cluster %in% c(50)]<-'COP'
+
+tab$annotation[tab$cluster %in% c(22)]<-'NNC'
+tab$annoType[tab$cluster %in% c(22)]<-'OPC'
+
+tab$annotation[tab$cluster %in% c(6,17)]<-'NNC'
+tab$annoType[tab$cluster %in% c(6,17)]<-paste0('Astro.',c(1:2))
+
+tab$annotation[tab$cluster %in% c(37)]<-'NNC'
+tab$annoType[tab$cluster %in% c(37)]<-'Ependy'
+
+tab$annotation[tab$cluster %in% c(37)]<-'NNC'
+tab$annoType[tab$cluster %in% c(37)]<-'Ependy'
+
+tab$annotation[tab$cluster %in% c(2)]<-'NNC'
+tab$annoType[tab$cluster %in% c(2)]<-'Micro'
+
+tab$annotation[tab$cluster %in% c(24)]<-'NNC'
+tab$annoType[tab$cluster %in% c(24)]<-'Tcell/Macro'
+
+tab$annotation[tab$cluster %in% c(27)]<-'NNC'
+tab$annoType[tab$cluster %in% c(27)]<-'SMC'
+
+tab$annotation[tab$cluster %in% c(16)]<-'NNC'
+tab$annoType[tab$cluster %in% c(16)]<-'Endo/PC'
+
+tab$annotation[tab$cluster %in% c(35)]<-'NNC'
+tab$annoType[tab$cluster %in% c(35)]<-'VLMC'
 
 ## Reproducibility information
 print("Reproducibility information:")
--- snRNAseq_hpc/code/build_sce/feature_selection_dimred.R
+++ snRNAseq_hpc/code/build_sce/feature_selection_dimred.R
@@ -9,14 +9,19 @@ library("BiocSingular")
 
 
 ## Load sce
-load(here("snRNAseq_hpc","processed-data", "sce", "sce_liana.rda"))
+load(here("snRNAseq_hpc","processed-data", "sce", "sce_post_qc.rda"))
 dim(sce)
+sce$brnum<-factor(tab$Brain[match(sce$Sample,tab$Sample..)])
+##rm problematic 17c-scp nuclei
+assay(sce,'binomial_deviance_residuals')<-NULL
+sce$discard<-ifelse(sce$Sample %in% "17c-scp" & sce$detected < 5000,T,F)
+sce<-sce[,sce$discard==F]
 
 ##feature selection using deviance
-##let's do a binomial model first
-#set.seed(800)
-#sce <- devianceFeatureSelection(sce,
-#                                assay = "counts", fam = "binomial", sorted = T)
+##let's do a poisson model
+set.seed(800)
+sce <- devianceFeatureSelection(sce,
+                                assay = "counts", fam = "poisson", sorted = T,batch=sce$brnum)
 #sce <- save(sce,file = here("snRNAseq_hpc","processed-data", "sce", "sce_deviance.rda"))
 
 # This temp file just used for getting batch-corrected components (drops a variety of entries)
@@ -24,28 +29,52 @@ dim(sce)
 #                                      rowData(sce)$gene_name)
 #}
 #pdf(here("snRNAseq_hpc","plots", "build_sce", "binomial_deviance.pdf"))
-#plot(sort(rowData(sce)$binomial_deviance, decreasing = T),
-#     type = "l", xlab = "ranked genes",
-#     ylim=c(0,500000),
-#     ylab = "binomial deviance", main = "Feature Selection with Deviance"
-#)
-#abline(v = 2000, lty = 2, col = "red")
-#abline(v = 3000, lty = 2, col = "blue")
-#abline(v = 4000, lty = 2, col = "green")
-#abline(v = 5000, lty = 2, col = "black")
+plot(sort(rowData(sce)$poisson_deviance, decreasing = T),
+     type = "l", xlab = "ranked genes",
+     ylim=c(0,1000000),
+     ylab = "poisson deviance", main = "Feature Selection with Deviance"
+)
+abline(v = 2000, lty = 2, col = "red")
+abline(v = 2500, lty = 2, col = "pink")
+abline(v = 3000, lty = 2, col = "blue")
+abline(v = 4000, lty = 2, col = "green")
+abline(v = 5000, lty = 2, col = "black")
 #dev.off()
 #
-hdg<-rownames(counts(sce))[1:2500]
+hdg<-rownames(counts(sce))[1:3000]
 set.seed(913)
 message("running nullResiduals - ", Sys.time())
-sce <- nullResiduals(sce,
-                     fam = "binomial", 
-                     type = "deviance"
+res<-sce[rownames(counts(sce)) %in% hdg,]
+res <- nullResiduals(res,
+                     fam = "poisson",
+                     type = "pearson",
+                     assay='counts'
 )
+# # Initialize an empty list to store results for each batch
+# residuals_list <- list()
+#
+# # Specify the fam and type variables explicitly
+# fam = "binomial"
+# type = "deviance"
+#
+# # Run the nullResiduals() function for each batch
+# for(b in levels(res$brnum)){
+#     idx <- which(res$brnum == b)
+#     temp_res <- nullResiduals(res[, idx], fam = fam, type = type, assay='counts')
+#     residuals_list[[b]] <- temp_res
+# }
+
+# Combine the results
+combined_residuals <- do.call(cbind, residuals_list)
+
 
 set.seed(915)
 message("running PCA - ", Sys.time())
-sce <- runPCA(sce, ncomponents = 100)
+res <- scater::runPCA(res,
+                      exprs_values='poisson_pearson_residuals',
+                      ncomponents=100,
+                      BSPARAM=IrlbaParam())
+
 
 ## Why multi match as well?
 # sce <- multiBatchNorm(sce, batch=sce$sample_short)
@@ -54,9 +83,11 @@ sce <- runPCA(sce, ncomponents = 100)
 
 #message("running TSNE - ", Sys.time())
 #sce <- runTSNE(sce, dimred = "PCA")
-set.seed(916)
+reducedDim(sce,'PCA')<-reducedDim(res,'PCA')
+set.seed(12)
 message("running UMAP - ", Sys.time())
-sce <- runUMAP(sce, dimred = "PCA")
+sce <- runUMAP(sce, dimred = "PCA",n_dimred=100)
+reducedDim(sce,'UMAP2')<-umap
 
 pdf(here("snRNAseq_hpc","plots","UMAP_uncorrected_by_sample.pdf"))
 plotUMAP(sce,colour_by='Sample')
@@ -66,18 +97,23 @@ message("Saving Data - ", Sys.time())
 save(sce, file = here("snRNAseq_hpc","processed-data", "sce", "sce.rda"))
 # sgejobs::job_single('normalize_step1_glm', create_shell = TRUE, queue= 'bluejay', memory = '75G', command = "Rscript normalize_step1_glm.R")
 
+
 # Run harmony
 message("running Harmony - ", Sys.time())
-sce <- RunHarmony(sce, group.by.vars = "Sample", verbose = TRUE)
+set.seed(1788)
+sce <- harmony::RunHarmony(sce, group.by.vars = "brnum",
+                           verbose = TRUE,max.iter.harmony=30)
 
+mnn<-batchelor::reducedMNN(reducedDim(sce,'PCA'),batch=sce$brnum,k=50)
 
 #### TSNE & UMAP ####
 #set.seed(602)
 #message("running TSNE - ", Sys.time())
 #sce <- runTSNE(sce, dimred = "HARMONY")
-
+reducedDim(sce,'MNN')<-mnn$corrected
+set.seed(125)
 message("running UMAP - ", Sys.time())
-sce <- runUMAP(sce, dimred = "HARMONY")
+sce <- runUMAP(sce, dimred = "MNN")
 
 pdf(here("snRNAseq_hpc","plots","UMAP_corrected_by_sample.pdf"))
 plotUMAP(sce,colour_by='Sample')
@@ -85,7 +121,7 @@ dev.off()
 
 message("Done UMAP - Saving data...", Sys.time())
 
-save(sce, file = here("snRNAseq_hpc","processed-data", "sce", "sce_unclustered.rda"))
+save(sce, file = here("snRNAseq_hpc","processed-data", "sce", "sce_harmony.rda"))
 
 
 ## Reproducibility information
