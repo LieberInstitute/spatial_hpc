6cf96ba9ffe8895782a448b5cc42d03a62b99f04
madhavitippani <madhavitippani28@gmail.com>
2023-07-26 14:16:10 -0400

    update allsamples txt filename
    Former-commit-id: 483904fd1856355bc0590fd696a2a2cceff01d15

--- code/VistoSeg/code/refineVNS.sh
+++ code/VistoSeg/code/refineVNS.sh
@@ -5,7 +5,7 @@
 #$ -e logs/refineVNS.$TASK_ID.txt
 #$ -m e
 #$ -M madhavitippani28@gmail.com
-#$ -t 1-40
+#$ -t 1
 #$ -tc 10
 
 echo "**** Job starts ****"
@@ -19,14 +19,14 @@ echo "Job name: ${JOB_NAME}"
 echo "Hostname: ${HOSTNAME}"
 echo "Task id: ${SGE_TASK_ID}"
 echo "****"
-echo "Sample id: $(cat /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/VistoSeg/code/ALLsamples.txt | awk '{print $1}' | awk "NR==${SGE_TASK_ID}") "
+echo "Sample id: $(cat /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/VistoSeg/code/ALLSAMPLES.txt | awk '{print $1}' | awk "NR==${SGE_TASK_ID}") "
 echo "****"
 
 module load matlab/R2019a
 
 toolbox='/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/VistoSeg/code'
-fname=$(cat /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/VistoSeg/code/ALLsamples.txt | awk '{print $1}' | awk "NR==${SGE_TASK_ID}")
-M=$(cat /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/VistoSeg/code/ALLsamples.txt |  awk '{print $2}' | awk "NR==${SGE_TASK_ID}")
+fname=$(cat /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/VistoSeg/code/ALLSAMPLES.txtt | awk '{print $1}' | awk "NR==${SGE_TASK_ID}")
+M=$(cat /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/VistoSeg/code/ALLSAMPLES.txt |  awk '{print $2}' | awk "NR==${SGE_TASK_ID}")
 
 matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), refineVNS('$fname',$M)"
 
