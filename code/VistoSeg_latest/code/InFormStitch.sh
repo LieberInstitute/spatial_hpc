b37fa48890d7911097ddf9bb5cf138f01a9cd809
madhavitippani <madhavitippani28@gmail.com>
2023-07-27 11:01:59 -0400

    correct order and channels in InFormStitch.sh
    Former-commit-id: 4fbea23dc68b353a9f34f549305b40f3e575ad04

--- code/VistoSeg_latest/code/InFormStitch.sh
+++ code/VistoSeg_latest/code/InFormStitch.sh
@@ -27,9 +27,9 @@ filename1='V12D07-332'
 filename2='V12D07-335'
 
 echo "V12D07-332_rerun/20230517_VSPG_HPC_Round1_Scan2_*_component_data.tif"
-matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), O1{1} = 'DAPI'; O1{2} = 'Alexa_488'; O1{3} = 'Alexa_594'; O1{4} = 'Alexa_647'; O1{5} = 'Autofluorescence'; InFormStitch('$path1',O1,6,'$filename1')"
+matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), O1{1} = 'DAPI'; O1{2} = 'Alexa_488'; O1{3} = 'Alexa_555'; O1{4} = 'Alexa_594'; O1{5} = 'Alexa_647'; O1{6} = 'Autofluorescence'; InFormStitch('$path1',O1,6,'$filename1')"
 echo "V12D07-335_rerun/20230518_VSPG_HPC_Round2_Scan2_*_component_data.tif"
-matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), O2{1} = 'DAPI'; O2{2} = 'Alexa_488'; O2{3} = 'Alexa_594'; O2{4} = 'Alexa_647'; O2{5} = 'Autofluorescence'; InFormStitch('$path2',O2,6,'$filename2')"
+matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), O2{1} = 'DAPI'; O2{2} = 'Alexa_488'; O2{3} = 'Alexa_555'; O2{4} = 'Alexa_594'; O2{5} = 'Alexa_647'; O2{6} = 'Autofluorescence'; InFormStitch('$path2',O2,6,'$filename2')"
 
 echo "**** Job ends ****"
 date
