53105357e2dc244313bf7cd662adc9ac76b0b62b
madhavitippani <madhavitippani28@gmail.com>
2023-07-28 13:25:16 -0400

    add user info
    Former-commit-id: 94b94915ef9d73e980c1d1eecdae12b922de350f

--- code/01_spaceranger/VSPG/spaceranger_VSPG.sh
+++ code/01_spaceranger/VSPG/spaceranger_VSPG.sh
@@ -9,3 +9,18 @@
 #$ -M madhavitippani28@gmail.com
 #$ -t 1-8
 #$ -tc 4
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
+## load SpaceRanger
+module load spaceranger/2.0.0
+
+## List current modules for reproducibility
+module list
