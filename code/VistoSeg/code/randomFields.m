0fec3dcdba692c8be8b89089e05e0b3587d19bee
madhavitippani <madhavitippani28@gmail.com>
2023-07-28 14:38:50 -0400

    stop displaying segmentations before printing in randomFields.m
    Former-commit-id: 414334d98cf0152a0b8c4cac3c213fc6bbf3474e

--- code/VistoSeg/code/randomFields.m
+++ code/VistoSeg/code/randomFields.m
@@ -1,11 +1,11 @@
 function randomFields(fname,M)
 he = imread(fname);
-
+fprintf('cluster %d\n', M)
 %load([fname(1:end-4),'_nuclei_WS_final.mat'])
 %load([fname(1:end-4),'_nuclei_WS.mat'])
-%load([fname(1:end-4),'_nuclei.mat'])
-load([fname(1:end-4),'_mask.mat'])
-mask_dark_blue = mask{M};
+load([fname(1:end-4),'_nuclei.mat'])
+%load([fname(1:end-4),'_mask.mat'])
+%mask_dark_blue = mask{M};
 
 [x,y,~] = size(he);
 xn = randi([500 x-1000],3,1);
@@ -15,13 +15,13 @@ A = he(xn(1):xn(1)+499, yn(1):yn(1)+499, :);
 B = he(xn(2):xn(2)+499, yn(2):yn(2)+499, :);
 C = he(xn(3):xn(3)+499, yn(3):yn(3)+499, :);
 temp = [A;zeros(10,500,3);B;zeros(10,500,3);C];
-imshow(temp)
+%imshow(temp)
 
 A1 = mask_dark_blue(xn(1):xn(1)+499, yn(1):yn(1)+499);
 B1 = mask_dark_blue(xn(2):xn(2)+499, yn(2):yn(2)+499);
 C1 = mask_dark_blue(xn(3):xn(3)+499, yn(3):yn(3)+499);
 temp1 = [A1;ones(10,500);B1;ones(10,500);C1];
-figure, imshow(temp1)
+%figure, imshow(temp1)
 
 close all
 imwrite(temp,[fname(1:end-4),'_nuclei_raw.png'])
