36fa7d55a54a171ab2cf193f8ae03d80ddbda1bf
madhavitippani <madhavitippani28@gmail.com>
2023-07-28 14:37:44 -0400

    switch to caracol, JHPCE busy
    Former-commit-id: 75d97027ac805cff07995841fa8e671d2a31b91e

--- code/01_spaceranger/VSPG/spaceranger_VSPG.sh
+++ code/01_spaceranger/VSPG/spaceranger_VSPG.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -cwd
-#$ -l mem_free=8G,h_vmem=8G,h_fsize=100G
+#$ -l caracol,mem_free=8G,h_vmem=8G,h_fsize=100G
 #$ -pe local 8
 #$ -N sparanger_HPC_VSPG
 #$ -o /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/01_spaceranger/VSPG/logs/sparanger_HPC_VSPG.$TASK_ID.txt
