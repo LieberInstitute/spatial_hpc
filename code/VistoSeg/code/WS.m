c947193c6a417cabfbe4b572b4a956f832ab02de
madhavitippani <madhavitippani28@gmail.com>
2023-08-04 09:22:01 -0400

    watershed code
    Former-commit-id: f6bd558b7955bd0c9655ba709f873b25e990e15a

--- code/VistoSeg/code/WS.m
+++ code/VistoSeg/code/WS.m
@@ -1,24 +1,41 @@
-function WS(fname)
+%function WS(fname)
+	opts = detectImportOptions('/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/VistoSeg/code/ALLSAMPLES.txt','Delimiter','\t', 'ReadVariableNames', false);
+	opts.VariableNames= {'filepath','M'};
+	t = readtable('/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/VistoSeg/code/ALLSAMPLES.txt',opts);
 
-load([fname(1:end-4),'_nuclei.mat'])
-he = imread(fname);
+	for i = 1:40
+		
+	   fname = t.filepath{i};	
+	   he = imread(fname);
+	   he = he(12501:13000,12501:13000,:);
+	   load([fname(1:end-4),'_mask.mat'])
+		   seg1 = mask{t.M(i)};
+	   load([fname(1:end-4),'_nuclei.mat'])
+		   seg2 = mask_dark_blue;	
+	   load([fname(1:end-4),'_sizeThresh.mat'])
+		   seg3 = mask_dark_blue;
 
-D = -bwdist(~mask_dark_blue);
-mask = imextendedmin(D,1);
-% figure
-% imshowpair(mask_dark_blue,mask,'blend')
+	   D = -bwdist(~mask_dark_blue);
+	   mask = imextendedmin(D,1);
+	   D2 = imimposemin(D,mask);
+	   Ld2 = watershed(D2);
+	   bw3 = mask_dark_blue;
+	   bw3(Ld2 == 0) = 0;
+	   mask_dark_blue = bw3;
 
-D2 = imimposemin(D,mask);
-Ld2 = watershed(D2);
-bw3 = mask_dark_blue;
-bw3(Ld2 == 0) = 0;
-%imshow(bw3)
-
-stats =  struct2table(regionprops(bw3, rgb2gray(he), 'Area', 'BoundingBox', 'Centroid', 'Circularity', 'Eccentricity', 'MajorAxisLength', 'MinorAxisLength', 'Perimeter', 'MeanIntensity' , 'WeightedCentroid'));
-writetable(stats,[fname(1:end-4),'_refineVNS_metric.csv'])
-mask_dark_blue = bw3;
-save([fname(1:end-4),'_nuclei_WS.mat'],'mask_dark_blue','-v7.3')
-blue_nuclei = he .* uint8(mask_dark_blue);
-imwrite(blue_nuclei,[fname(1:end-4),'_nuclei.tif'])
+	   save([fname(1:end-4),'_nuclei_WS.mat'],'mask_dark_blue','-v7.3')
+	
+   seg1 = 250*(double(seg1(12501:13000,12501:13000)));
+   seg1 = cat(3, seg1, seg1, seg1);
+   seg2 = 250*(double(seg2(12501:13000,12501:13000)));
+   seg2 = cat(3, seg2, seg2, seg2);
+   seg3 = 250*(double(seg3(12501:13000,12501:13000)));
+   seg3 = cat(3, seg3, seg3, seg3);
+   seg4 = 250*(double(bw3(12501:13000,12501:13000)));
+   seg4 = cat(3, seg4, seg4, seg4);
+   
+   img = [he; 250*ones(10,500,3); seg1; 250*ones(10,500,3); seg2; 250*ones(10,500,3); seg3; 250*ones(10,500,3); seg4];
+   imwrite(img, [fname(1:end-4),'_segFinal.png'])
+	
 end
 
