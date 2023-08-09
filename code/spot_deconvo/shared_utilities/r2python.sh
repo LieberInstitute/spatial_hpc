b17a36e8be87dbb73851a42e3f5d025074c2eb52
madhavitippani <madhavitippani28@gmail.com>
2023-08-09 11:52:59 -0400

    r2python.sh
    Former-commit-id: 5b075f5398a246d0d81feb11fd1597c021f9bd34

---
+++ code/spot_deconvo/shared_utilities/r2python.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --mem=100G
+#SBATCH --job-name=r2python
+#SBATCH -o logs/01-r2python_broad.log
+#SBATCH -e logs/01-r2python_broad.log
+
+#$ -l mem_free=100G,h_vmem=100G,h_fsize=100G
+#$ -N "r2python"
+#$ -o logs/01-r2python_broad.log
+#$ -e logs/01-r2python_broad.log
+
+USE_SLURM=2
+
+if [[ $USE_SLURM -eq 1 ]]; then
+    job_id=$SLURM_JOB_ID
+    job_name=$SLURM_JOB_NAME
+else
+    job_id=$JOB_ID
+    job_name=$JOB_NAME
+fi
+
+echo "**** Job starts ****"
+date
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${job_id}"
+echo "Job name: ${job_name}"
+echo "Hostname: ${HOSTNAME}" 
+
+module load conda_R/devel
+Rscript r2python.R
+
+echo "**** Job ends ****"
+date
