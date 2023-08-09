90c31e10cd28ea9ec737d45888883ebc436ab94f
madhavitippani <madhavitippani28@gmail.com>
2023-08-09 10:27:41 -0400

    update paths and run anndata conversion interactive
    Former-commit-id: 9518f25dd1aa8abd95a155d7dc17be4f63376a9b

--- code/spot_deconvo/shared_utilities/r2python.R
+++ code/spot_deconvo/shared_utilities/r2python.R
@@ -18,16 +18,12 @@ suppressPackageStartupMessages(library("tidyverse"))
 cell_group <- "broad" # "broad" or "layer"
 
 #  Paths
-sce_in <- "/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/cellranger-arc-hippo/cellranger-arc-hippo/snRNAseq_hpc/processed-data/sce/sce_nmf_final.rda"
+sce_in <- "/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/snRNAseq_hpc/processed-data/sce/sce_final.rda"
 #spe_IF_in <- here("processed-data", "02_build_spe", "spe_nmf_final.rda")
-spe_HE_in <- here("processed-data", "rdata", "spe", "01_build_spe","spe_filtered_final_with_clusters.Rdata")
-
-HE_id_path <- here("processed-data", "spot_deconvo", "HE_ID_table.csv")
-HE_counts_path <- here("processed-data", "rerun_spaceranger", "{sample_id}", "outs", "spatial", "tissue_spot_counts.csv")
+spe_HE_in <- here("processed-data","02_build_spe","spe_nmf_final.rda")
 
 sce_out <- here("processed-data", "spot_deconvo", "shared_utilities", paste0("sce_", cell_group, ".h5ad"))
 sce_r_out <- here("processed-data", "spot_deconvo", "shared_utilities", paste0("sce_", cell_group, ".rds"))
-
 #spe_IF_out <- here("processed-data", "spot_deconvo", "05-shared_utilities", "IF", "spe.h5ad")
 spe_HE_out <- here("processed-data", "spot_deconvo", "shared_utilities", "HE", "spe.h5ad")
 
@@ -37,13 +33,13 @@ sample_HE_out <- here("processed-data", "spot_deconvo", "shared_utilities", "HE"
 marker_object_out <- here("processed-data", "spot_deconvo", "shared_utilities", paste0("marker_stats_", cell_group, ".rds"))
 
 if (cell_group == "broad") {
-  cell_type_var <- "cellType_broad_hc"
+  cell_type_var <- "broad.type"
 } else {
-  cell_type_var <- "layer_level"
+  cell_type_var <- "cell.type"
 }
 
 #  Make sure output directories exist
-dir.create(dirname(spe_IF_out), recursive = TRUE, showWarnings = FALSE)
+# dir.create(dirname(spe_IF_out), recursive = TRUE, showWarnings = FALSE)
 dir.create(dirname(spe_HE_out), recursive = TRUE, showWarnings = FALSE)
 
 ###############################################################################
@@ -107,41 +103,43 @@ print(paste0("Running script at ", cell_group, "-resolution."))
 #-------------------------------------------------------------------------------
 #   Add cell counts to HE spatial object
 #-------------------------------------------------------------------------------
+# HE_id_path <- here("processed-data", "spot_deconvo", "HE_ID_table.csv")
+# HE_counts_path <- here("processed-data", "rerun_spaceranger", "{sample_id}", "outs", "spatial", "tissue_spot_counts.csv")
 
-id_table <- read.csv(HE_id_path)
-
-spe_HE$count <- NA
-for (sample_id in unique(spe_HE$sample_id)) {
-  #   Correctly determine the path for the cell counts for this sample, then
-  #   read in
-  long_id <- id_table[match(sample_id, id_table$short_id), "long_id"]
-  this_path <- sub("{sample_id}", long_id, HE_counts_path, fixed = TRUE)
-  cell_counts <- read.csv(this_path)
-  
-  #   All spots in the object should have counts
-  stopifnot(
-    all(
-      colnames(spe_nonIF[, spe_nonIF$sample_id == sample_id]) %in%
-        cell_counts$barcode
-    )
-  )
-  
-  #   Line up the rows of 'cell_counts' with the sample-subsetted SPE object
-  cell_counts <- cell_counts[
-    match(
-      colnames(spe_nonIF[, spe_nonIF$sample_id == sample_id]),
-      cell_counts$barcode
-    ),
-  ]
-  
-  #   Add this sample's counts to the SPE object
-  spe_nonIF$count[spe_nonIF$sample_id == sample_id] <- cell_counts$Nmask_dark_blue
-}
-
-#   Ensure counts were read in for all spots in the object
-if (any(is.na(spe_nonIF$count))) {
-  stop("Did not find cell counts for all non-IF spots.")
-}
+# id_table <- read.csv(HE_id_path)
+# 
+# spe_HE$count <- NA
+# for (sample_id in unique(spe_HE$sample_id)) {
+#   #   Correctly determine the path for the cell counts for this sample, then
+#   #   read in
+#   long_id <- id_table[match(sample_id, id_table$short_id), "long_id"]
+#   this_path <- sub("{sample_id}", long_id, HE_counts_path, fixed = TRUE)
+#   cell_counts <- read.csv(this_path)
+#   
+#   #   All spots in the object should have counts
+#   stopifnot(
+#     all(
+#       colnames(spe_nonIF[, spe_nonIF$sample_id == sample_id]) %in%
+#         cell_counts$barcode
+#     )
+#   )
+#   
+#   #   Line up the rows of 'cell_counts' with the sample-subsetted SPE object
+#   cell_counts <- cell_counts[
+#     match(
+#       colnames(spe_nonIF[, spe_nonIF$sample_id == sample_id]),
+#       cell_counts$barcode
+#     ),
+#   ]
+#   
+#   #   Add this sample's counts to the SPE object
+#   spe_nonIF$count[spe_nonIF$sample_id == sample_id] <- cell_counts$Nmask_dark_blue
+# }
+# 
+# #   Ensure counts were read in for all spots in the object
+# if (any(is.na(spe_nonIF$count))) {
+#   stop("Did not find cell counts for all non-IF spots.")
+# }
 
 #-------------------------------------------------------------------------------
 #   Convert snRNA-seq and spatial R objects to AnnData python objects
@@ -150,8 +148,8 @@ if (any(is.na(spe_nonIF$count))) {
 #   zellkonverter doesn't know how to convert the 'spatialCoords' slot. We'd
 #   ultimately like the spatialCoords in the .obsm['spatial'] slot of the
 #   resulting AnnDatas, which corresponds to reducedDims(spe)$spatial in R
-reducedDims(spe_IF)$spatial <- spatialCoords(spe_IF)
-reducedDims(spe_nonIF)$spatial <- spatialCoords(spe_nonIF)
+#reducedDims(spe_IF)$spatial <- spatialCoords(spe_IF)
+reducedDims(spe_HE)$spatial <- spatialCoords(spe_HE)
 
 #   Use Ensembl gene IDs for rownames (not gene symbol)
 rownames(sce) <- rowData(sce)$gene_id
@@ -167,7 +165,7 @@ write_anndata(sce, sce_out)
 #   need only be saved once
 if (cell_group == "broad") {
   # write_anndata(spe_IF, spe_IF_out)
-  write_anndata(spe_nonIF, spe_nonIF_out)
+  write_anndata(spe_HE, spe_HE_out)
   
   #   Write sample names to text files
   # writeLines(unique(spe_IF$sample_id), con = sample_IF_out)
@@ -180,8 +178,9 @@ gc()
 #-------------------------------------------------------------------------------
 
 #   We won't consider genes that aren't in both the spatial objects
-keep <- (rowData(sce)$gene_id %in% rowData(spe_IF)$gene_id) &
-  (rowData(sce)$gene_id %in% rowData(spe_nonIF)$gene_id)
+# keep <- (rowData(sce)$gene_id %in% rowData(spe_IF)$gene_id) &
+#   (rowData(sce)$gene_id %in% rowData(spe_HE)$gene_id)
+keep = (rowData(sce)$gene_id %in% rowData(spe_HE)$gene_id)
 sce <- sce[keep, ]
 
 perc_keep <- 100 * (1 - length(which(keep)) / length(keep))
@@ -200,7 +199,7 @@ marker_stats <- get_mean_ratio2(
 marker_stats_1vall <- findMarkers_1vAll(
   sce,
   cellType_col = cell_type_var, assay_name = "logcounts",
-  mod = "~BrNum"
+  mod = "~brnum"
 )
 marker_stats <- left_join(
   marker_stats, marker_stats_1vall,
