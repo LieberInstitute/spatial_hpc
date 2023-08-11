1ae08bdb3d2a25b463fc29043b0540d511fce66b
madhavitippani <madhavitippani28@gmail.com>
2023-08-10 20:24:24 -0400

    write anndata function
    Former-commit-id: fdf737773f3a6d51f91c663ab5638e7acdbec94e

---
+++ code/spot_deconvo/shared_utilities/write_anndata.R
@@ -0,0 +1,26 @@
+###############################################################################
+#  Functions
+###############################################################################
+
+write_anndata <- function(sce, out_path) {
+  invisible(
+    basiliskRun(
+      fun = function(sce, filename) {
+        library("zellkonverter")
+        library("reticulate")
+        
+        # Convert SCE to AnnData:
+        adata <- SCE2AnnData(sce)
+        
+        #  Write AnnData object to disk
+        adata$write(filename = filename)
+        
+        return()
+      },
+      env = zellkonverterAnnDataEnv(),
+      sce = sce,
+      filename = out_path
+    )
+  )
+}
+
