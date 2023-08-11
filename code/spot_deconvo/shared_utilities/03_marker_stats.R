aac1b23a9cc8f5be47fae3dd7d729cc6ae10b04f
madhavitippani <madhavitippani28@gmail.com>
2023-08-11 18:03:35 -0400

    combine marker_stats and find_markers
    Former-commit-id: f741e8f38267b0e6853bd277645f6790c48993f7

--- code/spot_deconvo/shared_utilities/03_marker_stats.R
+++ code/spot_deconvo/shared_utilities/03_marker_stats.R
@@ -6,6 +6,51 @@ suppressPackageStartupMessages(library("DeconvoBuddies"))
 suppressPackageStartupMessages(library("sessioninfo"))
 suppressPackageStartupMessages(library("here"))
 suppressPackageStartupMessages(library("tidyverse"))
+suppressPackageStartupMessages(library("HDF5Array"))
+
+sce_in <- "/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/snRNAseq_hpc/processed-data/sce/sce_final.rda"
+spe_in <- here("processed-data","02_build_spe","spe_nmf_final.rda")
+#spg_in <- here("processed-data", "02_build_spe", "spe_nmf_final.rda")
+
+load(sce_in, verbose = TRUE)
+rownames(sce) <- rowData(sce)$gene_id
+
+# spg <- readRDS(spe_IF_in)
+load(spe_in, verbose = TRUE)
+reducedDims(spe)$spatial <- spatialCoords(spe)
+#reducedDims(spg)$spatial <- spatialCoords(spg)
+
+#cell_group = "broad" 
+ cell_group = "layer" 
+ 
+out = here("processed-data","spot_deconvo","shared_utilities",paste0("marker_stats_",cell_group,".rds"))
+
+n_markers_per_type <- 25
+
+#   Define variables related to cell_group
+
+if (cell_group == "broad") {
+  cell_type_var <- "broad.type"
+} else {
+  cell_type_var <- "cell.type"
+}
+
+
+if (cell_group == "broad") {
+  cell_types <- c('ExcN', 'InhN', 'Glia', 'Immune', 'CSF', 'Vascular')
+  colors_col <- "cell_type_colors_broad"
+  cell_column <- "broad.type"
+  cell_type_nrow <- 2
+} else {
+  cell_types <- c('GC', 'CA2-4', 'CA1', 'ProS/Sub', 'L2/3', 'L5', 'L6/6b', 'HATA/AHi',
+                  'Thal', 'Cajal', 'GABA', 'Oligo', 'Astro', 'OPC', 'Micro/Macro/T',
+                  'Ependy', 'Choroid', 'Vascular')
+  colors_col <- "cell_type_colors_layer"
+  cell_column <- "cell.type"
+  cell_type_nrow <- 3
+}
+
+print(paste0("Running script at ", cell_group, "-resolution."))
 
 #-------------------------------------------------------------------------------
 #   Rank marker genes
@@ -14,25 +59,6 @@ suppressPackageStartupMessages(library("tidyverse"))
 #   We won't consider genes that aren't in both the spatial objects
 # keep <- (rowData(sce)$gene_id %in% rowData(spe_IF)$gene_id) &
 #   (rowData(sce)$gene_id %in% rowData(spe_HE)$gene_id)
-
-sce_in <- "/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/snRNAseq_hpc/processed-data/sce/sce_final.rda"
-spe_HE_in <- here("processed-data","02_build_spe","spe_nmf_final.rda")
-#spe_IF_in <- here("processed-data", "02_build_spe", "spe_nmf_final.rda")
-
-load(sce_in, verbose = TRUE)
-# spg <- readRDS(spe_IF_in)
-load(spe_HE_in, verbose = TRUE)
-
-#cell_group = "broad" 
- cell_group = "layer" 
-
-out = here("processed-data","spot_deconvo","shared_utilities",paste0("marker_stats_",cell_group,".rds"))
-
-reducedDims(spe)$spatial <- spatialCoords(spe)
-#reducedDims(spg)$spatial <- spatialCoords(spg)
-
-#   Use Ensembl gene IDs for rownames (not gene symbol)
-rownames(sce) <- rowData(sce)$gene_id
 
 keep = (rowData(sce)$gene_id %in% rowData(spe)$gene_id)
 sce <- sce[keep, ]
@@ -44,13 +70,6 @@ print(
     "that were not present in all the spatial data"
   )
 )
-
-if (cell_group == "broad") {
-  cell_type_var <- "broad.type"
-} else {
-  cell_type_var <- "cell.type"
-}
-
 
 print("Running getMeanRatio2 and findMarkers_1vAll to rank genes as markers...")
 marker_stats <- get_mean_ratio2(
@@ -66,5 +85,49 @@ marker_stats <- left_join(
   marker_stats, marker_stats_1vall,
   by = c("gene", "cellType.target")
 )
+
+#-------------------------------------------------------------------------------
+#   Filter out mitochondrial genes and re-rank 'rank_ratio' values. Add gene symbols to 'marker_stats' object
+#-------------------------------------------------------------------------------
+source(here("code", "spot_deconvo", "shared_utilities", "shared_function.R"))
+#   Add gene symbol
+marker_stats$symbol <- rowData(sce)$gene_name[match(marker_stats$gene, rownames(sce))]
+
+#   Filter out mitochondrial genes
+marker_stats <- marker_stats[!grepl("^MT-", marker_stats$symbol), ]
+
+#   Change "/" to "_" for layer-level cell types
+marker_stats <- marker_stats |>
+  mutate(
+    cellType.target = gsub("/", "_", cellType.target),
+    cellType = gsub("/", "_", cellType)
+  )
+stopifnot(
+  identical(sort(unique(marker_stats$cellType.target)), sort(cell_types))
+)
+
+#   "Re-rank" rank_ratio, since there may be missing ranks now
+for (ct in cell_types) {
+  old_ranks <- marker_stats |>
+    filter(cellType.target == ct) |>
+    pull(rank_ratio) |>
+    sort()
+  
+  for (i in 1:length(which((marker_stats$cellType.target == ct)))) {
+    index <- which(
+      (marker_stats$cellType.target == ct) &
+        (marker_stats$rank_ratio == old_ranks[i])
+    )
+    stopifnot(length(index) == 1)
+    marker_stats[index, "rank_ratio"] <- i
+  }
+}
+
+###############################################################################
+#  Subset and write markers
+###############################################################################
+source(here("code","spot_deconvo","shared_utilities","plottingfunctions.R"))
+print("Writing markers...")
+write_markers(n_markers_per_type, marker_out)
 
 saveRDS(marker_stats, out)
