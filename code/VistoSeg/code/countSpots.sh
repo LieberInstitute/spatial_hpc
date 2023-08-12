ff217f5673122623584be022de820b84602f4e2c
madhavitippani <madhavitippani28@gmail.com>
2023-08-12 16:44:23 -0400

    update counSpots.sh
    Former-commit-id: 703dce95d0b21c185ab11ec3015b35097ea4be75

--- code/VistoSeg/code/countSpots.sh
+++ code/VistoSeg/code/countSpots.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #$ -cwd
-#$ -l mem_free=20G,h_vmem=20G,h_fsize=100G
-#$ -pe local 5
+#$ -l mem_free=40G,h_vmem=40G,h_fsize=100G
 #$ -o /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/VistoSeg/code/logs/countSpots.$TASK_ID.txt
 #$ -e /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/VistoSeg/code/logs/countSpots.$TASK_ID.txt
 #$ -m e
@@ -20,25 +19,23 @@ echo "Job name: ${JOB_NAME}"
 echo "Hostname: ${HOSTNAME}"
 echo "Task id: ${SGE_TASK_ID}"
 echo "****"
-echo "Sample id: $(cat /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/VistoSeg/code/ALLsamples.txt | awk '{print $1}' | awk "NR==${SGE_TASK_ID}") "
+echo "Sample id: $(cat /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/VistoSeg/code/samples.txt | awk '{print $2}' | awk "NR==${SGE_TASK_ID}") "
 echo "****"
 
 module load matlab/R2019a
 
 toolbox='/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/VistoSeg/code'
-mask=$(cat /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/VistoSeg/code/ALLsamples.txt | awk '{print $1}' | awk "NR==${SGE_TASK_ID}")
-mask=$(echo ${mask} | cut -d "." -f 1)
+path='/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/Images/VistoSeg/Capture_areas/'
+sample=$(cat /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/VistoSeg/code/samples.txt | awk '{print $2}' | awk "NR==${SGE_TASK_ID}")
 add="_nuclei_WS_final.mat"
-maskname=$mask$add
+maskname=$path$sample$add
 
-path1=$(cat /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/VistoSeg/code/ALLsamples.txt |  awk '{print $3}' | awk "NR==${SGE_TASK_ID}")
-sample=$(echo ${mask} | cut -d "/" -f 11)
+path1=$(cat /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/VistoSeg/code/samples.txt |  awk '{print $1}' | awk "NR==${SGE_TASK_ID}")
+rest='outs/spatial/scalefactors_json.json'
+jsonname=$path1$rest
 
-rest='/outs/spatial/scalefactors_json.json'
-jsonname=$path1$sample$rest
-
-rest1='/outs/spatial/tissue_positions_list.csv'
-posname=$path1$sample$rest1
+rest1='outs/spatial/tissue_positions_list.csv'
+posname=$path1$rest1
 
 matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), countNuclei('$maskname','$jsonname','$posname')"
 
