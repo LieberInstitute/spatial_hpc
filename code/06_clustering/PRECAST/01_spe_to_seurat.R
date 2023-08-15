edd6e31e0300e6a9e9f17c447e5d43ec25aa7c7c
Erik Nelson <erik.nelson116@gmail.com>
2023-08-15 17:22:48 -0400

    updating code for Seurat conversion
    Former-commit-id: dc67f3506fd7897491a3260ae9f10f92a94fcbf4

--- code/06_clustering/PRECAST/01_spe_to_seurat.R
+++ code/06_clustering/PRECAST/01_spe_to_seurat.R
@@ -6,17 +6,67 @@ library("Seurat")
 library("SeuratData")
 })
 
-load(file = here::here("processed-data", "04_QC", "spe_QC_allSamples.Rdata"))
+load(file = here::here("processed-data", "05_preprocess_batchCorrection", "spe_norm.rda"))
 source(file = here::here("code", "06_clustering", "BayesSpace", "preprocess_harmony", "offset_check.R"))
-x = offset_check(spe)
-
-## check
+x<-spe
+#x = offset_check(spe)
+#
+### check
 # df <- cbind.data.frame(colData(x), spatialCoords(x))
 # ggplot(df, aes(x = row, y = col, color = sample_id)) +
 # geom_point(size = 1) +
 # coord_fixed() +
 # guides(color = guide_legend(override.aes = list(size = 3))) +
 # theme_bw()
+#  
+#x$spot_id = x$key
+#colData(x)$row<-colData(x)$array_row
+#colData(x)$col<-colData(x)$array_col
+
+sue <- CreateSeuratObject(
+      counts=as.matrix(counts(x)),
+      meta.data=data.frame(colData(x)),
+      project="HPC")
+
+seuList = list()
+
+samples = unique(x$sample_id)
+table(x$brnum, x$sample_id)
+
+for (i in seq_along(samples)){
+seuList[[i]] = subset(x=sue, subset = sample_id == samples[i])
+}
+
+save(seuList, file = here("processed-data", "06_clustering", "PRECAST", "seuList_counts.Rdata"))
+
+sue <- CreateSeuratObject(
+      counts=as.matrix(logcounts(x)),
+      meta.data=data.frame(colData(x)),
+      project="HPC")
+
+seuList = list()
+
+samples = unique(x$sample_id)
+table(x$brnum, x$sample_id)
+
+for (i in seq_along(samples)){
+seuList[[i]] = subset(x=sue, subset = sample_id == samples[i])
+}
+
+save(seuList, file = here("processed-data", "06_clustering", "PRECAST", "seuList_logcounts.Rdata"))
+
+
+source(file = here::here("code", "06_clustering", "BayesSpace", "preprocess_harmony", "offset_check.R"))
+x<-spe
+x = offset_check(spe)
+
+## check
+ df <- cbind.data.frame(colData(x), spatialCoords(x))
+ ggplot(df, aes(x = row, y = col, color = sample_id)) +
+ geom_point(size = 1) +
+ coord_fixed() +
+ guides(color = guide_legend(override.aes = list(size = 3))) +
+ theme_bw()
   
 x$spot_id = x$key
 
@@ -34,4 +84,4 @@ for (i in seq_along(brains)){
 seuList[[i]] = subset(x=sue, subset = brnum == brains[i])
 }
 
-save(seuList, file = here("processed-data", "06_clustering", "PRECAST", "seuList.Rdata"))
+save(seuList, file = here("processed-data", "06_clustering", "PRECAST", "seuList_counts_brnum.Rdata"))
