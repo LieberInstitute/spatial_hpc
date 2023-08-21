3019234dcb4a2c45140d6ffb93147de6fafffbd9
madhavitippani <madhavitippani28@gmail.com>
2023-08-21 12:09:00 -0400

    move cellpose files to respective folders
    Former-commit-id: 4df29e9cded04b91d6224bf3b6dcb58aad97abd4

--- code/spot_deconvo/cellpose/make_test.m
+++ code/spot_deconvo/cellpose/make_test.m
@@ -1,3 +1,13 @@
+myfiles = dir('/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/images/VistoSeg/VSPG/*.mat');
+
+for i=1:numel(myfiles)
+    fname = fullfile(myfiles(i).folder,myfiles(i).name);
+    load(fname)
+    imwrite(DAPI,[fname(1:end-4),'_DAPI.tif'])
+end
+
+
+%% make test samples with smaller size
 fname = '/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/Images/VistoSeg/VSPG/V12D07-335_D1_DAPI.tif'; 
 cd /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/Images/VistoSeg/VSPG/cellpose_test
 img = imread(fname);
@@ -11,3 +21,14 @@ end
 for i = 1:56                         
 imwrite(A{i},sprintf('test_%d.tif',i))
 end
+
+%% run cellpose interactively
+%% move files to respective folders
+
+movefile /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/Images/VistoSeg/VSPG/cellpose_test/models/* /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/spot_deconvo/cellpose/maddy_models/
+movefile /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/Images/VistoSeg/VSPG/cellpose_test/* /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/spot_deconvo/cellpose/maddy_models/
+movefile /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/Images/VistoSeg/VSPG/*.npy /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/spot_deconvo/cellpose/final_masks
+movefile /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/Images/VistoSeg/VSPG/*.png /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/spot_deconvo/cellpose/final_masks
+movefile /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/Images/VistoSeg/VSPG/*1.tif /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/spot_deconvo/samui/
+movefile /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/Images/VistoSeg/VSPG/*DAPI.tif /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/spot_deconvo/cellpose/final_masks
+
