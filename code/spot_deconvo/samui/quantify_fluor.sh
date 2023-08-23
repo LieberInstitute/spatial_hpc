efa521b22f6d764ee0373d3f5c128d2ec40c6cbc
madhavitippani <madhavitippani28@gmail.com>
2023-08-23 15:56:20 -0400

    submit quantify_fluor.sh
    Former-commit-id: 8593628bbb05a83b82ca3d1415d580fe6f261086

---
+++ code/spot_deconvo/samui/quantify_fluor.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#$ -cwd
+#$ -N "quantify_fluor"
+#$ -o /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/spot_deconvo/samui/logs/quantify_fluor_$TASK_ID.log
+#$ -e /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/spot_deconvo/samui/logs/quantify_fluor_$TASK_ID.log
+#$ -l mf=20G,h_vmem=20G
+#$ -t 1-8
+#$ -tc 4
+
+echo "**** Job starts ****"
+date
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
+module load cellpose/2.0
+python quantify_fluor.py
+
+echo "**** Job ends ****"
+date
