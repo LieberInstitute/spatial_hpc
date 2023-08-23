4d7a9e8b521cc6a7c8849a3c96b6952d6dceebaf
madhavitippani <madhavitippani28@gmail.com>
2023-08-23 15:52:51 -0400

    quantify intensities from different channels using cellpose masks
    Former-commit-id: d04bd2dc83e9dffc3d207526d196d6984295e217

---
+++ code/spot_deconvo/samui/quantify_fluor.py
@@ -0,0 +1,90 @@
+#   Given masks from cellpose (from segmenting the DAPI channel of Visium IF
+#   images), quantify mean fluorescence in each non-DAPI channel within each
+#   nucleus (dilated to include a region around each nucleus), and save a pandas
+#   DataFrame with these values for each nucleus.
+
+import os
+os.chdir('/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/')
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.random import default_rng
+import pandas as pd
+import seaborn as sns
+import tifffile
+from scipy.spatial import KDTree
+from scipy import ndimage
+from skimage.measure import regionprops, regionprops_table
+import pyhere
+from pathlib import Path
+import json
+
+################################################################################
+#   Variable definitions
+################################################################################
+
+#-------------------------------------------------------------------------------
+#   Paths
+#-------------------------------------------------------------------------------
+
+spaceranger_dirs = pd.read_csv(pyhere.here("code","spot_deconvo","shared_utilities","samples.txt"), sep = '\t', header=None, names = ['SPpath', 'sample_id', 'brain'])
+spaceranger_dirs = spaceranger_dirs.iloc[36:].reset_index(drop=True)
+
+img_path = pyhere.here('processed-data', 'Images', 'VistoSeg', 'VSPG', '{}.tif')
+mask_path = pyhere.here('processed-data', 'spot_deconvo', 'cellpose', 'final_masks', '{}_DAPI_seg.npy')
+spot_path = pyhere.here(spaceranger_dirs.SPpath, 'outs', 'spatial', 'tissue_positions.csv')
+scale_path = pyhere.here(spaceranger_dirs.SPpath, 'outs', 'spatial','scalefactors_json.json')
+
+out_df_path = pyhere.here('processed-data', 'spot_deconvo', 'samui')
+plot_dir = pyhere.here("plots", "spot_deconvo", "cellpose", "count_cells")
+Path(plot_dir).mkdir(parents=True, exist_ok=True)
+
+#-------------------------------------------------------------------------------
+#   Dataset-specific variables
+#-------------------------------------------------------------------------------
+
+names = {0: "DAPI", 1: "NeuN", 2: "TMEM119", 3: "GFAP", 4: "OLIG2", 5: "LIP"}
+plot_file_type = 'png' # 'pdf' is also supported for higher-quality plots
+
+
+################################################################################
+#   Analysis
+################################################################################
+
+# os.environ['SGE_TASK_ID'] = '1'
+
+#-------------------------------------------------------------------------------
+#   Read in sample info and adjust paths for this particular sample ID
+#-------------------------------------------------------------------------------
+
+#   Different sample IDs are used for different files associated with each
+#   sample. Determine both forms of the sample ID for this sample and update
+#   path variables accordingly
+
+sample_id_img = spaceranger_dirs.sample_id[int(os.environ['SGE_TASK_ID']) - 1]
+img_path = str(img_path).format(sample_id_img)
+mask_path = str(mask_path).format(sample_id_img)
+
+spot_path = str(spot_path[int(os.environ['SGE_TASK_ID']) - 1])
+out_df_path = pyhere.here(out_df_path,str(sample_id_img + '_df.csv'))
+
+Path(out_df_path).parents[0].mkdir(parents=True, exist_ok=True)
+
+#   Path to JSON from spaceranger including spot size for this sample
+json_path = scale_path[int(os.environ['SGE_TASK_ID']) - 1]
+with open(json_path) as f: 
+    json_data = json.load(f)
+
+#-------------------------------------------------------------------------------
+#   Read in spot data
+#-------------------------------------------------------------------------------
+
+#   Read in all spot data
+raw = pd.read_csv(spot_path,header=None,names=["barcode", "included", "row", "col", "x", "y"],)
+
+#   Take only spots that overlap tissue
+raw = raw.iloc[raw.included[raw.included == 1].index].reset_index().drop(columns=["included", "index"])
+
+#-------------------------------------------------------------------------------
+#   Quantify mean fluorescence for each channel at each nucleus
+#-------------------------------------------------------------------------------
+
