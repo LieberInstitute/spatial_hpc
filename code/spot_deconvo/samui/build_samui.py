819f3267eefbb7222eba9b7740e320e5b8f3c5cf
madhavitippani <madhavitippani28@gmail.com>
2023-08-23 16:00:37 -0400

    add default channels and colors in build_samui.py
    Former-commit-id: 14b197d0762f0a50ee4f90b5925742d08a5ef7c2

---
+++ code/spot_deconvo/samui/build_samui.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+import os
+os.chdir('/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/')
+from pyhere import here
+import json
+
+import scanpy as sc
+
+import numpy as np
+import pandas as pd
+from rasterio import Affine
+
+from loopy.sample import Sample
+from loopy.utils.utils import remove_dupes, Url
+
+spot_diameter_m = 55e-6 # 55-micrometer diameter for Visium spot
+img_channels = ['DAPI', 'Alexa_488', 'Alexa_555', 'Alexa_594', 'Alexa_647', 'Autofluorescence', 'segmented_DAPI']
+default_channels = {'blue': 'DAPI', 'green': 'Alexa_488', 'yellow': 'Alexa_555', 'red': 'Alexa_594', 'magenta': 'Alexa647', 'cyan': 'Autofluorescence', 'white': 'segmented_DAPI'}
+#default_gene = 'PPFIA2'
+
+#   Names of continuous features expected to be columns in the observation data (colData) of the AnnData
