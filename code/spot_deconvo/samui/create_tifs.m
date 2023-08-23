adbfcc2b6c097b8860c9d162555e4f6d77c9185e
madhavitippani <madhavitippani28@gmail.com>
2023-08-23 15:58:56 -0400

    permissions error when creating tiffs for samui
    Former-commit-id: 5e5f14d822a820d3e2aaa4cd1bf17883574c7044

--- code/spot_deconvo/samui/create_tifs.m
+++ code/spot_deconvo/samui/create_tifs.m
@@ -1,9 +1,15 @@
+copyfile /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/Images/VistoSeg/VSPG/*1.tif /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/spot_deconvo/samui/
+%scp /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/Images/VistoSeg/VSPG/*1.tif /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/spot_deconvo/samui/
 
-myfiles = dir('/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/spot_deconvo/samui/*1.tif');
 
+myfiles = dir('/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/spot_deconvo/cellpose/final_masks/*.png');
 
-fname='';
+for i = 1:numel(myfiles)
+fname=fullfile(myfiles(i).folder, myfiles(i).name);
 BW = imread(fname);
+BW = label2rgb(BW,'hsv','w','shuffle');
+Or = '/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/samui/';
+imwrite(BW,fullfile(Or, [myfiles(i).name(1:end-18),'.tif']),"WriteMode","append")
+disp(i)
+end
 
-
-imwrite(mat2gray(I1.(O{1})),[fname(1:end-4),'_A1.tif'])
