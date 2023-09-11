3c670c31b22d310d01d8cc4c86b691046d15b16c
madhavitippani <madhavitippani28@gmail.com>
2023-09-11 14:47:05 -0400

    subtract Autofluorescence from TMEM and remake tifs for samui
    Former-commit-id: b79ecfe6e975a60fc1152e92481d0846fcb5b68c

--- code/spot_deconvo/groundTruth/02_samui_manual_annotation/create_tifs.m
+++ code/spot_deconvo/groundTruth/02_samui_manual_annotation/create_tifs.m
@@ -2,6 +2,22 @@ copyfile /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-dat
 %scp /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/Images/VistoSeg/VSPG/*1.tif /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/spot_deconvo/groundTruth/02_samui_manual_annotation/
 
 
+%% subtract autofluorescence from TMEM
+myfiles = dir('/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/Images/VistoSeg/VSPG/*1.mat');
+for i = 1:numel(myfiles)
+    fname=fullfile(myfiles(i).folder, myfiles(i).name);
+    load(fname);
+    Alexa_555 = Alexa_555-Autofluorescence;
+    fname1=fullfile('/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/spot_deconvo/groundTruth/02_samui_manual_annotation/', myfiles(i).name);
+    imwrite(mat2gray(DAPI),[fname1(1:end-4),'.tif'])
+    imwrite(mat2gray(Alexa_488),[fname1(1:end-4),'.tif'])
+    imwrite(mat2gray(Alexa_555),[fname1(1:end-4),'.tif'])
+    imwrite(mat2gray(Alexa_594),[fname1(1:end-4),'.tif'])
+    imwrite(mat2gray(Alexa_647),[fname1(1:end-4),'.tif'])
+    imwrite(mat2gray(Autofluorescence),[fname1(1:end-4),'.tif'])
+end
+
+%% attach segmented image to raw image
 myfiles = dir('/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/spot_deconvo/groundTruth/01_cellpose/final_masks/*.png');
 
 for i = 1:numel(myfiles)
