c12c12b795b92224d6ec8239d72b4f06e0648ed1
madhavitippani <madhavitippani28@gmail.com>
2023-09-18 17:48:44 -0400

    test 03_runRCTD.R on 1 sample
    Former-commit-id: a49e41ebf30590a9a20ac3878e5a71fd13facb70

---
+++ code/spot_deconvo/RCTD/03_runRCTD.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#$ -cwd
+#$ -N "runRCTD"
+#$ -o /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/spot_deconvo/RCTD/logs/03_runRCTD_HE_broad_newClass_RCTDmarkers$TASK_ID.log
+#$ -e /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/spot_deconvo/RCTD/logs/03_runRCTD_HE_broad_newClass_RCTDmarkers$TASK_ID.log
+#$ -l caracol,mf=100G,h_vmem=100G,h_fsize=100G
+#$ -t 1
+#$ -tc 8
+
+USE_SLURM=2
+
+if [[ $USE_SLURM -eq 1 ]]; then
+    job_id=$SLURM_JOB_ID
+    job_name=$SLURM_JOB_NAME
+	task_id=$SLURM_ARRAY_TASK_ID
+	hostname=$SLURMD_NODENAME
+else
+    job_id=$JOB_ID
+    job_name=$JOB_NAME
+	task_id=$SGE_TASK_ID
+	hostname=$HOSTNAME
+fi
+
+
+echo "**** Job starts ****"
+date
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${job_id}"
+echo "Job name: ${job_name}"
+echo "Hostname: ${hostname}"
+echo "Task id: ${task_id}"
+
+module load conda_R/devel
+Rscript 03_runRCTD.R
+
+echo "**** Job ends ****"
+date
