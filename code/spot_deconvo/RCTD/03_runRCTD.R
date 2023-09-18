86f4d893e3947e7815b9de045cc1cb479d82dc16
madhavitippani <madhavitippani28@gmail.com>
2023-09-18 15:25:10 -0400

    explore RCTD doublet mode results
    Former-commit-id: f8a63b059c2aac682dd78ecca2f27c5156e0f0ec

--- code/spot_deconvo/RCTD/03_runRCTD.R
+++ code/spot_deconvo/RCTD/03_runRCTD.R
@@ -30,6 +30,16 @@ sample_id = sample_ids[as.numeric(Sys.getenv("SGE_TASK_ID"))]
 myRCTD = readRDS(here(Dr,sample_id,"myRCTD.rds"))
 celltypes = levels(myRCTD@reference@cell_types)
 
+# results <- myRCTD@results
+# norm_weights = normalize_weights(results$weights)
+# cell_type_names <- myRCTD@cell_type_info$info[[2]] #list of cell type names
+# spatialRNA <- myRCTD@spatialRNA
+# resultsdir <- 'RCTD_Plots' ## you may change this to a more accessible directory on your computer.
+# dir.create(resultsdir)
+# 
+# plot_all_cell_types(results$results_df, spatialRNA@coords, cell_type_names, resultsdir)
+# 
+
 myRCTD1 <- run.RCTD(myRCTD, doublet_mode = 'multi')
 saveRDS(myRCTD1, here(Dr,sample_id,paste0(sample_id,"_multi.rds")))
 
