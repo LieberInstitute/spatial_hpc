197960571435b7e51a93cb2def5ee98bb1c3e2f8
madhavitippani <madhavitippani28@gmail.com>
2023-09-18 14:43:29 -0400

    /cell2location/HE/2ndRun_newClass/layer results
    Former-commit-id: 22e695f45a4d2848b3b848566883d2826dc8c03e

--- code/spot_deconvo/RCTD/02_prepare_myRCTD.R
+++ code/spot_deconvo/RCTD/02_prepare_myRCTD.R
@@ -17,28 +17,28 @@ spaceranger_dirs = read.csv(file.path(here::here("code","VistoSeg","code","sampl
 spaceranger_dirs = spaceranger_dirs[1:36,]
 sample_ids = spaceranger_dirs$sample_id
 
-sample_id = sample_ids[Sys.getenv('SGE_TASK_ID')]
+sample_id = sample_ids[as.numeric(Sys.getenv('TASK_ID'))]
 print(sample_id)
 
-spe = readRDS(here(Dr,"spe.rds"))
-speb = spe[,which(spe$sample_id==sample_id)]
-
-counts = assays(speb)$counts
-rownames(counts) = rowData(speb)$gene_id
-coords = as.data.frame(spatialCoords(speb))
-colnames(coords) = c("ycoord","xcoord")
-nUMI <- colSums(counts) # In this case, total counts per pixel is nUMI
-
-### Create SpatialRNA object
-puck <- SpatialRNA(coords, counts, nUMI)
-
-### plotting
-barcodes <- colnames(puck@counts) # 
-dir.create(here(plots_out, cell_group, sample_id))
-pdf(here(plots_out, cell_group, sample_id, "UMIcount.pdf"), width = 10, height = 10)
-plot_puck_continuous(puck, barcodes, puck@nUMI, ylimit = c(0,round(quantile(puck@nUMI,0.9))), title ='plot of nUMI') 
-
-myRCTD <- create.RCTD(puck, reference, max_cores = 1, MAX_MULTI_TYPES = 5)
-
-dir.create(here(processed_out,cell_group,sample_id))
-saveRDS(myRCTD, here(processed_out,cell_group,sample_id,'myRCTD.rds'))
+# spe = readRDS(here(Dr,"spe.rds"))
+# speb = spe[,which(spe$sample_id==sample_id)]
+# 
+# counts = assays(speb)$counts
+# rownames(counts) = rowData(speb)$gene_id
+# coords = as.data.frame(spatialCoords(speb))
+# colnames(coords) = c("ycoord","xcoord")
+# nUMI <- colSums(counts) # In this case, total counts per pixel is nUMI
+# 
+# ### Create SpatialRNA object
+# puck <- SpatialRNA(coords, counts, nUMI)
+# 
+# ### plotting
+# barcodes <- colnames(puck@counts) # 
+# dir.create(here(plots_out, cell_group, sample_id))
+# pdf(here(plots_out, cell_group, sample_id, "UMIcount.pdf"), width = 10, height = 10)
+# plot_puck_continuous(puck, barcodes, puck@nUMI, ylimit = c(0,round(quantile(puck@nUMI,0.9))), title ='plot of nUMI') 
+# 
+# myRCTD <- create.RCTD(puck, reference, max_cores = 1, MAX_MULTI_TYPES = 5)
+# 
+# dir.create(here(processed_out,cell_group,sample_id))
+# saveRDS(myRCTD, here(processed_out,cell_group,sample_id,'myRCTD.rds'))
