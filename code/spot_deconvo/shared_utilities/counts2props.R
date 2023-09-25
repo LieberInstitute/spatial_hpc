3d00f9a1dd235ae6a5f05a8bad70322b62c0c351
madhavitippani <madhavitippani28@gmail.com>
2023-09-25 13:54:24 -0400

    final code to convert counts to props and build boxplots
    Former-commit-id: fadc4371f701b4c34d62ad149d46c4b238f937a0

--- code/spot_deconvo/shared_utilities/counts2props.R
+++ code/spot_deconvo/shared_utilities/counts2props.R
@@ -4,9 +4,10 @@ library("sessioninfo")
 library("SpatialExperiment")
 library('ggplot2')
 library('dplyr')
+library('gridExtra')
 
 spe = readRDS(here("processed-data", "spot_deconvo", "shared_utilities", "spe.rds"))
-dat = as.data.frame(colData(spe)) %>% select("key", "cluster_collapsed")
+dat = as.data.frame(colData(spe)) %>% select("key", "cluster_collapsed", "array_row", "array_col", "sample_id")
 
 ## cell2location
 tool =  "cell2location"
@@ -38,7 +39,6 @@ cell2location <- cbind(dat, counts_info)
 ## tangram
 tool =  "tangram"
 Dr = here("processed-data", "spot_deconvo",tool, "HE", "2ndRun_newClass", "layer")
-Dr = here("processed-data", "spot_deconvo",tool, "HE", "2ndRun_newClass", "layer")
 csv_files = data.frame(files = list.files(Dr, pattern = "clusters.csv" , recursive = TRUE, full.names = TRUE))
 
 csv_files$sample_id = sapply(strsplit(csv_files$files,"/"), `[`, 13)
@@ -60,7 +60,7 @@ for (celltype in celltypes) {
   counts[,celltype] = temp_df[,celltype]/temp_df$count
 }
 
-which(is.na(counts), arr.ind=TRUE)
+#which(is.na(counts), arr.ind=TRUE)
 counts <- replace(counts, is.na(counts), 0)
 counts$key <- gsub("Br2720", "V12F14-051", counts$key)
 which(is.na(counts), arr.ind=TRUE)
@@ -71,14 +71,73 @@ which(is.na(counts_info), arr.ind=TRUE)
 
 counts_info <- subset(counts_info, select = -key)
 tangram <- cbind(dat, counts_info)
-tangram$tool = "tangram"
+tangram$tool = tool
 
 column_order <- colnames(cell2location)
 tangram <- tangram[, column_order]
 
-dat = rbind(cell2location, tangram)
+## RCTD
+tool =  "RCTD"
+Dr = here("processed-data", "spot_deconvo",tool,"2ndRun_newClass_RCTDmarkers", "layer")
+csv_files = data.frame(files = list.files(Dr, pattern = "clusters.csv" , recursive = TRUE, full.names = TRUE))
+
+csv_files$sample_id = sapply(strsplit(csv_files$files,"/"), `[`, 12)
+counts_list <- list()
+for (file_path in csv_files$files) {
+  data <- read.csv(file_path, row.names = NULL)  # Read the CSV file
+  counts_list[[file_path]] <- data  # Store the data frame in the list with the file path as the key
+}
+
+temp_df <- do.call(rbind, counts_list)
+rownames(temp_df) <- NULL
+#colnames(temp_df)[colnames(temp_df) == "CA2.4"] = "CA2-4"
+colnames(temp_df)[colnames(temp_df) == "X"] = "key"
+celltypes = names(temp_df)[2:length(names(temp_df))]
+which(is.na(temp_df), arr.ind=TRUE)
+
+setdiff(temp_df$key, dat$key)
+temp_df$key <- gsub("Br2720", "V12F14-051", temp_df$key)
+setdiff(temp_df$key, dat$key)
+setdiff(dat$key,temp_df$key)
+
+#temp_df$slide = sapply(strsplit(temp_df$key,"_"), `[`, 2)
+counts_match <- match(dat$key, temp_df$key)
+counts_info <-temp_df[counts_match,]
+which(is.na(counts_info), arr.ind=TRUE)
+counts_info$tool = tool
+counts_info <- subset(counts_info, select = -key)
+RCTD <- cbind(dat, counts_info)
+
+column_order <- colnames(cell2location)
+RCTD <- RCTD[, column_order]
+
+dat1 = rbind(cell2location, tangram, RCTD)
+rmv = c("V11L05-335_B1", "V11U08-081_A1", "V11U08-081_C1", "V11U08-081_D1", "V11U08-084_D1")
+dat1 = dat1[!dat1$sample_id %in% rmv, ]  
+which(is.na(dat1), arr.ind=TRUE)
 
 ## for box plots
+plot_list <- lapply(celltypes, function(i){
+  ggplot(dat1, aes(x = dat1$cluster_collapsed, y = dat1[,i])) + geom_boxplot(aes(fill=tool), outlier.shape = NA)+labs(title = i)
+})
 
-ggplot(dat, aes(x = dat$cluster_collapsed, y = dat$GC)) + geom_boxplot(aes(fill=tool))
+ggsave(here("plots","spot_deconvo","shared_utilities","layer","boxplot.pdf"), plot = marrangeGrob(plot_list, nrow=1, ncol=1),  width = 24, height = 8)
+
+## proportion plots
+celltypes = colnames(dat1)[6:26]
+
+for (sample_id in unique(dat1$sample_id)){
+  datb = dat1[which(dat1$sample_id == sample_id), ]
+  plot_list <- lapply(celltypes, function(i){
+    ggplot(data = datb, aes(x=array_row, y=array_col, color = datb[,i]))+
+      geom_point(size = 3)+facet_wrap(~tool)+scale_color_gradientn(colours = viridis(10, option = "magma"))+
+      theme(legend.position = "none")+labs(title = i)
+  })
+  #gridplot = grid.arrange(grobs = plot_list, nrow = length(celltypes))
+  ggsave(here("plots","spot_deconvo","shared_utilities","layer",paste0(sample_id,".pdf")), plot = marrangeGrob(plot_list, nrow=1, ncol=1),  width = 24, height = 8)
+  print(paste0("done ", sample_id))
+}
+scale_y_reverse()+ theme(legend.key.width = unit(0.1, "cm"))+labs(color = "")
+
+ggsave(here("plots","spot_deconvo","temp.pdf"), plot = p, width = 24, height = 8)
 
