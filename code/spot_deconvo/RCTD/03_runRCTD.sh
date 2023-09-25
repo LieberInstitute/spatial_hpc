e9e6526f813fb680ccd61a64a6b77b82bc966ad9
madhavitippani <madhavitippani28@gmail.com>
2023-09-25 13:45:57 -0400

    run updated code on sample 24
    Former-commit-id: c95655743ae721559104866ce5cd844e129a96f0

--- code/spot_deconvo/RCTD/03_runRCTD.sh
+++ code/spot_deconvo/RCTD/03_runRCTD.sh
@@ -4,7 +4,7 @@
 #$ -o /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/spot_deconvo/RCTD/logs/03_runRCTD_HE_layer_newClass_RCTDmarkers$TASK_ID.log
 #$ -e /dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/code/spot_deconvo/RCTD/logs/03_runRCTD_HE_layer_newClass_RCTDmarkers$TASK_ID.log
 #$ -l caracol,mf=100G,h_vmem=100G,h_fsize=100G
-#$ -t 1-36
+#$ -t 24
 #$ -tc 4
 
 USE_SLURM=2
