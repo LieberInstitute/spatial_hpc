31b4007539446185cfbc97a655d37ed5009dd3d7
madhavitippani <madhavitippani28@gmail.com>
2023-09-28 15:54:50 -0400

    pull paths for cart_visualize.py
    Former-commit-id: fa2802ebd1e1f018ec79853ff3e694b1a5f47c83

--- code/spot_deconvo/groundTruth/03_CART/cart_visualize.py
+++ code/spot_deconvo/groundTruth/03_CART/cart_visualize.py
@@ -11,3 +11,16 @@ import matplotlib.pyplot as plt
 import graphviz
 
 
+#############
+## paths
+############
+
+mask_path = pyhere.here('processed-data', 'spot_deconvo', 'groundTruth', '01_cellpose', 'final_masks', '{}' + '_DAPI_seg.npy')
+plot_dir = pyhere.here('plots','spot_deconvo', 'groundTruth', '03_CART')
+img_path = pyhere.here('processed-data', 'spot_deconvo', 'groundTruth', '02_samui_manual_annotation', '{}.tif')
+df_path = pyhere.here('processed-data', 'spot_deconvo', 'groundTruth', '03_CART', '{}' + 'cell_metrics.csv')
+
+spaceranger_dirs = pd.read_csv(pyhere.here("code","spot_deconvo","shared_utilities","samples.txt"), sep = '\t', header=None, names = ['SPpath', 'sample_id', 'brain'])
+spaceranger_dirs = spaceranger_dirs.iloc[36:].reset_index(drop=True)
+sample_ids = spaceranger_dirs.sample_id
+sample_id = sample_ids[int(os.environ['SGE_TASK_ID']) - 1]
