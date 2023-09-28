9dbeb0f0c8e528954a91fd21edb74ed9be698aea
madhavitippani <madhavitippani28@gmail.com>
2023-09-28 15:55:33 -0400

    function to plot intensity figures per random cells
    Former-commit-id: 721f80fd4c6e28d143a95bc102aa0f67bf8e1cf5

--- code/spot_deconvo/groundTruth/03_CART/cart_visualize.py
+++ code/spot_deconvo/groundTruth/03_CART/cart_visualize.py
@@ -24,3 +24,48 @@ spaceranger_dirs = pd.read_csv(pyhere.here("code","spot_deconvo","shared_utiliti
 spaceranger_dirs = spaceranger_dirs.iloc[36:].reset_index(drop=True)
 sample_ids = spaceranger_dirs.sample_id
 sample_id = sample_ids[int(os.environ['SGE_TASK_ID']) - 1]
+
+#-------------------------------------------------------------------------------
+#   Visually verify cell-type calls
+#-------------------------------------------------------------------------------
+################################################################################
+#   Functions
+################################################################################
+
+def plot_roi(img, props, indices, vmax: int = 128, pad: int = 25):
+    #   Set up the plot
+    fig, axs = plt.subplots(nrows=len(indices), ncols=6, figsize=(18, len(indices) * 3))
+    axs = axs.flatten()
+    
+    #   Loop through each nucleus, each of which will be a row in the final plot
+    j = 0
+    for idx in indices:
+        bbox = props[idx]["bbox"]
+        roi = props[idx]["image"]
+        
+        axs[j].imshow(np.pad(roi, (pad, pad), mode="constant", constant_values=0), aspect="equal")
+        axs[j].grid(False)
+        if j < 6:
+            axs[j].set_title("Mask")
+        
+        j += 1
+        
+        for i in range(1, 6):
+            a = axs[j].imshow(
+                img[
+                    i-1,
+                    max(0, bbox[0] - pad) : min(img.shape[1], bbox[2] + pad),
+                    max(0, bbox[1] - pad) : min(img.shape[2], bbox[3] + pad),
+                ],
+                vmax=vmax,
+                aspect="equal",
+            )
+            plt.colorbar(a, ax = axs[j])
+            
+            if j < 6:
+                axs[j].set_title(names[i])
+            
+            axs[j].grid(False)
+            j += 1
+    #
+    return fig
