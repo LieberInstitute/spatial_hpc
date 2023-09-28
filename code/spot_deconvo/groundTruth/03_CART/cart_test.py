22bdbddc845c6cc551cfe58da3ade1faf0d680b8
madhavitippani <madhavitippani28@gmail.com>
2023-09-28 15:48:36 -0400

    code to count cells per spot
    Former-commit-id: 9cea9f3f75c50f652ed331fd48c06457cf636c39

--- code/spot_deconvo/groundTruth/03_CART/cart_test.py
+++ code/spot_deconvo/groundTruth/03_CART/cart_test.py
@@ -71,3 +71,50 @@ assert set(df['cell_type']) == set(list(cell_types.values()))
 
 #   Save
 df.to_csv(cells_path, float_format="%.3f")
+
+#-------------------------------------------------------------------------------
+#   Count cells per spot and print some related statistics
+#-------------------------------------------------------------------------------
+raw = pd.read_csv(spot_path,header=0,names=["barcode", "included", "row", "col", "x", "y"],)
+
+#   Take only spots that overlap tissue
+raw = raw.iloc[raw.included[raw.included == 1].index].reset_index().drop(columns=["included", "index"])
+
+# Build KD tree for nearest neighbor search.
+kd = KDTree(raw[["x", "y"]])
+#   For each mask, assign a distance to the nearest spot ('dist') and index of that spot in 'df' ('idx'). Add this info to 'df'
+dist, idx = kd.query(df[["x", "y"]])
+dist = pd.DataFrame({"dist": dist, "idx": idx})
+df = pd.concat([df, dist], axis=1)
+
+with open(json_path) as f: 
+    json_data = json.load(f)
+    
+frac_kept = round(100 * np.sum(df.dist < json_data['spot_diameter_fullres'] / 2) / len(df.dist), 1)
+print(f'Keeping {frac_kept}% of masks, which were within spots covered by tissue.')
+df = df[df.dist < json_data['spot_diameter_fullres'] / 2]
+
+#   Count cell types in each spot
+for cell_type in df['cell_type'].unique():
+    raw[cell_type] = df[df['cell_type'] == cell_type].reset_index(drop=True).groupby('idx')['idx'].count().astype(int)
+
+#   Some spots will have no cells (and NaN values for this reason). Also count the total number of cells per spot
+raw.fillna(0, inplace=True)
+raw['n_cells'] = raw[[x for x in df['cell_type'].unique()]].sum(axis=1)
+
+#   Correct the data type
+for column_name in list(df['cell_type'].unique()) + ['n_cells']:
+    raw[column_name] = raw[column_name].astype(int)
+
+#   Print number of cells of each type
+for cell_type in df['cell_type'].unique():
+    print(f'Total number of {cell_type} cells: {raw[cell_type].sum()}')
+
+
+#   Print some quick stats about the cell counts per spot
+prop_nonzero = round(100 * np.count_nonzero(raw['n_cells']) / raw.shape[0], 1)
+print(f"Percentage of spots with at least one cell: {prop_nonzero}%")
+print(f"Mean number of cells per spot: {round(np.mean(raw['n_cells']), 3)}")
+print(f"Standard deviation (num cells per spot): {round(np.std(raw['n_cells']), 2)}")
+print(f"Max number of cells per spot: {np.max(raw['n_cells'])}")
+
