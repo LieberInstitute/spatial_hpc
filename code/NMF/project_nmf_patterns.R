918d03d6ed2421ab0808884f731e7a0f35e18dea
Erik Nelson <erik.nelson116@gmail.com>
2023-10-06 18:14:07 -0400

    splitting nmf factorization and projection code
    Former-commit-id: d641fcee103276117ba2401c7f4afb3b93c24f15

---
+++ code/NMF/project_nmf_patterns.R
@@ -0,0 +1,23 @@
+library(CoGAPS)
+library(RcppML)
+library(SpatialExperiment)
+load(file=here::here(NMF_PATH_HERE))
+load(file=here::here(SPE_PATH_HERE)
+
+proj<-projectR(
+    data=data,
+    loadings=x$w,
+    full = FALSE,
+    family = "gaussianff",
+    bootstrapPval = FALSE,
+    bootIter = 1000
+)
+
+set.seed(1029)
+i<-intersect(rownames(spe),rownames(x$w))
+loadings<-x$w
+loadings<-loadings[rownames(loadings) %in% i,]
+spe2<-spe[rownames(spe) %in% i,]
+loadings<-loadings[match(rownames(spe2),rownames(loadings)),]
+
+proj<-project(loadings,logcounts(spe2),L1=0)
