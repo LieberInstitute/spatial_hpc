30b85416e383346b688c442d6f75c0d991d732c1
Erik Nelson <erik.nelson116@gmail.com>
2023-12-01 15:34:43 -0500

    code for fig S9
    Former-commit-id: 9fe9034688d759775ae6834c507568c162eff8a2

---
+++ snRNAseq_hpc/code/build_sce/figureS9_plots.R
@@ -0,0 +1,117 @@
+setwd('/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/')
+suppressPackageStartupMessages(library("SpatialExperiment"))
+suppressPackageStartupMessages(library("scuttle"))
+suppressPackageStartupMessages(library("scran"))
+suppressPackageStartupMessages(library("scater"))
+suppressPackageStartupMessages(library("scry"))
+suppressPackageStartupMessages(library("jaffelab"))
+suppressPackageStartupMessages(library("tidyverse"))
+suppressPackageStartupMessages(library("here"))
+suppressPackageStartupMessages(library("BiocSingular"))
+suppressPackageStartupMessages(library("sessioninfo"))
+
+load(here("snRNAseq_hpc",'processed-data','sce','sce_clustered_round2.rda'), verbose = TRUE)
+sce$cluster<-sce$k_5_louvain_initial
+
+
+sce$neuron<-ifelse(sce$k_5_louvain_initial %in% c(1,2,9,10,20,21,26,28,
+                                                  31,34,37,38,40,42,43,
+                                                  59),T,F)
+sce$discard_round_3<-ifelse(sce$k_5_louvain_initial %in% c(6,8,46,37),F,T)
+
+table(sce$discard)
+###first a UMAP by cluster
+pdf(file=here::here('plots','figures','supp_figures',
+                    'clusteringRound2_UMAP_byCluster.pdf'),h=3,w=3)
+p<-plotUMAP(sce,text_by='cluster',colour_by='cluster',point_size=0.01,add_legend=F,text_size=3.75)
+rasterize(p,dpi=500,layers='Point')
+dev.off()
+
+###now boxplot of SYT1 expression by cluster
+pdf(file=here::here('plots','figures','supp_figures',
+                    'clusteringRound2_boxplot_SYT1_byCluster.pdf'),h=3,w=5)
+ggcells(sce, mapping=aes(x=cluster, y=SYT1,fill=neuron)) +
+    geom_boxplot(outlier.size = 0.5)+theme(axis.text.x = element_text(angle = 90),
+                         text=element_text(size = 11,colour='black'))+
+    theme(legend.position='bottom',
+          panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+          panel.background = element_blank(),
+          axis.line = element_line(colour = "black"))+scale_fill_manual(values=c('green','blue'))
+dev.off()
+
+###now a UMAP by cluster coloured by neurons
+pdf(file=here::here('plots','figures','supp_figures',
+                    'clusteringRound2_UMAP_byCluster_neurons.pdf'),h=3,w=4)
+p<-plotUMAP(sce,text_by='cluster',colour_by='neuron',
+            point_size=0.01,add_legend=T,text_size=3.75)+
+                scale_color_manual(values=c('green','blue'))+labs(color='neuron')
+rasterize(p,dpi=500,layers='Point')
+dev.off()
+
+
+
+###now boxplot of detected genes by cluster
+pdf(file=here::here('plots','figures','supp_figures',
+                    'clusteringRound2_boxplot_detectedGenes_byCluster.pdf'),h=3,w=5)
+ggcells(sce, mapping=aes(x=cluster, y=detected,fill=neuron)) +
+    geom_boxplot(outlier.size = 0.5)+theme(axis.text.x = element_text(angle = 90),
+                                           text=element_text(size = 11,colour='black'))+
+    theme(legend.position='bottom',
+          panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+          panel.background = element_blank(),
+          axis.line = element_line(colour = "black"))+
+    scale_fill_manual(values=c('green','blue'))+
+    ylab('detected genes')+scale_y_log10()
+
+dev.off()
+
+##roundabout but easy way of getting ordered clusters for doublet plot
+p1<-plotGroupedHeatmap(sce,group = 'cluster',
+                       features=c('C3','CSF1R',
+
+                           'AQP4','GFAP','ETNPPL','MOBP',
+                                  'MBP','MOG','PDGFRA','VCAN'),
+                       center=T,scale=T)
+lab_order<-as.numeric(p1$tree_col$order)
+sce$cluster_ordered<-factor(sce$cluster,levels=lab_order)
+
+##doublet dotplot
+pdf(file=here::here('plots','figures','supp_figures',
+                    'clusteringRound2_dotPlot_markerGenes_doublets.pdf'),h=6,w=17)
+plotDots(sce,group='cluster_ordered',
+         features=c('PDGFRA',
+                    'AQP4','ETNPPL',
+                    'MOBP','MOG',
+                    'C3','CSF1R'),
+         color=c('white','black')) +
+    #scale_y_discrete(limits=rev(features)) +
+    #scale_x_discrete(limits=rev(levels(spe$cluster)))+
+    theme(axis.text.x = element_text(angle = 90,vjust=0.75),
+          text = element_text(size = 20))+
+    labs(x= "Cell type", y = "Gene",size='proportion',color='mean')
+dev.off()
+
+##discard UMAP
+
+
+pdf(file=here::here('plots','figures','supp_figures',
+                    'clusteringRound2_UMAP_byCluster_discardRound3.pdf'),h=3,w=4.5)
+p<-plotUMAP(sce,text_by='cluster',colour_by='discard_round_3',
+            point_size=0.01,add_legend=T,text_size=3.75)+
+    labs(color='neuron')
+rasterize(p,dpi=500,layers='Point')
+dev.off()
+
+save(sce,file='sce_clustered_round2.rda')
+
+rm(sce)
+load(sce,file='sce_clustered_round3.rda')
+
+pdf(file=here::here('plots','figures','supp_figures',
+                    'clusteringRound3_UMAP_byCluster_discardRound3.pdf'),h=3,w=4.5)
+p<-plotUMAP(sce,colour_by='discard_round_3',text_by='k_5_louvain',
+            point_size=0.01,add_legend=T,text_size=3.75)+
+    labs(color='discard_round_3')+scale_color_manual(values='#FF9E4A')
+rasterize(p,dpi=500,layers='Point')
+dev.off()
+
