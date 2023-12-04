6d6510eb8cd738cf9def94e730c65d162c12908f
madhavitippani <madhavitippani28@gmail.com>
2023-12-04 09:48:25 -0500

    code for supp fig 18
    Former-commit-id: bf166511e4e18524b182b5548ac6d69821b74975

---
+++ code/spot_deconvo/figures/sfig_celldensityINdomains.R
@@ -0,0 +1,34 @@
+setwd("/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/")
+suppressPackageStartupMessages(library("SingleCellExperiment"))
+suppressPackageStartupMessages(library("here"))
+suppressPackageStartupMessages(library("spatialLIBD"))
+suppressPackageStartupMessages(library("ggplot2"))
+
+Dr <- here("processed-data","spot_deconvo","shared_utilities")
+plot_dir <- here("plots", "spot_deconvo", "figures", "fig_celldensity")
+dir.create(plot_dir, recursive = TRUE, showWarnings = FALSE)
+
+spe = readRDS(here(Dr,"spe.rds"))
+dat = as.data.frame(colData(spe))
+dat = dat[dat$count<50, ]
+levels(dat$cluster_collapsed)[levels(dat$cluster_collapsed)=="WM.1"] <- "WM"
+levels(dat$cluster_collapsed)[levels(dat$cluster_collapsed)=="WM.2"] <- "WM"
+levels(dat$cluster_collapsed)[levels(dat$cluster_collapsed)=="WM.3"] <- "WM"
+
+load('/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/plots/palettes.rda')
+colors = precast_palette[c(1,2,4,6:14,17,18)]
+colors = unname(colors)
+png(here("plots","spot_deconvo","figures","fig_celldensity", "sfig_celldensityINdomains.png"), width = 1200, height = 600, units = "px") 
+p = ggplot(data = dat, aes(x = dat$cluster_collapsed, y=count, fill = cluster_collapsed))+
+  geom_boxplot(outlier.shape = NA, show.legend = FALSE)+theme_bw()+scale_fill_manual(values = colors)+
+  labs(y = "nuclei count per spot", x="spatial domains")+ 
+  theme(text = element_text(size = 20, colour = "black"),
+        axis.text = element_text(size = 24, colour = "black"),
+        axis.text.x = element_text(angle = 90),
+        axis.line = element_line(size=2, colour = "black"),
+        panel.grid.minor = element_blank(), 
+        panel.grid.major = element_blank(),
+        panel.border = element_blank())
+print(p)
+dev.off()
+
