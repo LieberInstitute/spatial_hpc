c242fcead5eae975458006a6a7b337658033e27a
madhavitippani <madhavitippani28@gmail.com>
2024-01-23 12:08:05 -0500

    code to clean sample info, extract transformations from xml and adapt to other resilutions
    Former-commit-id: 1bfd91198f7cf7e27c830dc0b1c597f50bc825c6

---
+++ code/VSPG_image_stitching/01-extract_translation.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -l
+
+#$ -cwd
+#$ -N "clean_sample_info"
+#$ -o logs/02-clean_sample_info.log
+#$ -e logs/02-clean_sample_info.log
+#$ -l mf=5G,h_vmem=5G
+
+
+#SBATCH --mem=5G
+#SBATCH --job-name=clean_sample_info
+#SBATCH -o logs/02-clean_sample_info.log
+
+
+set -e
+
+if [[ ! -z $SLURMD_NODENAME ]]; then
+    job_id=$SLURM_JOB_ID
+    job_name=$SLURM_JOB_NAME
+    node_name=$SLURMD_NODENAME
+else
+    job_id=$JOB_ID
+    job_name=$JOB_NAME
+    node_name=$HOSTNAME
+fi
+
+echo "**** Job starts ****"
+date
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${job_id}"
+echo "Job name: ${job_name}"
+echo "Node name: ${node_name}"
+
+module list
+
+module load samui/1.0.0-next.45
+python 02-clean_sample_info.py
+
+echo "**** Job ends ****"
+date
