2652be776d22a21a1a503b9b8302922fc9998676
madhavitippani <madhavitippani28@gmail.com>
2024-02-05 14:18:09 -0500

    define output and input directories
    Former-commit-id: e18e1e48daa1692540cf7faa87e3d26b00e0393b

--- code/VSPG_image_stitching/02-create_samui.py
+++ code/VSPG_image_stitching/02-create_samui.py
@@ -19,3 +19,14 @@ import scanpy as sc
 from rasterio import Affine
 from loopy.utils.utils import remove_dupes, Url
 import re
+
+sample_info_path = here('processed-data', 'VSPG_image_stitching', 'transformations.csv')
+samui_dir = Path(here('processed-data', 'VSPG_image_stitching', this_donor))
+samui_dir.mkdir(parents = True, exist_ok = True)
+combined_dir = Path(here('processed-data', 'VSPG_image_stitching',f'combined_{this_donor}'))
+combined_dir.mkdir(parents = True, exist_ok = True)
+
+img_out_fullres = Path(here(combined_dir, f'combined_{this_donor}.tif'))
+tissue_out_path = Path(here(combined_dir, f'tissue_positions_{this_donor}.csv'))
+img_out_lowres = Path(here(combined_dir, f'combined_tissue_lowres_{this_donor}.png'))
+json_out_path = Path(here(combined_dir, 'scalefactors_json.json'))
