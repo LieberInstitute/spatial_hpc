8b1bf53cdd8d2517ac8d6eca9fd95cb08e0664d2
madhavitippani <madhavitippani28@gmail.com>
2024-02-05 14:19:33 -0500

    define spot metrics and extract sample info
    Former-commit-id: 863e8be6c65e80cbe95361e57f1f112f5db7daa5

--- code/VSPG_image_stitching/02-create_samui.py
+++ code/VSPG_image_stitching/02-create_samui.py
@@ -30,3 +30,27 @@ img_out_fullres = Path(here(combined_dir, f'combined_{this_donor}.tif'))
 tissue_out_path = Path(here(combined_dir, f'tissue_positions_{this_donor}.csv'))
 img_out_lowres = Path(here(combined_dir, f'combined_tissue_lowres_{this_donor}.png'))
 json_out_path = Path(here(combined_dir, 'scalefactors_json.json'))
+
+#   55-micrometer diameter for Visium spot but 65-micrometer spot diameter used
+#   in 'spot_diameter_fullres' calculation for spaceranger JSON. The
+#   difference between 55 and 65 does indeed exist and is properly documented,
+#   but is likely a bug in the sense the choice was probably unintentional
+#   https://kb.10xgenomics.com/hc/en-us/articles/360035487812-What-is-the-size-of-the-spots-on-the-Visium-Gene-Expression-Slide-
+#   https://support.10xgenomics.com/spatial-gene-expression/software/pipelines/latest/output/spatial
+
+spg_path = here("processed-data", "VSPG_image_stitching", "spg.h5ad")
+
+SPOT_DIAMETER_M = 55e-6
+SPOT_DIAMETER_JSON_M = 65e-6
+
+LOWRES_MAX_SIZE = 1200
+BACKGROUND_COLOR = 0
+
+#   Read in sample info and subset to samples of interest
+sample_info = pd.read_csv(sample_info_path, index_col = 0)
+sample_info = sample_info.loc[
+    (sample_info['Brain'] == this_donor) &
+    ~sample_info['xml_path'].isna(),# &
+    #sample_info['In analysis'],
+    :
+]
