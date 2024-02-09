5686175cd7b2ae85dfa2da57256a4cb25e76e28f
rmill162 <rmill162@eb-li-qpmy24h3.win.ad.jhu.edu>
2024-02-09 16:12:15 -0500

    update deploy script
    Former-commit-id: eef06d63917dc29976ba640b8acb7d2deb296e3d

--- code/11_iSEE_apps/pseudobulk_iSEE/deploy.R
+++ code/11_iSEE_apps/pseudobulk_iSEE/deploy.R
@@ -4,8 +4,8 @@ library("rsconnect")
 
 options(repos = BiocManager::repositories())
 rsconnect::deployApp(
-    appFiles = c("app.R", "rse_gene_TrkB_KO_LS_n8_wm.Rdata", "initial.R"),
-    appName = "bulkseq_lateral_septum",
+    appFiles = c("app.R", "spe_pseudo.rda", "initial.R"),
+    appName = "bulkseq_HPC",
     account = "libd",
     server = "shinyapps.io"
 )
