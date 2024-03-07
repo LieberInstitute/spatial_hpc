7db221078715427007a99517904df562f9b4a003
Heena Divecha <heena.divecha@libd-c02g612uml85.win.ad.jhu.edu>
2024-03-07 09:39:50 -0500

    Edited the script to troubleshoot app deployment issue.
    Former-commit-id: fc8b7a5b7abd7fe8d05fafe33dae24cbbbe0b162

--- code/11_iSEE_apps/pseudobulk_iSEE/deploy.R
+++ code/11_iSEE_apps/pseudobulk_iSEE/deploy.R
@@ -1,11 +1,11 @@
 library("rsconnect")
 
-# source("token.R")
+#source("token.R")
 
 options(repos = BiocManager::repositories())
 rsconnect::deployApp(
-    appFiles = c("app.R", "spe_pseudo.rda", "initial.R"),
-    appName = "bulkseq_HPC",
+    appFiles = c("app.R", "spe_pseudo.rda", "spatial_palettes_isee.rda", "initial.R"),
+    appName = "HPC_bulkseq_data",
     account = "libd",
     server = "shinyapps.io"
 )
