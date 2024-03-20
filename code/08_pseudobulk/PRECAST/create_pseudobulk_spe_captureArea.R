75abfcadacf73918cf4aa043bdf46c454ddca23c
Erik Nelson <erik.nelson116@gmail.com>
2024-03-20 08:52:09 -0400

    cleanup/comment pseudobulk spatial code
    Former-commit-id: 32c8f129e6c21471d364e6e94fb87fcda0cfb649

--- code/08_pseudobulk/PRECAST/create_pseudobulk_spe_captureArea.R
+++ code/08_pseudobulk/PRECAST/create_pseudobulk_spe_captureArea.R
@@ -14,11 +14,16 @@ suppressPackageStartupMessages({
     library(gridExtra)
     library(ggforce)
     library(pheatmap)
+    library(scater)
+    library(scran)
 })
 
 # Load SPE
-load(file=here::here('processed-data','06_clustering','PRECAST','spe_precast_HE.rda'))
-# Set up palette
+load(file=here::here('processed-data','06_clustering',
+                     'PRECAST','spe_precast_HE_domain.rda'))
+
+##load palettes
+load(file=here::here('plots','spatial_palette_final.rda'))
 palette = spatial.palette
 
 names(palette) <- levels(spe$domain)
@@ -30,20 +35,18 @@ spe_pseudo <- aggregateAcrossCells(
         domain = colData(spe)$domain,
         sample_id = colData(spe)$sample_id
     ))
-
-spe_pseudo$domain <- factor(spe_pseudo$domain)
-spe_pseudo <- spe_pseudo[, spe_pseudo$ncells >= 50]
-colData(spe_pseudo)<-colData(spe_pseudo)[,c(21,22,24,31:33,159:162)]
-colData(spe_pseudo)$broad.domain<-factor(
-    ifelse(spe_pseudo$domain %in% levels(spe_pseudo$domain)[1:7],'Neuron',
-           ifelse(spe_pseudo$domain %in% levels(spe_pseudo$domain)[8:11],'Neuropil',
-                  ifelse(spe_pseudo$domain %in% levels(spe_pseudo$domain)[12:14],'WM',
-                         'Vasc/CSF'))))
-
 dim(spe_pseudo)
-# 31483   372
+#[1] 31483   524
 
-##
+##remove pseudobulked samples with very low ncells (here this means # spots)
+spe_pseudo <- spe_pseudo[, spe_pseudo$ncells >= 50]
+dim(spe_pseudo)
+#[1] 31483   372
+
+###remove irrelevant colData cols
+colData(spe_pseudo)<-colData(spe_pseudo)[,c(21,22,24,31:33,50,52,53,54)]
+
+
 pdf(file = here::here("plots","08_pseudobulk", "PRECAST", "histogram_boxplot_domain.pdf"), width = 14, height = 14)
 hist(spe_pseudo$ncells, breaks = 200)
 boxplot(ncells ~ spe_pseudo$domain, data = colData(spe_pseudo))
@@ -97,11 +100,13 @@ rm(x)
 
 ##quickly remove Thal and Amy from the data
 #Thal
-spe_pseudo$thal<-ifelse(spe_pseudo$sample_id=='V11A20-297_B1' & spe_pseudo$domain %in% c('SUB','SUB.RHP'),T,F)
+spe_pseudo$thal<-ifelse(spe_pseudo$sample_id=='Br8325_V11A20-297_B1' & spe_pseudo$domain %in% c('SUB','SUB.RHP'),T,F)
 spe_pseudo<-spe_pseudo[,spe_pseudo$thal==F]
 #Amy
-spe_pseudo$amy<-ifelse(spe_pseudo$sample_id %in% c('V10B01-085_C1','V10B01-086_B1') & spe_pseudo$domain %in% c('RHP'),T,F)
-spe_pseudo$amy<-ifelse(spe_pseudo$sample_id =='V10B01-085_A1',T,spe_pseudo$amy)
+spe_pseudo$amy<-ifelse(spe_pseudo$sample_id %in%
+                           c('Br6423_V10B01-085_C1','Br6432_V10B01-086_B1') &
+                           spe_pseudo$domain %in% c('RHP'),T,F)
+spe_pseudo$amy<-ifelse(spe_pseudo$sample_id =='Br6423_V10B01-085_A1',T,spe_pseudo$amy)
 spe_pseudo<-spe_pseudo[,spe_pseudo$amy==F]
 dim(spe_pseudo)
 #[1] 15404   317
@@ -115,7 +120,7 @@ message(Sys.time(), " % of variance explained for the top 20 PCs:")
 metadata(spe_pseudo)
 metadata(spe_pseudo) <- list("PCA_var_explained" = jaffelab::getPcaVars(pca)[seq_len(20)])
 metadata(spe_pseudo)
-$PCA_var_explained
+#$PCA_var_explained
 # [1] 18.200  5.890  3.860  3.140  3.000  2.430  2.080  1.650  1.220  1.100  1.040  0.891  0.851  0.803
 # [15]  0.726  0.665  0.650  0.619  0.603  0.587
 
@@ -124,16 +129,16 @@ pca_pseudo <- pca$x[, seq_len(50)]
 colnames(pca_pseudo) <- paste0("PC", sprintf("%02d", seq_len(ncol(pca_pseudo))))
 reducedDims(spe_pseudo) <- list(PCA = pca_pseudo)
 
-plotski<-jaffelab::getPcaVars(pca)[seq_len(50)]
+jaffelab::getPcaVars(pca)[seq_len(50)]
 # [1] 18.200  5.890  3.860  3.140  3.000  2.430  2.080  1.650  1.220  1.100  1.040  0.891  0.851  0.803
 # [15]  0.726  0.665  0.650  0.619  0.603  0.587  0.572  0.568  0.552  0.546  0.536  0.526  0.521  0.512
 # [29]  0.510  0.500  0.497  0.486  0.484  0.482  0.474  0.465  0.462  0.458  0.452  0.451  0.443  0.440
 # [43]  0.436  0.430  0.427  0.425  0.419  0.415  0.410  0.402
 
 # Plot PCA
-pdf(file = here::here("plots","08_pseudobulk", "PRECAST", "pseudobulk_PCA_visiumHE.pdf"), width = 14, height = 14)
+pdf(file = here::here("plots","08_pseudobulk", "PRECAST", "pseudobulk_PCA_visiumHE_final.pdf"), width = 14, height = 14)
 plotPCA(spe_pseudo, colour_by = "domain", ncomponents = 5, point_size = 1, point_alpha=1,label_format = c("%s %02i", " (%i%%)"),
-        percentVar = metadata(spe_pseudo)$PCA_var_explained)+scale_color_manual(values=spatial.palette)+labs(color='domain')
+        percentVar = metadata(spe_pseudo)$PCA_var_explained)+scale_color_manual(values=palette)+labs(color='domain')
 plotPCA(spe_pseudo, colour_by = "broad.domain", ncomponents = 5, point_size = 1, label_format = c("%s %02i", " (%i%%)"),
         percentVar = metadata(spe_pseudo)$PCA_var_explained)+scale_color_manual(values=spatial.palette2)+labs(color='broad.domain')
 plotPCA(spe_pseudo, colour_by = "dateImg", ncomponents = 5, point_size = 1, label_format = c("%s %02i", " (%i%%)"),
@@ -152,7 +157,6 @@ dev.off()
 
 # Extract the PCA data from spe_pseudo into a dataframe
 PCAData <- as.data.frame(reducedDim(spe_pseudo, "PCA"))
-# Assuming "broad" is part of colData in spe_pseudo
 PCAData$broad.domain <- colData(spe_pseudo)$broad.domain
 PCAData$domain<-spe_pseudo$domain
 
@@ -165,7 +169,7 @@ pca_plot <- ggplot(PCAData, aes(PC01, PC02)) +
                      panel.grid.minor = element_blank())  # Remove minor grid lines
 
 # Add the ellipses
-pdf(file=here::here('plots','figures','figure_2','pca_plot_final.pdf'),h=6,w=8)
+pdf(file=here::here('plots','figures','figure_2','pca_plot_final_check.pdf'),h=6,w=8)
 pca_plot + geom_mark_ellipse(aes(color = broad.domain, label = broad.domain),
                              expand = unit(0.5,"mm"),
                              label.buffer = unit(-5, 'mm'),
@@ -175,12 +179,10 @@ dev.off()
 
 ##############DE ANALYSIS################
 spe_pseudo$age_scaled<-scales::rescale(spe_pseudo$age,to=c(0,1))
-spe_pseudo$dateImg<-factor(
-    gsub(spe_pseudo$dateImg,pattern='-',replacement='_'))
-levels(spe_pseudo$broad.domain)[3]<-'Vasc_CSF'
+spe_pseudo$slide<-factor(spe_pseudo$slide)
 mod<-registration_model(
     spe_pseudo,
-    covars = c('sex','age_scaled','dateImg'),
+    covars = c('sex','slide','brnum'),
     var_registration = "broad.domain"
 )
 
@@ -193,7 +195,7 @@ cors<-registration_block_cor(
 reg<-registration_stats_enrichment(
     spe_pseudo,
     block_cor=cors,
-    covars = c('sex','age_scaled','dateImg'),
+    covars = c('sex','age_scaled','slide'),
     var_registration = "broad.domain",
     var_sample_id = "sample_id",
     gene_ensembl = 'gene_id',
@@ -224,9 +226,10 @@ stats<-list(reg,rega,regp)
 names(stats)<-c('enrichment','anova','pairwise')
 save(stats,file=here::here('processed-data','08_pseudobulk','PRECAST','visiumHE_DE_stats_broad_final.rda'))
 
+
 mod<-registration_model(
     spe_pseudo,
-    covars = c('sex','age_scaled','dateImg'),
+    covars = c('sex','age_scaled','slide'),
     var_registration = "domain"
 )
 
@@ -239,7 +242,7 @@ cors<-registration_block_cor(
 reg<-registration_stats_enrichment(
     spe_pseudo,
     block_cor=cors,
-    covars = c('sex','age_scaled','dateImg'),
+    covars = c('sex','age_scaled','slide'),
     var_registration = "domain",
     var_sample_id = "sample_id",
     gene_ensembl = 'gene_id',
@@ -249,7 +252,7 @@ reg<-registration_stats_enrichment(
 rega<-registration_stats_anova(
     spe_pseudo,
     block_cor=cors,
-    covars = c('sex','age_scaled','dateImg'),
+    covars = c('sex','age_scaled','slide'),
     var_registration = "domain",
     var_sample_id = "sample_id",
     gene_ensembl = 'gene_id',
@@ -270,6 +273,7 @@ stats<-list(reg,rega,regp)
 names(stats)<-c('enrichment','anova','pairwise')
 save(stats,file=here::here('processed-data','08_pseudobulk','PRECAST','visiumHE_DE_stats_domain.rda'))
 save(spe_pseudo,file=here::here('processed-data','08_pseudobulk','PRECAST','spe_pseudo_HE.rda'))
+
 ###make supplementary figures
 vars <- getVarianceExplained(spe_pseudo, variables=c("domain",'broad.domain',"age","sex","brnum","slide","sample_id",
                                                      'dateImg','detected','sum','ncells','subsets_Mito_percent'))
