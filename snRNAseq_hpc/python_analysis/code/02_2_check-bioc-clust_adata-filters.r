b46d6257710053f6550f5806d182d46fe6339494
jac-thom <jthom338@jh.edu>
2024-08-22 11:27:13 -0400

    Compare loose threshold

--- snRNAseq_hpc/python_analysis/code/02_2_check-bioc-clust_adata-filters.r
+++ snRNAseq_hpc/python_analysis/code/02_2_check-bioc-clust_adata-filters.r
@@ -34,3 +34,34 @@ res <- scater::runPCA(res,ntop=2000,
 
 reducedDim(sce,'PCA')<-reducedDim(res,'PCA')
 save(sce, file="snRNAseq_hpc/python_analysis/processed-data/sce_postqc-loose.Rdata")
+
+# remaining steps performed in Rstudio
+##MNN batch correction
+#set.seed(1788)
+#mnn<-batchelor::reducedMNN(reducedDim(sce,'PCA'),batch=sce$brnum,k=50)
+#reducedDim(sce,'MNN')<-mnn$corrected
+##set.seed(1788)
+#mnn<-batchelor::reducedMNN(reducedDim(sce,'MNN'),batch=sce$round,k=50)
+#reducedDim(sce,'MNN')<-mnn$corrected
+
+##umap
+#sce <- runUMAP(sce, dimred = "MNN")
+
+#sce = addPerCellQC(sce)
+#sce$lg10.sum = log10(sce$sum)
+
+# comparison to erik's final clusters also performed in rstudio
+#sce_keep <- sce
+#load(here("snRNAseq_hpc",'processed-data','sce','sce_final.rda'))
+#cmp.df = left_join(as.data.frame(colData(sce_keep)), as.data.frame(colData(sce)[,1:31]),
+#                   by=c("Sample","Barcode","brnum","round","sorted"="sort"))
+#filt.df = filter(cmp.df, !is.na(cell.type))
+#nrow(filt.df)
+
+#sce_keep$erik_final = FALSE
+#colData(sce_keep)[sce_keep$key %in% filt.df$key.x, "erik_final"] = TRUE
+#sce_keep$cell.type="none"
+#colData(sce_keep)[sce_keep$erik_final==T,"cell.type"] = as.character(filt.df$cell.type)
+
+#sce <- sce_keep
+#save(sce, file="snRNAseq_hpc/python_analysis/processed-data/sce_postqc-strict.Rdata")
