06da2fb1e0e7a307ca8c8cd21e8810a3023d31db
jac-thom <jthom338@jh.edu>
2024-11-20 20:26:20 -0500

    Update Figure 4 supp to include all boxplots

--- code/revision/plots_Figure4-and-supp.R
+++ code/revision/plots_Figure4-and-supp.R
@@ -8,75 +8,99 @@ set.seed(123)
 load(file=here::here('processed-data','NMF','spe_nmf_final.rda'))
 load(file=here::here('plots','spatial_palette_final.rda'))
 
-#boxplots
 #oligo
-ggcells(spe, aes(x=domain, y=nmf44, fill=domain))+
+p1 <- ggcells(spe, aes(x=domain, y=nmf44, fill=domain))+
   geom_boxplot(outlier.size=.1)+theme_bw()+
   scale_fill_manual(values=spatial.palette)+
   scale_y_continuous(labels=function(x) format(x, scientific=T, digits=1))+
-  labs(title="nmf44", y="spot weights")+
+  labs(title="Oligodendrocytes (nmf44)", y="spot weights")+
   theme(axis.text.x=element_text(angle=90, hjust=1, vjust=.5),
-        legend.position="none")
+        axis.text.y=element_text(size=8),
+        legend.position="none", axis.title.x=element_blank())
 #astro
-ggcells(spe, aes(x=domain, y=nmf81, fill=domain))+
+p2 <- ggcells(spe, aes(x=domain, y=nmf81, fill=domain))+
   geom_boxplot(outlier.size=.1)+theme_bw()+
   scale_fill_manual(values=spatial.palette)+
   scale_y_continuous(labels=function(x) format(x, scientific=T, digits=1))+
-  labs(title="nmf81", y="spot weights")+
+  labs(title="Astrocytes (nmf81)", y="spot weights")+
   theme(axis.text.x=element_text(angle=90, hjust=1, vjust=.5),
-        legend.position="none")
+        axis.text.y=element_text(size=8),
+        legend.position="none", axis.title.x=element_blank())
 #exc postsynaptic
-ggcells(spe, aes(x=domain, y=nmf13, fill=domain))+
+p3 <- ggcells(spe, aes(x=domain, y=nmf13, fill=domain))+
   geom_boxplot(outlier.size=.1)+theme_bw()+
   scale_fill_manual(values=spatial.palette)+
   scale_y_continuous(labels=function(x) format(x, scientific=T, digits=1))+
-  labs(title="nmf13", y="spot weights")+
+  labs(title="Excitatory postsynaptic (nmf13)", y="spot weights")+
   theme(axis.text.x=element_text(angle=90, hjust=1, vjust=.5),
-        legend.position="none")
+        axis.text.y=element_text(size=8),
+        legend.position="none", axis.title.x=element_blank())
 #inhb postsynaptic
-ggcells(spe, aes(x=domain, y=nmf7, fill=domain))+
+p4 <- ggcells(spe, aes(x=domain, y=nmf7, fill=domain))+
   geom_boxplot(outlier.size=.1)+theme_bw()+
   scale_fill_manual(values=spatial.palette)+
   scale_y_continuous(labels=function(x) format(x, scientific=T, digits=1))+
-  labs(title="nmf7", y="spot weights")+
+  labs(title="Inhibitory postsynaptic (nmf7)", y="spot weights")+
   theme(axis.text.x=element_text(angle=90, hjust=1, vjust=.5),
-        legend.position="none")
+        axis.text.y=element_text(size=8),
+        legend.position="none", axis.title.x=element_blank())
+
 
 #extended data boxplots
 load(file=here::here('snRNAseq_hpc','processed-data','sce','sce_final.rda'))
 load(file=here::here('plots','snRNAseq_palettes.rda'))
 
-ggcells(sce, aes(x=fine.cell.class, y=nmf13, fill=fine.cell.class))+
+#PrsPas --> L2/3.1; PrsENT --> L2/3.2
+sce$fine.cell.class <- factor(sce$fine.cell.class, levels=levels(sce$fine.cell.class),
+                              labels=c(levels(sce$fine.cell.class)[1:7],
+                                       "L2/3.1","L2/3.2",
+                                       levels(sce$fine.cell.class)[10:24]))
+names(sn.fine.palette) <- levels(sce$fine.cell.class)
+
+p5 <- ggcells(sce, aes(x=fine.cell.class, y=nmf13, fill=fine.cell.class))+
   geom_boxplot(outlier.size=.1)+theme_bw()+
   scale_fill_manual(values=sn.fine.palette)+
   scale_y_continuous(labels=function(x) format(x, scientific=T, digits=1))+
-  labs(title="nmf13", y="nuclei weights")+
-  theme(axis.text.x=element_text(angle=90, hjust=1, vjust=.5),
-        legend.position="none")
-ggcells(sce, aes(x=fine.cell.class, y=nmf7, fill=fine.cell.class))+
+  labs(title="Excitatory postsynaptic (nmf13) - snRNAseq", y="nuclei weights")+
+  theme(axis.text.x=element_text(angle=90, hjust=1, vjust=.5, size=6),
+        axis.text.y=element_text(size=8),
+        legend.position="none", axis.title.x=element_blank())
+p6 <- ggcells(sce, aes(x=fine.cell.class, y=nmf7, fill=fine.cell.class))+
   geom_boxplot(outlier.size=.1)+theme_bw()+
   scale_fill_manual(values=sn.fine.palette)+
   scale_y_continuous(labels=function(x) format(x, scientific=T, digits=1))+
-  labs(title="nmf7", y="nuclei weights")+
-  theme(axis.text.x=element_text(angle=90, hjust=1, vjust=.5),
-        legend.position="none")
+  labs(title="Inhibitory postsynaptic (nmf7) - snRNAseq", y="nuclei weights")+
+  theme(axis.text.x=element_text(angle=90, hjust=1, vjust=.5, size=6),
+        axis.text.y=element_text(size=8),
+        legend.position="none", axis.title.x=element_blank())
 
 #nmf13 and 7 overlap
-t1 = table(cbind.data.frame(nmf13=spe$nmf13>0, nmf7=spe$nmf7>0))
-t1/rowSums(t1)
 
-ggcells(spe, aes(x=nmf13, y=nmf7, color=domain))+
+p7 <- ggcells(spe, aes(x=nmf13, y=nmf7, color=domain))+
   geom_point(size=.1)+scale_color_manual(values=spatial.palette)+
-  facet_wrap(vars(domain))+
-  theme(axis.text.x=element_text(angle=45, hjust=1), legend.position="none")
+  scale_x_continuous(labels=function(x) format(x, scientific=T, digits=1))+
+  scale_y_continuous(labels=function(x) format(x, scientific=T, digits=1))+
+  facet_wrap(vars(domain))+ggtitle("Co-occurrence of nmf13 and nmf7 in SRT")+
+  theme(text=element_text(size=10), 
+        axis.text.x=element_text(angle=45, hjust=1, size=6), 
+        axis.text.y=element_text(size=6), legend.position="none")
 
-
-t2 = table(cbind.data.frame(nmf13=sce$nmf13>0, nmf7=sce$nmf7>0))
-t2/rowSums(t2)
-
-ggcells(sce, aes(x=nmf13, y=nmf7, color=fine.cell.class))+
+p8 <- ggcells(sce, aes(x=nmf13, y=nmf7, color=fine.cell.class))+
   geom_point(size=.1)+scale_color_manual(values=sn.fine.palette)+
   scale_x_continuous(labels=function(x) format(x, scientific=T, digits=1))+
   scale_y_continuous(labels=function(x) format(x, scientific=T, digits=1))+
-  facet_wrap(vars(fine.cell.class))+
-  theme(axis.text.x=element_text(angle=45, hjust=1), legend.position="none")
+  facet_wrap(vars(fine.cell.class))+ggtitle("Co-occurrence of nmf13 and nmf7 in snRNAseq")+
+  theme(text=element_text(size=10), panel.spacing=unit(3,'points'), 
+        strip.text=element_text(margin=margin(2,2,2,2, unit="pt")),
+        axis.text.x=element_text(angle=45, hjust=1, size=6),
+        axis.text.y=element_text(size=6), legend.position="none")
+
+laymat = rbind(c(1,2),c(1,2),c(3,4),c(3,4),c(5,6),c(5,6),c(7,8),c(7,8),c(7,8),c(7,8))
+
+gridExtra::grid.arrange(p1, p2, p3, p4, p5, p6, p7, p8, layout_matrix=laymat)
+
+pdf(file = "plots/revision/Figure4_supp-boxplots-scatter.pdf",
+    width=8.5, height=11)
+gridExtra::grid.arrange(p1, p2, p3, p4, p5, p6, p7, p8, layout_matrix=laymat)
+dev.off()
+
