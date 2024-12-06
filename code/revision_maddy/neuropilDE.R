83148aff81309c2651d9ff08a87d93a693b81fa2
madhavitippani <madhavitippani28@gmail.com>
2024-12-06 11:06:02 -0500

    update neuropil plots

--- code/revision_maddy/neuropilDE.R
+++ code/revision_maddy/neuropilDE.R
@@ -7,25 +7,26 @@ suppressPackageStartupMessages({
   library(spatialLIBD)
 })
 
-spe = readRDS('/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/spot_deconvo/shared_utilities/spe.rds')
-spe$neuropil_pos <- ifelse(spe$Pmask_dark_blue < 0.005, TRUE, FALSE)
+#spe = readRDS('/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/processed-data/spot_deconvo/shared_utilities/spe.rds')
+#spe$neuropil_pos <- ifelse(spe$Pmask_dark_blue < 0.005, TRUE, FALSE)
 
 #############SLM.SGZ-SL.SR#############		
-spe_sub <- spe[, colData(spe)$cluster_collapsed %in% c("SLM.WM", "SL.SR") &  colData(spe)$neuropil_pos == TRUE]
-spe_sub$cluster_collapsed <- droplevels(spe_sub$cluster_collapsed)
-neuropil_pseudo <-
-  registration_pseudobulk(
-    spe_sub,
-    var_registration = "cluster_collapsed",
-    var_sample_id = "sample_id",
-    covars = c("age", "sex"),
-    min_ncells = 50,
-    pseudobulk_rds_file = here("processed-data", "revision_maddy", "neuropilDE", "SLM.SGZ-SL.SR_neuropil_pseudo.rds")
-    )
-	
-dim(neuropil_pseudo)
+#spe_sub <- spe[, colData(spe)$cluster_collapsed %in% c("SLM.WM", "SL.SR") &  colData(spe)$neuropil_pos == TRUE]
+#spe_sub$cluster_collapsed <- droplevels(spe_sub$cluster_collapsed)
+#neuropil_pseudo <-
+#  registration_pseudobulk(
+#    spe_sub,
+#    var_registration = "cluster_collapsed",
+#    var_sample_id = "sample_id",
+#    covars = c("age", "sex"),
+#    min_ncells = 50,
+#    pseudobulk_rds_file = here("processed-data", "revision_maddy", "neuropilDE", "SLM.SGZ-SL.SR_neuropil_pseudo.rds")
+#    )
+#	
+#dim(neuropil_pseudo)
 	#[1] 3077   16	
 
+neuropil_pseudo = readRDS(here("processed-data", "revision_maddy", "neuropilDE", "SLM.SGZ-SL.SR_neuropil_pseudo.rds"))
 dx_res <- registration_stats_enrichment(
 	  neuropil_pseudo,
 	  block_cor = "sample_id",
@@ -37,19 +38,23 @@ dx_res <- registration_stats_enrichment(
 	
 
 	dx_res$sig <- with(dx_res, 
-	                   (logFC_SLM.WM > log2(2) & fdr_SLM.WM < 0.03) | 
-	                   (logFC_SLM.WM < -log2(2) & fdr_SLM.WM < 0.03)
+	                   (logFC_SLM.WM > log2(3) & fdr_SLM.WM < 0.05) | 
+	                   (logFC_SLM.WM < -log2(3) & fdr_SLM.WM < 0.05)
 	)
  
-	top_genes <- dx_res %>% 
-	  filter(sig == TRUE) %>% 
+	top_genes_up <- dx_res %>% 
+	  filter(sig == TRUE & logFC_SLM.WM > log2(3)) %>% 
 	  arrange(desc(abs(logFC_SLM.WM))) %>% 
-	  slice(1:10)
-
+	  slice(1:5)
+  	top_genes_down <- dx_res %>% 
+  	  filter(sig == TRUE & logFC_SLM.WM < -log2(3)) %>% 
+  	  arrange(desc(abs(logFC_SLM.WM))) %>% 
+  	  slice(1:5)
+	top_genes = rbind(top_genes_up,top_genes_down)
 			
 	p <- ggplot(data = dx_res, aes(x = logFC_SLM.WM, y = -log10(p_value_SLM.WM), color = sig)) +
 	  geom_point() + 
-	  scale_color_manual(values = c("FALSE" = "grey", "TRUE" = "red"), breaks = c("TRUE", "FALSE"), name = "FDR<0.03") + 
+	  scale_color_manual(values = c("FALSE" = "grey", "TRUE" = "red"), breaks = c("TRUE", "FALSE"), name = "FDR<0.05") + 
 	  theme_minimal() +
 	  geom_text(data = top_genes, aes(label = gene), color = "black", vjust = -0.5, hjust = 0.5) +
 	  #geom_hline(yintercept = -log10(0.03), lty = "dashed", color = "royalblue") + 
@@ -77,23 +82,24 @@ ggsave(here("plots", "revision_maddy", "neuropilDE", "SLM.SGZ-SL.SR.png"), plot 
 
 #############SLM.SGZ-E#############			
 
-spe_sub <- spe[, colData(spe)$cluster_collapsed %in% c("SLM.WM", "SL.SR", "SR.SLM", "ML") &  colData(spe)$neuropil_pos == TRUE]
-spe_sub$neuropil_cluster = 2
-spe_sub$neuropil_cluster[spe_sub$cluster_collapsed == "SLM.WM"] = 1
+#spe_sub <- spe[, colData(spe)$cluster_collapsed %in% c("SLM.WM", "SL.SR", "SR.SLM", "ML") &  colData(spe)$neuropil_pos == TRUE]
+#spe_sub$neuropil_cluster = 2
+#spe_sub$neuropil_cluster[spe_sub$cluster_collapsed == "SLM.WM"] = 1
+#
+#neuropil_pseudo <-
+#  registration_pseudobulk(
+#    spe_sub,
+#    var_registration = "neuropil_cluster",
+#    var_sample_id = "sample_id",
+#    covars = c("age", "sex"),
+#    min_ncells = 50,
+#    pseudobulk_rds_file = here("processed-data", "revision_maddy", "neuropilDE", "SLM.SGZ-E_neuropil_pseudo.rds")
+#    )
+#	
+#dim(neuropil_pseudo)
+#	#[1] 15719    62	
 
-neuropil_pseudo <-
-  registration_pseudobulk(
-    spe_sub,
-    var_registration = "neuropil_cluster",
-    var_sample_id = "sample_id",
-    covars = c("age", "sex"),
-    min_ncells = 50,
-    pseudobulk_rds_file = here("processed-data", "revision_maddy", "neuropilDE", "SLM.SGZ-E_neuropil_pseudo.rds")
-    )
-	
-dim(neuropil_pseudo)
-	#[1] 15719    62	
-
+neuropil_pseudo = readRDS(here("processed-data", "revision_maddy", "neuropilDE", "SLM.SGZ-E_neuropil_pseudo.rds"))
 dx_res <- registration_stats_enrichment(
   neuropil_pseudo,
   block_cor = "sample_id",
@@ -105,17 +111,22 @@ dx_res <- registration_stats_enrichment(
 	
 
 dx_res$sig <- with(dx_res, 
-                   (logFC_1> log2(2) & fdr_1 < 0.03) | 
-                   (logFC_1< -log2(2) & fdr_1 < 0.03)
+                   (logFC_1> log2(3) & fdr_1 < 0.05) | 
+                   (logFC_1< -log2(3) & fdr_1 < 0.05)
 ) 
-top_genes <- dx_res %>% 
-  filter(sig == TRUE) %>% 
+top_genes_up <- dx_res %>% 
+  filter(sig == TRUE & logFC_1 > log2(3)) %>% 
   arrange(desc(abs(logFC_1))) %>% 
-  slice(1:10)
+  slice(1:5)
+top_genes_down <- dx_res %>% 
+  filter(sig == TRUE & logFC_1 < -log2(3)) %>% 
+  arrange(desc(abs(logFC_1))) %>% 
+  slice(1:5)
+top_genes = rbind(top_genes_up,top_genes_down)
 			
 p <- ggplot(data = dx_res, aes(x = logFC_1, y = -log10(p_value_1), color = sig)) +
   geom_point() + 
-  scale_color_manual(values = c("FALSE" = "grey", "TRUE" = "red"), breaks = c("TRUE", "FALSE"), name = "FDR<0.03") + 
+  scale_color_manual(values = c("FALSE" = "grey", "TRUE" = "red"), breaks = c("TRUE", "FALSE"), name = "FDR<0.05") + 
   theme_minimal() +
   geom_text(data = top_genes, aes(label = gene), color = "black", vjust = -0.5, hjust = 0.5) +
   #geom_hline(yintercept = -log10(0.03), lty = "dashed", color = "royalblue") + 
@@ -131,21 +142,22 @@ ggsave(here("plots", "revision_maddy", "neuropilDE", "SLM.SGZ-E.png"), plot = p,
 				
 #############SLM.SGZ-MLM#############				
 				
-spe_sub <- spe[, colData(spe)$cluster_collapsed %in% c("SLM.WM", "ML") &  colData(spe)$neuropil_pos == TRUE]
-spe_sub$cluster_collapsed <- droplevels(spe_sub$cluster_collapsed)
-
-neuropil_pseudo <-
-  registration_pseudobulk(
-    spe_sub,
-    var_registration = "cluster_collapsed",
-    var_sample_id = "sample_id",
-    covars = c("age", "sex"),
-    min_ncells = 50,
-    pseudobulk_rds_file = here("processed-data", "revision_maddy", "neuropilDE", "SLM.SGZ-ML_neuropil_pseudo.rds")
-    )
-
-dim(neuropil_pseudo)
-	#[1] 15719    62	
+#spe_sub <- spe[, colData(spe)$cluster_collapsed %in% c("SLM.WM", "ML") &  colData(spe)$neuropil_pos == TRUE]
+#spe_sub$cluster_collapsed <- droplevels(spe_sub$cluster_collapsed)
+#
+#neuropil_pseudo <-
+#  registration_pseudobulk(
+#    spe_sub,
+#    var_registration = "cluster_collapsed",
+#    var_sample_id = "sample_id",
+#    covars = c("age", "sex"),
+#    min_ncells = 50,
+#    pseudobulk_rds_file = here("processed-data", "revision_maddy", "neuropilDE", "SLM.SGZ-ML_neuropil_pseudo.rds")
+#    )
+#
+#dim(neuropil_pseudo)
+#	#[1] 15719    62	
+neuropil_pseudo = readRDS(here("processed-data", "revision_maddy", "neuropilDE", "SLM.SGZ-ML_neuropil_pseudo.rds"))
 
 dx_res <- registration_stats_enrichment(
   neuropil_pseudo,
@@ -157,18 +169,23 @@ dx_res <- registration_stats_enrichment(
 )
 
 dx_res$sig <- with(dx_res, 
-                   (logFC_SLM.WM > log2(2) & fdr_SLM.WM < 0.03) | 
-                   (logFC_SLM.WM < -log2(2) & fdr_SLM.WM < 0.03)
+                   (logFC_SLM.WM > log2(3) & fdr_SLM.WM < 0.05) | 
+                   (logFC_SLM.WM < -log2(3) & fdr_SLM.WM < 0.05)
 ) 
-top_genes <- dx_res %>% 
-  filter(sig == TRUE) %>% 
+top_genes_up <- dx_res %>% 
+  filter(sig == TRUE & logFC_SLM.WM > log2(3)) %>% 
   arrange(desc(abs(logFC_SLM.WM))) %>% 
-  slice(1:10)
+  slice(1:5)
+top_genes_down <- dx_res %>% 
+  filter(sig == TRUE & logFC_SLM.WM < -log2(3)) %>% 
+  arrange(desc(abs(logFC_SLM.WM))) %>% 
+  slice(1:5)
+top_genes = rbind(top_genes_up,top_genes_down)
 
 			
 p <- ggplot(data = dx_res, aes(x = logFC_SLM.WM, y = -log10(p_value_SLM.WM), color = sig)) +
   geom_point() + 
-  scale_color_manual(values = c("FALSE" = "grey", "TRUE" = "red"), breaks = c("TRUE", "FALSE"), name = "FDR<0.03") + 
+  scale_color_manual(values = c("FALSE" = "grey", "TRUE" = "red"), breaks = c("TRUE", "FALSE"), name = "FDR<0.05") + 
   theme_minimal() +
   geom_text(data = top_genes, aes(label = gene), color = "black", vjust = -0.5, hjust = 0.5) +
   #geom_hline(yintercept = -log10(0.03), lty = "dashed", color = "royalblue") + 
@@ -186,21 +203,21 @@ ggsave(here("plots", "revision_maddy", "neuropilDE", "SLM.SGZ-ML.png"), plot = p
 
 #############SLM.SGZ-SR.SLM#############
 
-spe_sub <- spe[, colData(spe)$cluster_collapsed %in% c("SLM.WM", "SR.SLM") &  colData(spe)$neuropil_pos == TRUE]
-spe_sub$cluster_collapsed <- droplevels(spe_sub$cluster_collapsed)
-neuropil_pseudo <-
-  registration_pseudobulk(
-    spe_sub,
-    var_registration = "cluster_collapsed",
-    var_sample_id = "sample_id",
-    covars = c("age", "sex"),
-    min_ncells = 50,
-    pseudobulk_rds_file = here("processed-data", "revision_maddy", "neuropilDE", "SLM.SGZ-SR.SLM_neuropil_pseudo.rds")
-    )
-
-dim(neuropil_pseudo)
+#spe_sub <- spe[, colData(spe)$cluster_collapsed %in% c("SLM.WM", "SR.SLM") &  colData(spe)$neuropil_pos == TRUE]
+#spe_sub$cluster_collapsed <- droplevels(spe_sub$cluster_collapsed)
+#neuropil_pseudo <-
+#  registration_pseudobulk(
+#    spe_sub,
+#    var_registration = "cluster_collapsed",
+#    var_sample_id = "sample_id",
+#    covars = c("age", "sex"),
+#    min_ncells = 50,
+#    pseudobulk_rds_file = here("processed-data", "revision_maddy", "neuropilDE", "SLM.SGZ-SR.SLM_neuropil_pseudo.rds")
+#    )
+#
+#dim(neuropil_pseudo)
 	#[1] 15719    62	
-
+neuropil_pseudo = readRDS(here("processed-data", "revision_maddy", "neuropilDE", "SLM.SGZ-SR.SLM_neuropil_pseudo.rds"))
 dx_res <- registration_stats_enrichment(
   neuropil_pseudo,
   block_cor = "sample_id",
@@ -211,19 +228,24 @@ dx_res <- registration_stats_enrichment(
 )
 
 dx_res$sig <- with(dx_res, 
-                   (logFC_SLM.WM > log2(2) & fdr_SLM.WM < 0.03) | 
-                   (logFC_SLM.WM < -log2(2) & fdr_SLM.WM < 0.03)
+                   (logFC_SLM.WM > log2(3) & fdr_SLM.WM < 0.05) | 
+                   (logFC_SLM.WM < -log2(3) & fdr_SLM.WM < 0.05)
 )
  
-top_genes <- dx_res %>% 
-  filter(sig == TRUE) %>% 
+top_genes_up <- dx_res %>% 
+  filter(sig == TRUE & logFC_SLM.WM > log2(3)) %>% 
   arrange(desc(abs(logFC_SLM.WM))) %>% 
-  slice(1:10)
+  slice(1:5)
+top_genes_down <- dx_res %>% 
+  filter(sig == TRUE & logFC_SLM.WM < -log2(3)) %>% 
+  arrange(desc(abs(logFC_SLM.WM))) %>% 
+  slice(1:5)
+top_genes = rbind(top_genes_up,top_genes_down)
 
 			
 p <- ggplot(data = dx_res, aes(x = logFC_SLM.WM, y = -log10(p_value_SLM.WM), color = sig)) +
   geom_point() + 
-  scale_color_manual(values = c("FALSE" = "grey", "TRUE" = "red"), breaks = c("TRUE", "FALSE"), name = "FDR<0.03") + 
+  scale_color_manual(values = c("FALSE" = "grey", "TRUE" = "red"), breaks = c("TRUE", "FALSE"), name = "FDR<0.05") + 
   theme_minimal() +
   geom_text(data = top_genes, aes(label = gene), color = "black", vjust = -0.5, hjust = 0.5) +
   #geom_hline(yintercept = -log10(0.03), lty = "dashed", color = "royalblue") + 
